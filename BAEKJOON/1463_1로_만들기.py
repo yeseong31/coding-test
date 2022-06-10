da057d42ee20aa3e0970ee025d2257f45a496685
예성 <yeseong31@naver.com>
2022-06-10 20:34:09 +0900

    0610 1로 만들기(수정)

--- BAEKJOON/1463_1로_만들기.py
+++ BAEKJOON/1463_1로_만들기.py
@@ -1,13 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 x = int(input())
 
 d = [0] * (x + 1)
-d[2] = 1
 
-for i in range(3, x + 1):
+for i in range(2, x + 1):
     d[i] = d[i - 1] + 1
-    if i % 3 == 0:
-        d[i] = min(d[i], d[i // 3] + 1)
-    elif i % 2 == 0:
-        d[i] = min(d[i], d[i // 2] + 1)
+    for j in [2, 3]:
+        if i % j == 0:
+            d[i] = min(d[i], d[i // j] + 1)
 
 print(d[x])
