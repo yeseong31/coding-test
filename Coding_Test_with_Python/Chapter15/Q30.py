979823ba59c954811292e03b7d9fad36ecb0ebe8
yeseong31 <yeseong31@naver.com>
2022-06-11 10:23:20 +0900

    0611 가사 검색(수정)

--- Coding_Test_with_Python/Chapter15/Q30.py
+++ Coding_Test_with_Python/Chapter15/Q30.py
@@ -1,80 +1,48 @@
 """
 가사 검색(370p)
 """
-import collections
+from bisect import bisect_left, bisect_right
 
 
 def solution(words, queries):
-    # 트라이 노드
-    class Node:
-        def __init__(self):
-            self.cnt = 0
-            self.children = {}
+    # 쿼리와 매칭되는 단어의 수(범위)
+    def count(a, left, right):
+        return bisect_right(a, right) - bisect_left(a, left)
 
-    # 트라이 클래스
-    class Trie:
-        def __init__(self):
-            self.root = Node()
+    # 탐색
+    def search():
+        result = []
+        # 단어를 배열에 저장
+        for word in words:
+            arr[len(word)].append(word)
+            reversed_arr[len(word)].append(word[::-1])  # 문자를 뒤집어서 저장
+        # 정렬 (for 이진 탐색)
+        for i in range(10001):
+            arr[i].sort()
+            reversed_arr[i].sort()
+        # 정렬된 단어에 대해 쿼리를 하나씩 확인
+        for query in queries:
+            # 접두사가 '?'인 경우
+            if query[0] == '?':
+                res = count(reversed_arr[len(query)],
+                                        query[::-1].replace('?', 'a'),
+                                        query[::-1].replace('?', 'z'))
+            # 접미사가 '?'인 경우
+            else:
+                res = count(arr[len(query)],
+                                        query.replace('?', 'a'),
+                                        query.replace('?', 'z'))
+            # 결과 저장
+            result.append(res)
+        return result
 
-        # word 삽입
-        def insert(self, word):
-            node = self.root
-            for c in word:
-                if c not in node.children:
-                    node.children[c] = Node()
-                node = node.children[c]
-                node.cnt += 1
+    arr = [[] for _ in range(10001)]
+    reversed_arr = [[] for _ in range(10001)]
 
-        # word 탐색
-        def search(self, word):
-            node = self.root
-            for c in word:
-                if c not in node.children:
-                    return node.cnt
-                node = node.children[c]
-            return node.cnt
-
-    answer = []
-
-    # 트라이 구현
-    trie = Trie()
-
-    word_len_dic = collections.defaultdict(int)
-    for word in words:
-        length = len(word)
-        word_len_dic[length] += 1
-        trie.insert(str(length) + word)
-        trie.insert(str(length) + word[::-1])
-
-    dic = collections.defaultdict(int)
-    for query in queries:
-        # 중복 키워드 처리
-        if query in answer:
-            answer.append(dic[query])
-
-        length = len(query)
-
-        # 와일드카드 문자가 없으면
-        if query.count('?') == 0:
-            target = query
-        # 와일드카드 문자로만 이루어졌다면
-        elif query.count('?') == length:
-            answer.append(word_len_dic[length])
-            continue
-        # 와일드카드 문자가 접두사라면
-        elif query[0] == '?':
-            query = query[::-1]
-            target = query[:query.index('?')]
-        # 와일드카드 문자가 접미사라면
-        else:
-            target = query[:query.index('?')]
-
-        dic[target] = trie.search(str(len(query)) + target)
-        answer.append(dic[target])
-
-    return answer
+    # 탐색 시작
+    return search()
 
 
 words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
-queries = ["???rf"]
+queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
 print(solution(words, queries))
