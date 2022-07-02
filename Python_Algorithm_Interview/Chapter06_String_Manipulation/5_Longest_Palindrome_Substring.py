50e16bc3be6173a9d1e24a447267ffad2fb084e9
예성 <yeseong31@naver.com>
2022-07-02 18:13:15 +0900

    0702

--- Python_Algorithm_Interview/Chapter06_String_Manipulation/5_Longest_Palindrome_Substring.py
+++ Python_Algorithm_Interview/Chapter06_String_Manipulation/5_Longest_Palindrome_Substring.py
@@ -2,22 +2,22 @@
 
 
 def longestPalindrome(s: str) -> str:
-    # 팰린드롬 문자열 판별 및 투 포인터 확장
-    def expand(left: int, right: int) -> str:
+
+    def check_palindrome(left: int, right: int) -> str:
         while left >= 0 and right <= len(s) and s[left] == s[right - 1]:
             left -= 1
             right += 1
         return s[left + 1:right - 1]
 
-    # 문자열의 길이가 두 글자 미만이거나 그 자체로 팰린드롬 문자열이라면 그대로 반환
-    if len(s) < 2 or s[:] == s[::-1]:
+    # 문자열의 길이가 2 미만이거나 s 자체가 팰린드롬이라면 s 반환
+    if len(s) < 2 or s == s[::-1]:
         return s
 
-    result = ''
+    target = ''
     for i in range(len(s) - 1):
-        # 홀수와 짝수의 경우를 나누어서 생각해야 함
-        result = max(result, expand(i, i + 1), expand(i, i + 2), key=len)
-    return result
+        target = max(target, check_palindrome(i, i + 1), check_palindrome(i, i + 2))
+
+    return target
 
 
 s = "babad"
--- Python_Algorithm_Interview/Chapter06_String_Manipulation/819_Most_Common_Word.py
+++ Python_Algorithm_Interview/Chapter06_String_Manipulation/819_Most_Common_Word.py
@@ -7,8 +7,8 @@ from collections import Counter
 
 
 def mostCommonWord(paragraph: str, banned: list[str]) -> str:
-    words = [x for x in re.sub(r'[^\w]', ' ', paragraph).lower().split() if x not in banned]
-    return Counter(words).most_common(1)[0][0]
+    target = [w for w in re.sub(r'[^\w]', ' ', paragraph.lower()).split() if w not in banned]
+    return Counter(target).most_common(1)[0][0]
 
 
 paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
