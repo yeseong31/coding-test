1a60fedd8c5692fd3ca072c662b19c8299dcd39f
예성 <yeseong31@naver.com>
2022-07-28 10:48:04 +0900

    0728 숫자와 표현

---
+++ Programmers/숫자의 표현.py
@@ -0,0 +1,17 @@
+def solution(n):
+    cnt = 0
+    for i in range(n, 0, -1):
+        target = n
+        j = i
+        while target > 0:
+            target -= j
+            j -= 1
+            if j <= 0:
+                break
+        if target == 0:
+            cnt += 1
+
+    return cnt
+
+
+print(solution(15))
