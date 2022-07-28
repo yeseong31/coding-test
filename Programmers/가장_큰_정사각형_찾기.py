4a8b47c21f26aebbe6e14fc31fa3e1495fdbe309
yeseong31 <yeseong31@naver.com>
2022-07-28 22:44:10 +0900

    0728 가장 큰 정사각형 찾기

---
+++ Programmers/가장_큰_정사각형_찾기.py
@@ -0,0 +1,19 @@
+def solution(board):
+    answer = 0
+    row, col = len(board), len(board[0])
+    n = max(row, col)
+    dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+    for i in range(1, len(board) + 1):
+        for j in range(1, len(board[0]) + 1):
+            if board[i-1][j-1] != 0:
+                dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
+                answer = max(answer, dp[i][j])
+    return answer ** 2
+
+
+board = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
+print(solution(board))
+
+board = [[0,0,1,1],[1,1,1,1]]
+print(solution(board))
