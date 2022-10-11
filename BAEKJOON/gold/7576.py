ba13e14d82a288ea9787881bd0d24cab14ab8ebb
yeseong31 <yeseong31@naver.com>
2022-10-11 22:31:08 +0900

    1011 feat: 토마토

---
+++ BAEKJOON/gold/7576.py
@@ -0,0 +1,39 @@
+# 토마토
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+q = deque()
+size = m * n
+
+dx = (1, 0, -1, 0)
+dy = (0, 1, 0, -1)
+
+box = []
+for i in range(n):
+    lst = list(map(int, input().split()))
+    for j in range(m):
+        if lst[j] == 1:
+            q.append((i, j, 0))
+        elif lst[j] == -1:
+            size -= 1
+    box.append(lst)
+
+answer = 0
+while q:
+    x, y, day = q.popleft()
+    answer = max(answer, day)
+    size -= 1
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if nx < 0 or nx >= n or ny < 0 or ny >= m or box[nx][ny] != 0:
+            continue
+        q.append((nx, ny, day + 1))
+        box[nx][ny] = 1
+
+if size != 0:
+    print(-1)
+else:
+    print(answer)
