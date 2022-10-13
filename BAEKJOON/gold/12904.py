e15fa9f347c78448a9933f58ae57f7874a0774a6
yeseong31 <yeseong31@naver.com>
2022-10-13 14:37:31 +0900

    1013 feat: A와 B

---
+++ BAEKJOON/gold/12904.py
@@ -0,0 +1,17 @@
+# A와 B
+
+s = input()
+t = list(input())
+
+answer = 0
+while len(t) > len(s):
+    if t[-1] == 'A':
+        t.pop()
+    elif t[-1] == 'B':
+        t.pop()
+        t.reverse()
+
+if s == ''.join(t):
+    print(1)
+else:
+    print(0)
