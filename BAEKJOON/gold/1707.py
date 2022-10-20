0a594b5c587fa9e4055c2ca0d90aca885fa31d51
yeseong31 <yeseong31@naver.com>
2022-10-20 21:56:24 +0900

    1020 refactor: 이분 그래프

--- BAEKJOON/gold/1707.py
+++ BAEKJOON/gold/1707.py
@@ -6,16 +6,16 @@ from collections import deque
 input = sys.stdin.readline
 
 
-def bfs(start, group):
+def bfs(start: int):
     q = deque([start])
-    visited[start] = group
+    visited[start] = True
     while q:
         x = q.popleft()
         for p in graph[x]:
             if not visited[p]:
                 q.append(p)
                 visited[p] = -1 * visited[x]
-            elif visited[p] == visited[x]:
+            if visited[p] == visited[x]:
                 return False
     return True
 
@@ -25,13 +25,16 @@ for _ in range(int(input())):
     n, m = map(int, input().split())
     graph = [[] for _ in range(n + 1)]
     visited = [False] * (n + 1)
+
     for _ in range(m):
         a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
+
     for i in range(1, n + 1):
         if not visited[i]:
-            answer = bfs(i, 1)
+            answer = bfs(i)
             if not answer:
                 break
+
     print('YES' if answer else 'NO')
