9fd5d7a20695ae9061af417da3df062a551f3238
예성 <66625672+yeseong31@users.noreply.github.com>
2022-10-29 10:49:42 +0900

    [Gold IV] Title: 알파벳, Time: 2620 ms, Memory: 51316 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,32 +1,23 @@
-def dfs(x, y, d):
-    global answer
-    answer = max(answer, d)
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if nx < 0 or nx >= r or ny < 0 or ny >= c:
-            continue
-        target = board[nx][ny]
-        if alpha[find_index(target)] != 0:
-            continue
-        alpha[find_index(target)] += 1
-        dfs(nx, ny, d + 1)
-        alpha[find_index(target)] -= 1
-
-
-def find_index(c):
-    return ord(c) - ord('A')
-
+import sys
+input = sys.stdin.readline
 
 answer = 0
-
 r, c = map(int, input().split())
 board = [list(input()) for _ in range(r)]
-alpha = [0] * 26
-alpha[find_index(board[0][0])] += 1
 
 dx = (1, 0, -1, 0)
 dy = (0, 1, 0, -1)
 
-dfs(0, 0, 1)
+q = set()
+q.add((0, 0, board[0][0]))
+
+while q:
+    x, y, alpha = q.pop()
+    answer = max(answer, len(alpha))
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if nx < 0 or nx >= r or ny < 0 or ny >= c or board[nx][ny] in alpha:
+            continue
+        q.add((nx, ny, alpha + board[nx][ny]))
 
 print(answer)
