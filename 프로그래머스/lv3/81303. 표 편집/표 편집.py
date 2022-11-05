4cb549d087a7f4f1eb8588ae85af6be42fc3b8ff
예성 <66625672+yeseong31@users.noreply.github.com>
2022-11-05 22:07:45 +0900

    [level 3] Title: 표 편집, Time: 1219.13 ms, Memory: 245 MB -BaekjoonHub

--- 프로그래머스/lv3/81303. 표 편집/표 편집.py
+++ 프로그래머스/lv3/81303. 표 편집/표 편집.py
@@ -5,44 +5,59 @@ class Node:
         self.rm = False
 
 
+class Table:
+    def __init__(self, n: int, k: int):
+        self.table = [Node(i - 1, i + 1) for i in range(n)]
+        self.table[0].left = self.table[-1].right = None
+        self.removes = []
+        self.k = k
+
+    def up(self, x):
+        for _ in range(x):
+            self.k = self.table[self.k].left
+
+    def down(self, x):
+        for _ in range(x):
+            self.k = self.table[self.k].right
+
+    def remove(self):
+        self.removes.append(self.k)
+        self.table[self.k].rm = True
+        left, right = self.table[self.k].left, self.table[self.k].right
+        if left is not None:
+            self.table[left].right = right
+            self.k = left
+        if right is not None:
+            self.table[right].left = left
+            self.k = right
+
+    def restore(self):
+        r = self.removes.pop()
+        self.table[r].rm = False
+        left, right = self.table[r].left, self.table[r].right
+        if left is not None:
+            self.table[left].right = r
+        if right is not None:
+            self.table[right].left = r
+
+    def get_table(self):
+        return self.table
+
+
 def solution(n, k, cmd):
     answer = ''
-
-    removes = []
-    table = [Node(i - 1, i + 1) for i in range(n)]
-    table[0].left = table[-1].right = None
+    table = Table(n, k)
 
     for target in cmd:
         if target[0] == 'C':
-            removes.append(k)
-            table[k].rm = True
-            left, right = table[k].left, table[k].right
-            if left is not None:
-                table[left].right = right
-                k = left
-            if right is not None:
-                table[right].left = left
-                k = right
-
+            table.remove()
         elif target[0] == 'Z':
-            r = removes.pop()
-            table[r].rm = False
-            left, right = table[r].left, table[r].right
-            if left is not None:
-                table[left].right = r
-            if right is not None:
-                table[right].left = r
-
+            table.restore()
         elif target[0] == 'U':
-            x = int(target.split()[1])
-            for _ in range(x):
-                k = table[k].left
-
+            table.up(int(target.split()[1]))
         elif target[0] == 'D':
-            x = int(target.split()[1])
-            for _ in range(x):
-                k = table[k].right
+            table.down(int(target.split()[1]))
 
-    for node in table:
+    for node in table.get_table():
         answer += 'X' if node.rm else 'O'
     return answer
