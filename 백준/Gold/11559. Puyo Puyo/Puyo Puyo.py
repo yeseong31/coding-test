074237ae6c68e69daef392a51d6dae085c9780f8
예성 <66625672+yeseong31@users.noreply.github.com>
2022-11-09 23:17:59 +0900

    [Gold IV] Title: Puyo Puyo, Time: 92 ms, Memory: 32556 KB -BaekjoonHub

---
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -0,0 +1,75 @@
+from collections import deque
+
+
+def rotate_matrix(a):
+    row, col = len(a), len(a[0])
+    result = [deque([0] * 12) for _ in range(6)]
+    for r in range(12):
+        for c in range(6):
+            result[c][row - 1 - r] = a[r][c]
+    return result
+
+
+def bfs(x, y, p):
+    color = board[x][y]
+    q.append((x, y))
+    p.append((x, y))
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or nx >= 6 or ny < 0 or ny >= 12:
+                continue
+            if board[nx][ny] != color or (nx, ny) in p or visited[nx][ny]:
+                continue
+            visited[nx][ny] = True
+            p.append((nx, ny))
+            q.append((nx, ny))
+    return p
+    
+
+board = rotate_matrix([list(input()) for _ in range(12)])
+dx = (1, 0, -1, 0)
+dy = (0, 1, 0, -1)
+
+answer = 0
+q = deque()
+
+while True:
+    visited = [[False] * 12 for _ in range(6)]
+    check = False
+    check_area = 0
+    points = []
+    
+    for i in range(6):
+        for j in range(12):
+            if board[i][j] == '.':
+                break
+            if visited[i][j]:
+                continue
+            p = bfs(i, j, [])
+            if len(p) >= 4:
+                check_area += len(p)
+                points.extend(p)
+                check = True
+                
+    if not check:
+        break
+    answer += 1
+    
+    while points:
+        x, y = points.pop()
+        board[x][y] = '.'
+        
+    for i in range(6):
+        stack = []
+        for _ in range(12):
+            b = board[i].popleft()
+            if b == '.':
+                board[i].append(b)
+            else:
+                stack.append(b)
+        while stack:
+            board[i].appendleft(stack.pop())
+            
+print(answer)
