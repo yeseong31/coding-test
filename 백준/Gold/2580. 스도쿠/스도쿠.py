832d414280161b7a1d57e58e5233d4bb49c97820
예성 <66625672+yeseong31@users.noreply.github.com>
2022-11-15 17:10:31 +0900

    [Gold IV] Title: 스도쿠, Time: 3268 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -2,41 +2,37 @@ import sys
 input = sys.stdin.readline
 
 
-def check_row(x, k):
-    for y in range(9):
-        if board[x][y] == k:
-            return False
-    return True
-
-
-def check_col(y, k):
-    for x in range(9):
-        if board[x][y] == k:
-            return False
-    return True
-
-
-def check_square(x, y, k):
-    for i in range(x, x + 3):
-        for j in range(y, y + 3):
-            if board[i][j] == k:
-                return False
-    return True
-
-
-def dfs(i):
-    if i == len(blank):
-        for b in board:
-            print(*b)
-        exit()
-    for n in range(1, 10):
-        x, y = blank[i]
-        if check_row(x, n) and check_col(y, n) and check_square(x // 3 * 3, y // 3 * 3, n):
+def solution():
+    def dfs(k):
+        if k == len(blanks):
+            return True
+        x, y = blanks[k]
+        z = (x // 3 * 3) + y // 3
+        for n in range(1, 10):
+            if row[x][n] or col[n][y] or block[z][n]:
+                continue
+            row[x][n] = col[n][y] = block[z][n] = True
             board[x][y] = n
-            dfs(i + 1)
+            if dfs(k + 1):
+                return True
+            row[x][n] = col[n][y] = block[z][n] = False
             board[x][y] = 0
+        return False
+
+    board = [list(map(int, input().rstrip().split())) for _ in range(9)]
+    blanks = [(i, j) for i in range(9) for j in range(9) if board[i][j] == 0]
+    row = [[False] * 10 for _ in range(10)]
+    col = [[False] * 10 for _ in range(10)]
+    block = [[False] * 10 for _ in range(10)]
+
+    for x in range(9):
+        for y in range(9):
+            z, i = (x // 3 * 3) + y // 3, board[x][y]
+            row[x][i] = col[i][y] = block[z][i] = True
+
+    dfs(0)
+    for b in board:
+        print(*b)
 
 
-board = [list(map(int, input().rstrip().split())) for _ in range(9)]
-blank = [(i, j) for i in range(9) for j in range(9) if board[i][j] == 0]
-dfs(0)
+solution()
