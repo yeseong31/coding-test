097dc2df8914a856d2c68b2c6289cee8e677386e
예성 <66625672+yeseong31@users.noreply.github.com>
2022-11-20 22:04:45 +0900

    [Silver II] Title: 스타트와 링크, Time: 704 ms, Memory: 55976 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,17 +1,16 @@
+import sys
 from itertools import combinations
+
+input = sys.stdin.readline
 
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
 sums = [sum(r) + sum(c) for r, c in zip(board, zip(*board))]
+total = sum(sums)
 
 answer = 20001
 for comb in list(combinations(range(n), n // 2)):
-    red = blue = 0
-    for i in range(n):
-        if i in comb:
-            red += sums[i]
-        else:
-            blue += sums[i]
-    answer = min(answer, (abs(red - blue)) // 2)
+    red = sum([sums[i] for i in range(n) if i in comb])
+    answer = min(answer, (abs(total - 2 * red)) // 2)
 
 print(answer)
