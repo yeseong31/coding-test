900f58147c4e2bf073cd7dc3fa71c5149e1d826a
예성 <66625672+yeseong31@users.noreply.github.com>
2022-11-25 15:58:44 +0900

    [level 3] Title: 합승 택시 요금, Time: 277.64 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -0,0 +1,18 @@
+def solution(n, s, a, b, fares):
+    inf = int(1e9)
+    graph = [[inf] * (n + 1) for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if i == j:
+                graph[i][j] = 0
+                
+    for v, w, c in fares:
+        graph[v][w] = graph[w][v] = c
+    
+    for k in range(1, n + 1):
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+    
+    return min([graph[s][i] + graph[i][a] + graph[i][b] for i in range(1, n + 1)])
