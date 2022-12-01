0903898b117f8a4a1ed8cb0ea318145a874fcf45
예성 <66625672+yeseong31@users.noreply.github.com>
2022-12-01 22:01:32 +0900

    [unrated] Title: 점 찍기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
@@ -1,5 +1,2 @@
-import math
-
-
 def solution(k, d):
-    return sum([1 + math.floor(math.sqrt(d ** 2 - x ** 2)) // k for x in range(0, d + 1, k)])
+    return sum([1 + int((d ** 2 - x ** 2) ** 0.5) // k for x in range(0, d + 1, k)])
