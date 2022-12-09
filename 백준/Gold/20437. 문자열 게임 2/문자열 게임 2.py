09be71b27256773d8a49868238c145356cd31807
예성 <66625672+yeseong31@users.noreply.github.com>
2022-12-09 22:28:22 +0900

    [Gold V] Title: 문자열 게임 2, Time: 2996 ms, Memory: 34076 KB -BaekjoonHub

---
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+
+for _ in range(int(input())):
+    w = input().strip()
+    k = int(input())
+    count = defaultdict(list)
+    
+    for i in range(len(w)):
+        if w.count(w[i]) >= k:
+            count[w[i]].append(i)
+            
+    if not count:
+        print(-1)
+        continue
+    
+    mn, mx = 10000, 0
+    for i in count:
+        for j in range(len(count[i]) - k + 1):
+            _len = count[i][j + k - 1] - count[i][j] + 1
+            mn, mx = min(mn, _len), max(mx, _len)
+    print(mn, mx)
