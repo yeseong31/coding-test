f643314095a5e8e6ae8366c0ca30ee408a2b3e2a
예성 <66625672+yeseong31@users.noreply.github.com>
2022-12-12 16:05:28 +0900

    [Gold V] Title: 두 배 더하기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/12931. 두 배 더하기/두 배 더하기.py
+++ 백준/Gold/12931. 두 배 더하기/두 배 더하기.py
@@ -1,17 +1,16 @@
 n = int(input())
 b = list(map(int, input().split()))
-answer = 0
+ans = 0
 
 while True:
-    chk = _sum = 0
-    for i in range(len(b)):
+    c = s = 0
+    for i in range(n):
         if b[i] % 2 == 1:
-            chk += 1
+            c += 1
             b[i] -= 1
-        _sum += b[i]
         b[i] //= 2
-    answer += chk
-    if _sum == 0:
+        s += b[i]
+    ans += c + 1
+    if s == 0:
+        print(ans - 1)
         break
-    answer += 1
-print(answer)
