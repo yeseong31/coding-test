d861bcdd872ef7db2cee9f28aa7268fc23cb8ac4
예성 <66625672+yeseong31@users.noreply.github.com>
2022-12-14 10:37:45 +0900

    [level 3] Title: 표 편집, Time: 1096.04 ms, Memory: 245 MB -BaekjoonHub

--- 프로그래머스/lv3/81303. 표 편집/표 편집.py
+++ 프로그래머스/lv3/81303. 표 편집/표 편집.py
@@ -2,27 +2,28 @@ class Node:
     def __init__(self, left=None, right=None):
         self.left = left
         self.right = right
-        self.rm = False
-
-
+        self.removed = False
+    
+    
 class Table:
-    def __init__(self, n: int, k: int):
+    def __init__(self, n, k):
         self.table = [Node(i - 1, i + 1) for i in range(n)]
-        self.table[0].left = self.table[-1].right = None
-        self.removes = []
+        self.table[0].left = None
+        self.table[-1].right = None
+        self.removed = []
         self.k = k
-
+        
     def up(self, x):
         for _ in range(x):
             self.k = self.table[self.k].left
-
+    
     def down(self, x):
         for _ in range(x):
             self.k = self.table[self.k].right
-
-    def remove(self):
-        self.removes.append(self.k)
-        self.table[self.k].rm = True
+    
+    def delete(self):
+        self.removed.append(self.k)
+        self.table[self.k].removed = True
         left, right = self.table[self.k].left, self.table[self.k].right
         if left is not None:
             self.table[left].right = right
@@ -30,20 +31,20 @@ class Table:
         if right is not None:
             self.table[right].left = left
             self.k = right
-
+    
     def restore(self):
-        r = self.removes.pop()
-        self.table[r].rm = False
-        left, right = self.table[r].left, self.table[r].right
+        node = self.removed.pop()
+        self.table[node].removed = False
+        left, right = self.table[node].left, self.table[node].right
         if left is not None:
-            self.table[left].right = r
+            self.table[left].right = node
         if right is not None:
-            self.table[right].left = r
-
-    def get_answer(self):
+            self.table[right].left = node
+        
+    def get_result(self):
         answer = ''
         for node in self.table:
-            answer += 'X' if node.rm else 'O'
+            answer += 'X' if node.removed else 'O'
         return answer
 
 
@@ -51,15 +52,11 @@ def solution(n, k, cmd):
     t = Table(n, k)
     for target in cmd:
         if target[0] == 'C':
-            t.remove()
-            continue
-        if target[0] == 'Z':
+            t.delete()
+        elif target[0] == 'Z':
             t.restore()
-            continue
-        x = int(target.split()[1])
-        if target[0] == 'U':
-            t.up(x)
-            continue
-        if target[0] == 'D':
-            t.down(x)
-    return t.get_answer()
+        elif target[0] == 'U':
+            t.up(int(target[2:]))
+        elif target[0] == 'D':
+            t.down(int(target[2:]))
+    return t.get_result()
