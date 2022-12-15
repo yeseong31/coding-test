18fabfa7cba31158294d5a6213173ab0d3711a30
예성 <66625672+yeseong31@users.noreply.github.com>
2022-12-15 23:09:26 +0900

    [Gold V] Title: 배, Time: 3608 ms, Memory: 115132 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+
+def solution():
+    n = int(input())
+    cranes = sorted(list(map(int, input().split())), reverse=True)
+    m = int(input())
+    boxes = sorted(list(map(int, input().split())), reverse=True)
+    
+    if cranes[0] < boxes[0]:
+        return -1
+    
+    answer = 0
+    while boxes:
+        for c in range(n):
+            for b in range(len(boxes)):
+                if cranes[c] >= boxes[b]:
+                    boxes.pop(b)
+                    break
+        answer += 1
+    return answer
+
+
+print(solution())
