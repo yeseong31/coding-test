f288e75fed2d55f8a9a09a49088158110d272bc2
예성 <66625672+yeseong31@users.noreply.github.com>
2023-01-09 22:18:47 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 18.44 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,37 +1,19 @@
-from collections import deque
-
-
 def solution(cap, n, deliveries, pickups):
-    def check_point(target):
-        q = deque()
-        point = n
-        sum_value = 0
-
-        while target:
-            sum_value += target.pop()
-            if sum_value >= cap:
-                target.append(sum_value - cap)
-                q.append(point)
-                
-                sum_value = 0
-                while target and target[-1] == 0:
-                    target.pop()
-                    
-                point = len(target)
-        
-        if point != n and point >= 0:
-            q.append(point)
-        return q
-
     answer = 0
-    d = check_point(deliveries)
-    p = check_point(pickups)
-    
-    while d and p:
-        answer += max(d.popleft(), p.popleft()) * 2
-    while d:
-        answer += d.popleft() * 2
-    while p:
-        answer += p.popleft() * 2
+    d = p = 0
+    point = n - 1
+
+    for i in range(n - 1, -1, -1):
+        d += deliveries[i]
+        p += pickups[i]
+
+        while d > cap or p > cap:
+            d -= cap
+            p -= cap
+            answer += 2 * (point + 1)
+            point = i
+
+    if d > 0 or p > 0:
+        answer += 2 * (point + 1)
 
     return answer
