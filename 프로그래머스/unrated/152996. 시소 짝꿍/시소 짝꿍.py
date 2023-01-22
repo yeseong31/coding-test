2f0c0d00cd2b82164bbdf68a34e7fa1cf2e3b507
예성 <66625672+yeseong31@users.noreply.github.com>
2023-01-22 20:01:27 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -3,18 +3,11 @@ from itertools import combinations
 
 
 def solution(weights):
-    def check(x, y):
-        for a, b in target:
-            if a * x == b * y:
-                return True
-        return False
-
     answer = 0
     cnt = Counter(weights)
     target = [(3, 2), (4, 2), (4, 3)]
 
     for l, r in list(combinations(sorted(set(weights)), 2)):
-        if check(l, r):
-            answer += cnt[l] * cnt[r]
-            
+        answer += sum(cnt[l] * cnt[r] for a, b in target if a * l == b * r)
+
     return answer + sum((c * (c - 1)) // 2 for c in cnt.values() if c > 1)
