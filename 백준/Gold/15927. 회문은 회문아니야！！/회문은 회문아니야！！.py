3140194786d0a6574c8c0ec9354dd3d4edef866f
예성 <66625672+yeseong31@users.noreply.github.com>
2023-01-24 13:55:25 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 144 ms, Memory: 31804 KB -BaekjoonHub

---
+++ 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+
+def solution(s):
+    def check_palindrome(target, left, right):
+        while left < right:
+            if target[left] != target[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+    n = len(s)
+    if not check_palindrome(s, 0, n - 1):
+        return n
+    elif not check_palindrome(s, 0, n - 2):
+        return n - 1
+    return -1
+
+
+print(solution(input().strip()))
