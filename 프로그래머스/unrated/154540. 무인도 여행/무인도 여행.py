845bc9462ce109ca9600f994357a5bde2e7d3761
예성 <66625672+yeseong31@users.noreply.github.com>
2023-01-28 23:57:04 +0900

    [unrated] Title: 무인도 여행, Time: 0.39 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+
+def solution(maps: list):
+    dx = (-1, 0, 1, 0)
+    dy = (0, -1, 0, 1)
+    q = deque()
+    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
+    
+    def bfs(x, y):
+        q.append((x, y))
+        visited[x][y] = True
+        cnt = int(maps[x][y])
+        while q:
+            x, y = q.popleft()
+            for k in range(4):
+                nx, ny = x + dx[k], y + dy[k]
+                if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]):
+                    continue
+                if maps[nx][ny] != 'X' and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    cnt += int(maps[nx][ny])
+                    q.append((nx, ny))
+        return cnt
+    
+    answer = []
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
+            if maps[i][j] != 'X' and not visited[i][j]:
+                answer.append(bfs(i, j))
+        
+    return sorted(answer) if answer else [-1]
