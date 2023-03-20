64bce51d84f0519904d103a75d9fef9c0665ccea
예성 <66625672+yeseong31@users.noreply.github.com>
2023-03-20 13:57:04 +0900

    [level 4] Title: 징검다리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/43236. 징검다리/징검다리.py
@@ -0,0 +1,21 @@
+def solution(distance, rocks, n):
+    answer = 0
+    start, end = 0, distance
+    rocks.sort()
+    while start <= end:
+        mid = (start + end) // 2
+        prev = cnt = 0
+        for rock in rocks:
+            if rock - prev < mid:
+                cnt += 1
+            else:
+                prev = rock
+            if cnt > n:
+                break
+        if cnt > n:
+            end = mid - 1
+        else:
+            answer = mid
+            start = mid + 1
+        
+    return answer
