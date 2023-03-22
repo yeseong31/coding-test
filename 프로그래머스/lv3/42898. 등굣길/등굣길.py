b99c905de7ebcb99c767eaa4e268b80124ebbd66
예성 <66625672+yeseong31@users.noreply.github.com>
2023-03-22 09:56:52 +0900

    [level 3] Title: 등굣길, Time: 2.25 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -1,18 +1,9 @@
 def solution(m, n, puddles):
     board = [[0] * m for _ in range(n)]
-    for x, y in puddles:
-        board[y - 1][x - 1] = -1
-
     board[0][0] = 1
     for i in range(n):
         for j in range(m):
-            if board[i][j] == -1:
-                board[i][j] = 0
+            if [j + 1, i + 1] in puddles or (i, j) == (0, 0):
                 continue
-            if i != 0:
-                board[i][j] += board[i - 1][j]
-            if j != 0:
-                board[i][j] += board[i][j - 1]
-            board[i][j] %= 1000000007
-
-    return board[n - 1][m - 1]
+            board[i][j] = (board[i - 1][j] + board[i][j - 1]) % 1000000007
+    return board[-1][-1]
