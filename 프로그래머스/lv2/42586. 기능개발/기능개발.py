097a73b2bd00b4077dcba162f4599d12ef346702
예성 <66625672+yeseong31@users.noreply.github.com>
2023-03-22 16:01:06 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,18 +1,20 @@
 from math import ceil
+from collections import deque
 
 
 def solution(progresses, speeds):
     answer = []
-    stack = []
+    q = deque()
     
     cnt = 0
-    for day in [ceil((100 - p) / s) for p, s in zip(progresses, speeds)]:
-        while stack and stack[0] < day:
-            stack.pop()
+    for p, s in zip(progresses, speeds):
+        day = ceil((100 - p) / s)
+        while q and q[0] < day:
+            q.popleft()
             cnt += 1
-        stack.append(day)
+        q.append(day)
         if cnt > 0:
             answer.append(cnt)
             cnt = 0
     
-    return answer + [len(stack)]
+    return answer + [len(q)]
