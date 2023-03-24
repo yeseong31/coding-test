b5eafdbea8dc05d1dff833b27c70e578935cdad8
예성 <66625672+yeseong31@users.noreply.github.com>
2023-03-24 10:35:12 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -2,23 +2,31 @@ from collections import deque
 
 
 def solution(board):
-    n = len(board)
-    dist = [[[int(1e9)] * 4 for _ in range(n)] for _ in range(n)]
-    q = deque([(0, 0, 0, 0), (0, 0, 3, 0)])
-    
-    dx = (1, 0, -1, 0)
-    dy = (0, -1, 0, 1)
-    
-    while q:
-        x, y, d, c = q.popleft()
-        for i in range(4):
-            nx, ny, nc = x + dx[i], y + dy[i], c + 100
-            if nx < 0 or nx >= n or ny < 0 or ny >= n or board[nx][ny] == 1:
-                continue
-            if d != i:
-                nc += 500
-            if dist[nx][ny][i] > nc:
-                dist[nx][ny][i] = nc
-                q.append((nx, ny, i, nc))
+    def bfs(x, y, d, c):
+        q = deque([(x, y, d, c)])
+        graph = [[0] * n for _ in range(n)]
+        visited = [[False] * n for _ in range(n)]
+        
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    graph[i][j] = -1
 
-    return min([dist[n - 1][n - 1][i] for i in range(4)])
+        while q:
+            x, y, d, c = q.popleft()
+            for i in range(4):
+                nx, ny = x + dx[i], y + dy[i]
+                if nx < 0 or nx >= n or ny < 0 or ny >= n or graph[nx][ny] == -1:
+                    continue
+                if i == d:
+                    nc = c + 100
+                else:
+                    nc = c + 600
+                if graph[nx][ny] == 0 or graph[nx][ny] > nc:
+                    graph[nx][ny] = nc            
+                    q.append((nx, ny, i, nc))
+        return graph[n - 1][n - 1]
+    
+    n = len(board)
+    dx, dy = (1, 0, -1, 0), (0, 1, 0, -1)
+    return min(bfs(0, 0, 0, 0), bfs(0, 0, 1, 0))
