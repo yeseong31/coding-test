5719ba1b4bd1c3c52c9c846a6503f32b680b78fc
예성 <66625672+yeseong31@users.noreply.github.com>
2023-03-24 09:15:10 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -5,22 +5,23 @@ def solution(begin, target, words):
     if target not in words:
         return 0
     
-    length = len(begin)
-    q = deque([(begin, 0)])
-    visited = set()
-    
+    q = deque([(begin, 0, {begin, })])
     while q:
-        cur_word, cnt = q.popleft()
-        if cur_word == target:
-            return cnt
-        for target_word in words:
-            if target_word in visited:
+        cur, step, visited = q.popleft()
+        if cur == target:
+            return step
+        lst = []
+        for word in words:
+            if word in visited:
                 continue
-            check = 0
-            for a, b in zip(cur_word, target_word):
-                if a == b:
-                    check += 1
-            if length - check == 1:
-                q.append((target_word, cnt + 1))
-    
-    return answer
+            count = 0
+            for c, w in zip(cur, word):
+                if c == w:
+                    count += 1
+            if len(cur) - count == 1:
+                lst.append(word)
+        for l in lst:
+            visited.add(l)
+            q.append((l, step + 1, visited))
+        
+    return 0
