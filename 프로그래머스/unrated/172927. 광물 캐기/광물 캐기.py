5663c43bb4ecb67fb218468ae353ff8531b8177a
예성 <66625672+yeseong31@users.noreply.github.com>
2023-03-28 11:12:47 +0900

    [unrated] Title: 광물 캐기, Time: 24.75 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+
+def solution(picks, minerals):
+    answer = int(1e4)
+    choice = {'diamond': 0, 'iron': 1, 'stone': 2}
+    scores = [[1, 1, 1], [5, 1, 1], [25, 5, 1]]
+    q = deque([(0, -1, 0, *picks)])
+    
+    while q:
+        i, c, total, *pick = q.popleft()
+        if total > 0:
+            pick[c] -= 1
+        if i >= len(minerals) or sum(pick) == 0:
+            answer = min(answer, total)
+            continue
+        for a in range(3):
+            if pick[a] == 0:
+                continue
+            res = sum(scores[a][choice[minerals[b]]] for b in range(i, i + 5) if b < len(minerals))
+            q.append((i + 5, a, total + res, *pick))
+
+    return answer
