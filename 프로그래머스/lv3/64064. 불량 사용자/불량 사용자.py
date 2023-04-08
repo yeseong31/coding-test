c5f7f54bb87a744ffee33752e16dd212d875219b
예성 <66625672+yeseong31@users.noreply.github.com>
2023-04-08 22:11:30 +0900

    [level 3] Title: 불량 사용자, Time: 0.26 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -1,17 +1,21 @@
-import re
+from re import fullmatch
+
+
+def dfs(idx, visit, answer, user_id, ban_patterns):
+    if idx == len(ban_patterns):
+        answer.add(visit)
+        return
+    # 다음 제재 아이디와 다음 방문 상태를 점검하여 규칙이 서로 일치하는지 확인
+    for i in range(len(user_id)):
+        # 방문 여부 확인: and 연산
+        if visit & (1 << i) > 0 or not fullmatch(ban_patterns[idx], user_id[i]):
+            continue
+        # 다음 위치 생성: or 연산
+        dfs(idx + 1, visit | (1 << i), answer, user_id, ban_patterns)
 
 
 def solution(user_id, banned_id):
-    def search(index: int, visited: int, answer: set, patterns: list) -> None:
-        if index == len(patterns):
-            answer.add(visited)
-            return
-        for i in range(len(user_id)):
-            if visited & (1 << i) > 0 or not re.fullmatch(patterns[index], user_id[i]):
-                continue
-            search(index + 1, visited | (1 << i), answer, patterns)
-
     answer = set()
-    patterns = [x.replace('*', '.') for x in banned_id]
-    search(0, 0, answer, patterns)
+    ban_patterns = [x.replace('*', '.') for x in banned_id]
+    dfs(0, 0, answer, user_id, ban_patterns)
     return len(answer)
