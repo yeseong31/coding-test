ab6899ea719cbaff0a0d032b5923d60a1c2bc01b
예성 <66625672+yeseong31@users.noreply.github.com>
2023-04-08 21:25:50 +0900

    [level 4] Title: 호텔 방 배정, Time: 529.54 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.py
+++ 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.py
@@ -1,15 +1,21 @@
 import sys
+from collections import defaultdict
+
 sys.setrecursionlimit(2000)
 
 
+def find_empty_room(n, rooms):
+    if not rooms[n]:
+        rooms[n] = n + 1
+        return n
+    next_n = find_empty_room(rooms[n], rooms)
+    rooms[n] = next_n + 1
+    return next_n
+
+
 def solution(k, room_number):
-    def find_empty_room(n: int, rooms: dict):
-        if n not in rooms:
-            rooms[n] = n + 1
-            return n
-        
-        rooms[n] = find_empty_room(rooms[n], rooms) + 1
-        return rooms[n] - 1
-    
-    rooms = dict()
-    return [find_empty_room(x, rooms) for x in room_number]
+    answer = []
+    rooms = defaultdict(int)
+    for n in room_number:
+        check_in = find_empty_room(n, rooms)
+    return list(rooms)
