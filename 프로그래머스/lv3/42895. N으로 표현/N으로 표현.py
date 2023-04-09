86d83c26e3c1e6522637946e0eb0098292058c87
예성 <66625672+yeseong31@users.noreply.github.com>
2023-04-09 17:06:45 +0900

    [level 3] Title: N으로 표현, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -1,28 +1,21 @@
 def solution(N, number):
     answer = 0
-    # 연산 결과 저장 (N은 최대 8번 사용)
-    dp = [set() for _ in range(9)]
+    dp = [set() for i in range(9)]
     
-    # N 사용
     for i in range(1, 9):
-        # 연산 결과에 N을 i번 반복한 숫자 저장
-        v = dp[i]
-        v.add(int(str(N) * i))
-        
-        # 연산 횟수 지정
+        case = dp[i]
+        case.add(int(str(N) * i))
+        # 이전에 만들었던 결과들을 전부 순회
         for j in range(1, i):
-            # 연산을 수행할 열 선택
             for k in dp[j]:
-                # 이전 연산 결과에서부터 사칙 연산 수행
                 for l in dp[i - j]:
-                    v.add(k + l)
-                    v.add(k - l)
-                    v.add(k * l)
-                    if k != 0 and l != 0:
-                        v.add(k // l)
-                        
-        # 연산 결과 내 결괏값이 있다면 return
-        if number in v:
-            return i
+                    case.add(k + l)
+                    case.add(k - l)
+                    case.add(k * l)
+                    if l != 0 and k != 0:
+                        case.add(k // l)
         
+        if number in case:
+            return i
+    
     return -1
