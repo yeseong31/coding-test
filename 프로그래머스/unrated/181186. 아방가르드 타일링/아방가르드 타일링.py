ba1a7c220331fa418bbf31523a85a6e7f0bbe1ac
예성 <66625672+yeseong31@users.noreply.github.com>
2023-04-13 21:59:14 +0900

    [unrated] Title: 아방가르드 타일링, Time: 104.06 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.py
+++ 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.py
@@ -5,16 +5,14 @@ def solution(n):
         return dp[n]
     
     check = [2, 4, 2]
-    tmp = [dp[1] * 2, dp[2] * 2 + dp[1] * 2, dp[3] * 2 + dp[2] * 2 + dp[1] * 4]
+    sums = [dp[1] * 2, dp[2] * 2 + dp[1] * 2, dp[3] * 2 + dp[2] * 2 + dp[1] * 4]
     
-    for i in range(5, 8):
-        dp.append(check[i - 5] + dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + tmp[i - 5])
-    
-    for i in range(8, n + 1):
-        result = check[(i + 1) % 3] + dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + tmp[(i + 1) % 3]
-        k = dp[i - 4] * 2 + dp[i - 5] * 2 + dp[i - 6] * 4
-        result += k
-        tmp[(i + 1) % 3] += k
+    for i in range(5, n + 1):
+        result = check[(i + 1) % 3] + dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + sums[(i + 1) % 3]
+        if i >= 8:
+            tmp = dp[i - 4] * 2 + dp[i - 5] * 2 + dp[i - 6] * 4
+            result += tmp
+            sums[(i + 1) % 3] += tmp
         dp.append(result % 1000000007)
 
     return dp[n]
