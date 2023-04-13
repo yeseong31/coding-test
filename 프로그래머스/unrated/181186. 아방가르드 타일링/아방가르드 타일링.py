15ee17468fb3400b68011536c2cdd42276297799
예성 <66625672+yeseong31@users.noreply.github.com>
2023-04-13 21:56:23 +0900

    [unrated] Title: 아방가르드 타일링, Time: 91.90 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.py
+++ 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.py
@@ -6,19 +6,15 @@ def solution(n):
     
     check = [2, 4, 2]
     tmp = [dp[1] * 2, dp[2] * 2 + dp[1] * 2, dp[3] * 2 + dp[2] * 2 + dp[1] * 4]
-    p = 0
     
     for i in range(5, 8):
-        result = check[p] + dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + tmp[p]
-        dp.append(result % 1000000007)
-        p = (p + 1) % 3
+        dp.append(check[i - 5] + dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + tmp[i - 5])
     
     for i in range(8, n + 1):
-        result = check[p] + dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + tmp[p]
+        result = check[(i + 1) % 3] + dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + tmp[(i + 1) % 3]
         k = dp[i - 4] * 2 + dp[i - 5] * 2 + dp[i - 6] * 4
         result += k
-        tmp[p] += k
+        tmp[(i + 1) % 3] += k
         dp.append(result % 1000000007)
-        p = (p + 1) % 3
 
     return dp[n]
