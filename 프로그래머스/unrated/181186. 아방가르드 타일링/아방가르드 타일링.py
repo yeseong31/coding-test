eef0a7f6a438c20af1e47e9eb6bb47fd23fe7e4a
예성 <66625672+yeseong31@users.noreply.github.com>
2023-04-13 22:03:14 +0900

    [unrated] Title: 아방가르드 타일링, Time: 81.51 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.py
+++ 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.py
@@ -4,15 +4,17 @@ def solution(n):
     if n <= 4:
         return dp[n]
     
+    p = 0
     check = [2, 4, 2]
     sums = [dp[1] * 2, dp[2] * 2 + dp[1] * 2, dp[3] * 2 + dp[2] * 2 + dp[1] * 4]
-    
-    for i in range(5, n + 1):
-        result = check[(i + 1) % 3] + dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + sums[(i + 1) % 3]
-        if i >= 8:
-            tmp = dp[i - 4] * 2 + dp[i - 5] * 2 + dp[i - 6] * 4
-            result += tmp
-            sums[(i + 1) % 3] += tmp
-        dp.append(result % 1000000007)
+
+    for i in range(5, 8):
+        dp.append(check[p] + dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + sums[p])
+        p = (p + 1) % 3
+
+    for i in range(8, n + 1):
+        sums[p] += dp[i - 4] * 2 + dp[i - 5] * 2 + dp[i - 6] * 4
+        dp.append((check[p] + dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + sums[p]) % 1000000007)
+        p = (p + 1) % 3
 
     return dp[n]
