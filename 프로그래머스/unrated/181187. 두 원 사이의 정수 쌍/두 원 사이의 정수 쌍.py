52580d86fe31acdf0fea1cddc76e6eb3804af948
예성 <66625672+yeseong31@users.noreply.github.com>
2023-04-14 14:43:54 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 699.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -3,10 +3,10 @@ def solution(r1, r2):
     for w in range(1, r2):
         h2 = (r2 ** 2 - w ** 2) ** 0.5
         if w >= r1:
-            answer += 4 * (int(h2) + 1)
+            answer += int(h2) + 1
             continue
         h1 = (r1 ** 2 - w ** 2) ** 0.5
-        answer += 4 * (int(h2) - int(h1))
+        answer += int(h2) - int(h1)
         if h1 == int(h1):
-            answer += 4
-    return answer + 4
+            answer += 1
+    return 4 * (answer + 1)
