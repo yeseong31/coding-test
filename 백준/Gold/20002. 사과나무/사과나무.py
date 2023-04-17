f7af3cea9bb57583b5df034cf7fbe39761fdda68
예성 <66625672+yeseong31@users.noreply.github.com>
2023-04-17 20:46:57 +0900

    [Gold V] Title: 사과나무, Time: 240 ms, Memory: 116724 KB -BaekjoonHub

--- 백준/Gold/20002. 사과나무/사과나무.py
+++ 백준/Gold/20002. 사과나무/사과나무.py
@@ -1,17 +1,14 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
 dp = [[0] * (n + 1) for _ in range(n + 1)]
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + board[i - 1][j - 1]
+for i in range(n):
+    for j in range(n):
+        dp[i + 1][j + 1] = dp[i][j + 1] + dp[i + 1][j] - dp[i][j] + board[i][j]
 
-answer = dp[1][1]
-for k in range(n):
-    for i in range(1, n + 1 - k):
-        for j in range(1, n + 1 - k):
-            answer = max(answer, dp[i + k][j + k] - dp[i + k][j - 1] - dp[i - 1][j + k] + dp[i - 1][j - 1])
+answer = -1001
+for k in range(n, 0, -1):
+    for i in range(n, k - 1, -1):
+        for j in range(n, k - 1, -1):
+            answer = max(answer, dp[i][j] + dp[i - k][j - k] - dp[i][j - k] - dp[i - k][j])
 print(answer)
