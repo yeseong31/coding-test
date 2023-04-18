51c50d8e35a3d6f38c0ecfc1cd6da509d0e3d990
예성 <66625672+yeseong31@users.noreply.github.com>
2023-04-18 21:32:46 +0900

    [unrated] Title: 유사 칸토어 비트열, Time: 17.80 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
+++ 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -1,14 +1,14 @@
-def check(x, n):
+def check(x, n, target):
     while n >= 1:
-        div, mod = divmod(x, 5)
-        if div == 1:
-            return '11011'[mod]
+        x, mod = divmod(x, 5)
         if mod == 2:
-            return '0'
-        x = div
+            return False
+        if x == 1:
+            return target[mod]
         n -= 1
-    return '1'
+    return True
 
 
 def solution(n, l, r):
-    return [check(x, n) for x in range(l - 1, r)].count('1')
+    target = (1, 1, 0, 1, 1)
+    return sum(check(x, n, target) for x in range(l - 1, r))
