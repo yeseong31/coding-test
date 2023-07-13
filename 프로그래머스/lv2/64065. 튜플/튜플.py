75e564f737aa004a61bbb7e45bba42818fc96dcc
예성 <66625672+yeseong31@users.noreply.github.com>
2023-07-13 21:22:44 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = dict()
+    s = sorted(s[2:-2].split("},{"), key=len)
+    for target in s:
+        for v in target.split(','):
+            v = int(v)
+            if v not in answer:
+                answer[v] = 1
+    return list(answer)
+    
