74793cad98755630cd082b815bde3d923cff6334
예성 <66625672+yeseong31@users.noreply.github.com>
2023-07-13 18:31:59 +0900

    [level 2] Title: 삼각 달팽이, Time: 130.81 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,13 +1,15 @@
 def solution(n):
     board = [[0] * i for i in range(1, n + 1)]
-    dx, dy = (1, 0, -1), (0, 1, -1)
     
-    x, y = -1, 0
-    step = 1
+    x, y, d = -1, 0, 0
+    dx, dy = (1, 0, -1), (0, 1, -1)
+    cnt = 1
+    
     for i in range(n):
         for j in range(i, n):
-            nx, ny = x + dx[i % 3], y + dy[i % 3]
-            board[nx][ny] = step
-            x, y, step = nx, ny, step + 1
-    
-    return [r for row in board for r in row]
+            d = i % 3
+            x, y = x + dx[d], y + dy[d]
+            board[x][y] = cnt
+            cnt += 1
+
+    return [i for j in board for i in j]
