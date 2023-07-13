ad210ad9b600ab31a1e26c7a69cde8b14066c0c4
예성 <66625672+yeseong31@users.noreply.github.com>
2023-07-13 23:10:56 +0900

    [level 2] Title: 문자열 압축, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -2,22 +2,32 @@ def solution(s):
     answer = len(s)
     
     for n in range(1, len(s) // 2 + 1):
-        i = 0
-        prev = ''
+        result = 0
+        # 중복 카운트
         count = 1
-        result = ''
-        
-        while i < len(s):
-            target = s[i:i + n]
-            if target == prev:
+        # 이전 문자
+        prev = ''
+
+        # 길이 n씩 자르면서 확인
+        for i in range(0, len(s) + 1, n):
+            word = s[i:i+n]
+            # 이전에 등장한 문자라면 중복 카운트
+            if prev == word:
                 count += 1
+            # 새로운 문자라면 result에 반영
             elif prev != '':
-                result += f'{count}{prev}' if count != 1 else prev
+                if count != 1:
+                    result += len(str(count))
+                result += len(prev)
                 count = 1
-            prev = target
-            i += n
             
-        result += f'{count}{prev}' if count != 1 else prev
-        answer = min(answer, len(result))
+            prev = word
+            i += n
         
+        if count != 1:
+            result += len(str(count))
+        
+        result += len(prev)
+        answer = min(answer, result)
+    
     return answer
