383e0dc4d5c2a589bad2181ec21c988b8a0635e4
예성 <66625672+yeseong31@users.noreply.github.com>
2023-07-15 14:50:43 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.30 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -2,47 +2,58 @@ import re
 from itertools import permutations
 
 
-def calc(target):
-    stack = []
-    for t in target:
-        if t.isdigit():
-            stack.append(int(t))
-            continue
-        n2, n1 = stack.pop(), stack.pop()
-        if t == '+':
-            stack.append(n1 + n2)
-        elif t == '-':
-            stack.append(n1 - n2)
-        else:
-            stack.append(n1 * n2)
-    return stack.pop()
-
-def to_postfix(tokens, priority):
-    stack, result = [], []
+def to_postfix(token_list, ops):
+    result, stack = [], []
+    priorities = {v: i for i, v in enumerate(ops)}
     
-    for token in tokens:
-        if token.isdigit():
-            result.append(token)
+    for t in token_list:
+        if t.isdigit():
+            result.append(t)
             continue
+        # 연산자 스택이 비어 있다면 스택에 추가
         if not stack:
-            stack.append(token)
+            stack.append(t)
             continue
-        # 우선순위가 낮다면 더 이상 낮아지지 않을 때까지 스택 비우기
+        # 연산자 스택에 쌓인 연산자와 현재 연산자의 우선순위 비교
         while stack:
-            if priority[token] < priority[stack[-1]]:
+            if priorities[t] < priorities[stack[-1]]:
                 break
             result.append(stack.pop())
-        stack.append(token)
-
+        # 현재 연산자를 스택에 추가
+        stack.append(t)
+    # 연산자 스택에 남아 있는 연산자들을 result에 추가
     while stack:
         result.append(stack.pop())
-    return calc(result)
+    return result
+
+
+def calcul_postfix(exp):
+    result, stack = 0, []
+    
+    for x in exp:
+        if x.isdigit():
+            stack.append(int(x))
+            continue
+        # 연산자라면 연산 수행 후 스택에 추가
+        num2, num1 = stack.pop(), stack.pop()
+        if x == '+':
+            stack.append(num1 + num2)
+        elif x == '-':
+            stack.append(num1 - num2)
+        elif x == '*':
+            stack.append(num1 * num2)
+    
+    return abs(stack.pop())
 
 
 def solution(expression):
     answer = 0
     tokens = re.split(r'([-+*])|\s+', expression)
-    for perm in permutations(['+', '-', '*'], 3):
-        priority = {v: i for i, v in enumerate(perm)}
-        answer = max(answer, abs(to_postfix(tokens, priority)))
+    
+    for p in permutations(('+', '-', '*'), 3):
+        # 후위 연산자로 변환
+        post_exp = to_postfix(tokens, p)
+        # 수식 계산 후 최댓값 저장
+        answer = max(answer, calcul_postfix(post_exp))
+    
     return answer
