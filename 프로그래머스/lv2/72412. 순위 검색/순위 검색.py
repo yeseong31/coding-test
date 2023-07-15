aeb8566a8bd7af84934427f8729b4897f228a07e
예성 <66625672+yeseong31@users.noreply.github.com>
2023-07-15 17:29:51 +0900

    [level 2] Title: 순위 검색, Time: 599.36 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -1,25 +1,33 @@
-from collections import defaultdict
 from itertools import combinations
+from collections import defaultdict
 from bisect import bisect_left
 
 
-def solution(info, query):    
+def solution(info, query):
     answer = []
-    applicants = defaultdict(list)
+    applicant = defaultdict(list)
     
-    for inf in info:
-        *target, score = inf.split()
-        applicants[''.join(target)].append(int(score))
+    # 지원자 정보
+    for target in info:
+        *q, score = target.split()
+        # 주어진 정보를 키로 설정, 점수 추가
+        applicant[''.join(q)].append(int(score))
+        # 가능한 16가지 조합에 대한 점수 추가
         for x in range(4):
-            for case in combinations(target, x):
-                applicants[''.join(case)].append(int(score))
-    
-    for i in applicants:
-        applicants[i].sort()
+            for comb in combinations(q, x):
+                applicant[''.join(comb)].append(int(score))
+        
+    # 점수 정렬
+    for k in applicant:
+        applicant[k].sort()
     
     for q in query:
+        # 쿼리와 점수 분리
         *q, score = q.split()
-        q = ''.join(q)
-        key = q.replace('and', '').replace(' ', '').replace('-', '')
-        answer.append(len(applicants[key]) - bisect_left(applicants[key], int(score)))
+        # 쿼리 전처리
+        q = ''.join(q).replace('-', '').replace(' ', '').replace('and', '')
+        # score를 넘는 지원자 수 카운트
+        count = len(applicant[q]) - bisect_left(applicant[q], int(score))
+        answer.append(count)
+    
     return answer
