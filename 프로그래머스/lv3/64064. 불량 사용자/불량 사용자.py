38ba594d2b79107ca4c424ece4ad4bf018e48f05
예성 <66625672+yeseong31@users.noreply.github.com>
2023-07-15 13:32:16 +0900

    [level 3] Title: 불량 사용자, Time: 2.20 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -1,21 +1,16 @@
 from re import fullmatch
-
-
-def dfs(idx, visit, answer, user_id, ban_patterns):
-    if idx == len(ban_patterns):
-        answer.add(visit)
-        return
-    # 다음 제재 아이디와 다음 방문 상태를 점검하여 규칙이 서로 일치하는지 확인
-    for i in range(len(user_id)):
-        # 방문 여부 확인: and 연산
-        if visit & (1 << i) > 0 or not fullmatch(ban_patterns[idx], user_id[i]):
-            continue
-        # 다음 위치 생성: or 연산
-        dfs(idx + 1, visit | (1 << i), answer, user_id, ban_patterns)
+from itertools import permutations
 
 
 def solution(user_id, banned_id):
     answer = set()
-    ban_patterns = [x.replace('*', '.') for x in banned_id]
-    dfs(0, 0, answer, user_id, ban_patterns)
+    bids = ' '.join(banned_id).replace('*', '.')
+    
+    # 응모자 순열
+    for uids in permutations(user_id, len(banned_id)):
+        # 응모자와 불량 사용자 매칭
+        if fullmatch(bids, ' '.join(uids)):
+            # 제재 아이디 목록 내용이 동일하면 같음 -> 정렬 사용
+            answer.add(' '.join(sorted(uids)))
+        
     return len(answer)
