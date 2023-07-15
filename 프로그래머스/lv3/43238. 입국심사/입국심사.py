1f18b0fe7a373baf926d718b06daf931e0077f82
예성 <66625672+yeseong31@users.noreply.github.com>
2023-07-15 16:56:25 +0900

    [level 3] Title: 입국심사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -1,18 +1,20 @@
-def binary_search(n, times):
-    start, end = 1, max(times) * n
-    while start <= end:
-        mid = (start + end) // 2
-        check = 0
-        for t in times:
-            check += mid // t
-            if check >= n:
-                break
-        if check >= n:
-            result = mid
-            end = mid - 1
-        else:
-            start = mid + 1
-    return result
-
 def solution(n, times):
-    return binary_search(n, times)
+    answer = 0
+    left, right = 0, max(times) * n
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        cnt = 0
+        for t in times:
+            cnt += mid // t
+            if cnt >= n:
+                break
+                
+        if cnt >= n:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+        
+    return answer
