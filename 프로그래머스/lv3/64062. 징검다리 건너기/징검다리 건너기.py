507a60601ab3983674644e29f06240506bc8c844
예성 <66625672+yeseong31@users.noreply.github.com>
2023-07-16 23:18:02 +0900

    [level 3] Title: 징검다리 건너기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,24 +1,30 @@
 def solution(stones, k):
-    answer = 0
-    start, end = 0, max(stones)
-    
-    while start <= end:
-        mid = (start + end) // 2
-        result = True
-        count = 0
+    def available(n):
+        jump = 0
         
         for stone in stones:
-            if stone < mid:
-                count += 1
-                if count >= k:
-                    result = False
+            # n명이 밟을 수 없다면 건너뛰기
+            if stone - n < 0:
+                jump += 1
+                if jump >= k:
+                    return False
+            # 건너뛰어도 되지 않다면 건너뛴 횟수 초기화
             else:
-                count = 0
+                jump = 0
         
-        if result:
+        return True
+    
+    
+    answer = 0
+    left, right = 0, max(stones)
+    
+    while left <= right:
+        mid = (left + right) // 2
+        # 현재 인원으로 징검다리를 건널 수 있는지 확인
+        if available(mid):
             answer = max(answer, mid)
-            start = mid + 1
+            left = mid + 1
         else:
-            end = mid - 1
+            right = mid - 1
         
     return answer
