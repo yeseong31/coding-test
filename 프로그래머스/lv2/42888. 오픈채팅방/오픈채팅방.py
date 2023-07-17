da5be6cb792994e6412374f1230242c4e036d050
예성 <66625672+yeseong31@users.noreply.github.com>
2023-07-17 19:26:36 +0900

    [level 2] Title: 오픈채팅방, Time: 87.52 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -2,16 +2,18 @@ from collections import defaultdict
 
 
 def solution(record):
-    result = []
     users = defaultdict(str)
+    events = []
+    txt = ('님이 들어왔습니다.', '님이 나갔습니다.')
     
     for r in record:
-        _type, *info = r.split()
-        if _type == 'Leave':
-            result.append((info[0], '님이 나갔습니다.'))
-            continue
-        users[info[0]] = info[1]
-        if _type == 'Enter':
-            result.append((info[0], '님이 들어왔습니다.'))
-            
-    return [f'{users[uid]}{txt}' for uid, txt in result]
+        event, *userinfo = r.split()
+        if event == 'Enter':
+            events.append((userinfo[0], 0))
+            users[userinfo[0]] = userinfo[1]
+        elif event == 'Leave':
+            events.append((userinfo[0], 1))
+        else:
+            users[userinfo[0]] = userinfo[1]
+    
+    return [f'{users[uid]}{txt[i]}' for uid, i in events]
