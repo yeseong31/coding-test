f7775a4fb28bc0f9a53771d32de2220ee3a76119
예성 <66625672+yeseong31@users.noreply.github.com>
2023-07-31 09:41:47 +0900

    [level 3] Title: 순위, Time: 5.71 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.py
+++ 프로그래머스/lv3/49191. 순위/순위.py
@@ -5,18 +5,19 @@ def solution(n, results):
     answer = 0
     win, lose = defaultdict(set), defaultdict(set)
     
-    for w, l in results:
-        win[w].add(l)
-        lose[l].add(w)
-
+    for a, b in results:
+        win[a].add(b)
+        lose[b].add(a)
+    
+    # A 선수는 B 선수를 항상 이김 
     for i in range(1, n + 1):
         for w in win[i]:
             lose[w].update(lose[i])
         for l in lose[i]:
             win[l].update(win[i])
     
+    # 이긴 경기의 수와 진 경기의 수를 합했을 때 n - 1이 나와야 함
     for i in range(1, n + 1):
         if len(win[i]) + len(lose[i]) == n - 1:
             answer += 1
-    
     return answer
