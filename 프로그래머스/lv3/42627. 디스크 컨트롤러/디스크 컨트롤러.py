b7af67f978168f975289c9ae931bfbef58219233
예성 <66625672+yeseong31@users.noreply.github.com>
2023-08-31 10:11:44 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -3,28 +3,24 @@ from heapq import heappush, heappop
 
 def solution(jobs):
     answer = 0
-    # 우선순위 큐
+    length = len(jobs)
     q = []
-    # 현재 시간
+
+    jobs.sort(key=lambda x: -x[0])
+
     t = 0
-    # 작업 시작 시간
-    s = -1
-    # 처리한 프로세스
-    p = 0
-    
-    while p < len(jobs):
-        # 요청 시간이 작업 시작 시간과 현재 시간 사이에 있을 때 큐에 삽입
-        for st, it in jobs:
-            if s < st <= t:
-                heappush(q, (it, st))
-        # 큐에 프로세스가 있다면
-        if q:
-            it, st = heappop(q)
-            s = t  # 작업 시작 시간 갱신
-            t += it  # 현재 시간 갱신
-            answer += (t - st)  # 요청~종료 시간 계산
-            p += 1
+    while jobs or q:
+
+        while jobs and jobs[-1][0] <= t:
+            target = jobs.pop()
+            heappush(q, (target[1], target[0]))
+
+        if q and t >= q[0][1]:
+            work_time, start_time = heappop(q)
+            end_time = t + work_time
+            answer += end_time - start_time
+            t = end_time
         else:
             t += 1
-    
-    return answer // len(jobs)
+
+    return answer // length
