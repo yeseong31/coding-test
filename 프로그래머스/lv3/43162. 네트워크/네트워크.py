0346cd173455b2f04560c800687bacd579b1f32d
예성 <66625672+yeseong31@users.noreply.github.com>
2023-09-05 13:52:04 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,15 +1,28 @@
-def dfs(n, idx, computers, visited):
-    visited[idx] = True
-    for i in range(n):
-        if not visited[i] and computers[idx][i] == 1:
-            dfs(n, i, computers, visited)
+def find_parent(parent, x):
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
 
 def solution(n, computers):
-    answer = 0
-    visited = [False] * n
+    parent = list(range(n))
+    
+    for i in range(1, n):
+        for j in range(i):
+            if i == j or computers[i][j] == 0:
+                continue
+            union_parent(parent, i, j)
+    
     for i in range(n):
-        if not visited[i]:
-            dfs(n, i, computers, visited)
-            answer += 1
-    return answer
+        parent[i] = find_parent(parent, i)
+    
+    return len(set(parent))
