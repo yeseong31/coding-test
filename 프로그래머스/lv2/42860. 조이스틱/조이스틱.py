477ece5dcd66add6aa24f5e0bba193442b825d8b
예성 <66625672+yeseong31@users.noreply.github.com>
2023-09-06 19:38:29 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -1,17 +1,16 @@
 def solution(name):
-    count = 0
+    def to_A(i):
+        return min(ord(name[i]) - ord('A'), ord('Z') - ord(name[i]) + 1)
+    
     n = len(name)
-    _min = n - 1
+    target = ['A'] * n
+    count = sum(to_A(i) for i in range(n))
+    min_step = n - 1
     
-    for i, v in enumerate(name):
-        # 'A'를 문자 v로 바꾸고
-        count += min(ord(v) - ord('A'), ord('Z') - ord(v) + 1)
-        # (왼쪽->오른쪽) 방향으로 'A'가 아닌 문자의 위치를 구하고
-        idx = i + 1
-        while idx < n and name[idx] == 'A':
-            idx += 1
-        # 최소 거리를 계산하여 min_move 갱신
-        _min = min(_min, 2 * i + (n - idx), 2 * (n - idx) + i)
+    for i, c in enumerate(name):
+        x = i + 1
+        while x < n and name[x] == 'A':
+            x += 1
+        min_step = min(min_step, 2 * i + (n - x), i + 2 * (n - x))
     
-    # 문자 변환 + 이동 거리 반환
-    return count + _min
+    return min_step + count
