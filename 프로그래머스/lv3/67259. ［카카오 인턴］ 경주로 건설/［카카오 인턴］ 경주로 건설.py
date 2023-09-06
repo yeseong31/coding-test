2e6b68bdb380b4da4ada364ab6d2bb0e64bfd6cf
예성 <66625672+yeseong31@users.noreply.github.com>
2023-09-06 13:55:38 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.12 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,32 +1,30 @@
 from collections import deque
+from copy import deepcopy
 
 
 def solution(board):
-    def bfs(x, y, d, c):
-        q = deque([(x, y, d, c)])
-        graph = [[0] * n for _ in range(n)]
+    def bfs(a, b, d, c):
+        q = deque([(a, b, d, c)])
         visited = [[False] * n for _ in range(n)]
+        graph = deepcopy(board)
         
-        for i in range(n):
-            for j in range(n):
-                if board[i][j] == 1:
-                    graph[i][j] = -1
-
         while q:
             x, y, d, c = q.popleft()
-            for i in range(4):
-                nx, ny = x + dx[i], y + dy[i]
+            
+            for nd in range(4):
+                nx, ny = x + dx[nd], y + dy[nd]
                 if nx < 0 or nx >= n or ny < 0 or ny >= n or graph[nx][ny] == -1:
                     continue
-                if i == d:
-                    nc = c + 100
-                else:
-                    nc = c + 600
-                if graph[nx][ny] == 0 or graph[nx][ny] > nc:
-                    graph[nx][ny] = nc            
-                    q.append((nx, ny, i, nc))
+                
+                nc = c + (100 if d == nd else 600)
+                if 0 < graph[nx][ny] <= nc:
+                    continue
+                
+                graph[nx][ny] = nc
+                q.append((nx, ny, nd, nc))
+    
         return graph[n - 1][n - 1]
     
     n = len(board)
-    dx, dy = (1, 0, -1, 0), (0, 1, 0, -1)
+    dx, dy = (0, 1, 0, -1), (1, 0, -1 , 0)
     return min(bfs(0, 0, 0, 0), bfs(0, 0, 1, 0))
