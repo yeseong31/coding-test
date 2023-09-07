7b1eab32445ef19ca384e50745e1546ab582a5cc
예성 <66625672+yeseong31@users.noreply.github.com>
2023-09-07 10:05:04 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,20 +1,21 @@
-def is_prime_number(n):
-    if n < 2:
-        return 0
-    for i in range(3, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return 0
-    return 1
-
-
-def change(n, k):
-    result = []
-    while n > 0:
-        div, mod = divmod(n, k)
-        result.append(str(mod))
-        n = div
-    return ''.join(result[::-1])
-
-
 def solution(n, k):
-    return sum(1 for n in change(n, k).split('0') if n != '' and is_prime_number(int(n)))
+    def is_prime_number(x):
+        for i in range(2, int(x ** 0.5) + 1):
+            if x % i == 0:
+                return False
+        return not x < 2
+    
+    def change_digit(x):
+        result = []
+        while x != 0:
+            x, mod = divmod(x, k)
+            result.append(str(mod))
+        return ''.join(result[::-1])
+    
+    answer = 0
+    for target in change_digit(n).split('0'):
+        if not target.isdigit():
+            continue
+        if is_prime_number(int(target)):
+            answer += 1
+    return answer
