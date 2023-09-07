82499a2565a9ec30d1e7551f2595a9662c3f793b
예성 <66625672+yeseong31@users.noreply.github.com>
2023-09-08 00:01:00 +0900

    [level 2] Title: 양궁대회, Time: 455.58 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,28 +1,38 @@
-from itertools import combinations_with_replacement as cwr
+from itertools import combinations_with_replacement
 
+
+def get_ryan_board(scores):
+    result = [0] * 11
+    for score in scores:
+        result[10 - score] += 1
+    return result
+
+
+def get_scores(apeach_board, ryan_board):
+    apeach_score = ryan_score = 0
+    for i, (a, b) in enumerate(zip(apeach_board, ryan_board)):
+        score = 10 - i
+        if a == b == 0:
+            continue
+        if a >= b:
+            apeach_score += score
+        else:
+            ryan_score += score
+    return apeach_score, ryan_score
 
 def solution(n, info):
     answer = [-1]
-    gap = -1
-
-    for case in cwr(range(11), n):
-        score = [0] * 11
-        for i in case:
-            score[10 - i] += 1
-
-        apeach = ryan = 0
-        for i in range(11):
-            if info[i] == score[i] == 0:
-                continue
-            elif info[i] < score[i]:
-                ryan += 10 - i
-            else:
-                apeach += 10 - i
-
-        if apeach >= ryan:
+    max_gap = -1
+    
+    for shoot in combinations_with_replacement(range(11), n):
+        ryan_board = get_ryan_board(shoot)
+        apeach_score, ryan_score = get_scores(info, ryan_board)
+    
+        if ryan_score <= apeach_score:
             continue
-        if ryan - apeach > gap:
-            gap = ryan - apeach
-            answer = score
+        
+        if ryan_score - apeach_score > max_gap:
+            max_gap = ryan_score - apeach_score
+            answer = ryan_board
     
     return answer
