09a3647aebba438bd22b5f108dc50abb690f79fc
예성 <66625672+yeseong31@users.noreply.github.com>
2023-09-08 11:14:19 +0900

    [level 3] Title: 양과 늑대, Time: 0.80 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -1,30 +1,20 @@
-from collections import defaultdict
-
-
 def solution(info, edges):
-    def dfs(cur, sheep, wolf, path):
-        # XOR 연산으로 if문 없이 양, 늑대 정보 판단
-        sheep += info[cur] ^ 1
-        wolf += info[cur]
-        # DFS 진행 조건
+    def dfs(sheep, wolf):
         if sheep <= wolf:
             return
+        
         answer.append(sheep)
-        # 다음 탐색 범위 계산
-        # 이진 트리에서 나아갈 수 있는 모든 길에 대해 재귀 호출
-        for p in path:
-            tmp = set(graph.get(p, []))
-            path |= tmp
-            path -= set([p])
-            dfs(p, sheep, wolf, path)
-            path |= set([p])
-            path -= tmp
-    
+        for parent, child in edges:
+            check = (info[child] == 1)
+            if visited[parent] and not visited[child]:
+                visited[child] = True
+                dfs(sheep + (not check), wolf + check)
+                visited[child] = False
     
     answer = []
-    graph = defaultdict(list)
-    for p, c in edges:
-        graph[p].append(c)
-        
-    dfs(0, 0, 0, set(graph.get(0)))
+    visited = [False] * len(info)
+    visited[0] = True
+    
+    dfs(1, 0)
+    
     return max(answer)
