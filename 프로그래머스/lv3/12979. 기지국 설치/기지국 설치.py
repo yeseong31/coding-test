64e16049950d0f29c724a5d87bee8ec5baac9323
예성 <66625672+yeseong31@users.noreply.github.com>
2023-09-19 15:37:51 +0900

    [level 3] Title: 기지국 설치, Time: 2.89 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.py
@@ -1,15 +1,14 @@
-import math
+from math import ceil
 
 
 def solution(n, stations, w):
     answer = 0
     area = 2 * w + 1
-
-    while stations and n > 0:
-        if stations[-1] - w <= n <= stations[-1] + w:
-            n = stations.pop() - w - 1
-        if stations:
-            answer += math.ceil((n - (stations[-1] + w)) / area)
-            n = stations[-1]
-
-    return answer + math.ceil(n / area)
+    
+    while n > 0 and stations:
+        distance = n - stations[-1] - w
+        answer += ceil(distance / area)
+        n = stations.pop() - w - 1
+        
+    answer += ceil(n / area)
+    return answer
