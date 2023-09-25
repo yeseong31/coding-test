131be30d079d61d4fdfbd3da615b11b206cd1a92
예성 <66625672+yeseong31@users.noreply.github.com>
2023-09-25 21:31:55 +0900

    [level 3] Title: 등산코스 정하기, Time: 835.16 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -1,42 +1,47 @@
 from heapq import heapify, heappush, heappop
 
 
+def dijkstra(start, graph, summits_set, gates_set, intensity):
+    q = []
+    intensity[start] = 0
+    heappush(q, (0, start))
+
+    while q:
+        dist, now = heappop(q)
+        if now in summits_set or dist > intensity[now]:
+            continue
+
+        for c, v in graph[now]:
+            if v in gates_set:
+                continue
+
+            cost = max(intensity[now], c)
+            if intensity[v] <= cost:
+                continue
+
+            intensity[v] = cost
+            heappush(q, (cost, v))
+
+
 def solution(n, paths, gates, summits):
-    answer = [0, 10000001]
+    _min, _max = 0, int(1e8)
     graph = [[] for _ in range(n + 1)]
+    
     for a, b, c in paths:
         graph[a].append((c, b))
         graph[b].append((c, a))
     
-    def dijkstra(start):
-        q = []
-        intensity[start] = 0
-        heappush(q, (0, start))
-
-        while q:
-            dist, now = heappop(q)
-            if now in summits_set or dist > intensity[now]:
-                continue
-
-            for c, v in graph[now]:
-                if v in gates_set:
-                    continue
-                cost = max(intensity[now], c)
-                if intensity[v] <= cost:
-                    continue
-                intensity[v] = cost
-                heappush(q, (cost, v))
-    
     summits_set = set(summits)
     gates_set = set(gates)
-    intensity = [10000001] * (n + 1)
+    
+    intensity = [int(1e8)] * (n + 1)
     for gate in gates:
-        dijkstra(gate)
+        dijkstra(gate, graph, summits_set, gates_set, intensity)
     
-    for summit in summits_set:
-        if intensity[summit] < answer[1]:
-            answer = [summit, intensity[summit]]
-        elif intensity[summit] == answer[1]:
-            answer[0] = min(summit, answer[0])
+    for s in summits_set:
+        if intensity[s] < _max:
+            _min, _max = s, intensity[s]
+        elif intensity[s] == _max:
+            _min = min(s, _min)
     
-    return answer
+    return _min, _max
