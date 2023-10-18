e81165e21f2a1fd4e0c1d0d145d92de08a807c89
예성 <66625672+yeseong31@users.noreply.github.com>
2023-10-18 22:13:13 +0900

    [Gold V] Title: 무한 수열, Time: 68 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.py
+++ 백준/Gold/1351. 무한 수열/무한 수열.py
@@ -1,13 +1,17 @@
+import sys
 from collections import defaultdict
+
+n, p, q = map(int, sys.stdin.readline().split())
+a = defaultdict(int)
+a[0] = 1
 
 
 def dfs(x):
-    if not visited[x]:
-        visited[x] = dfs(x // p) + dfs(x // q)
-    return visited[x]
+    if a[x] != 0:
+        return a[x]
+
+    a[x] = dfs(x // p) + dfs(x // q)
+    return a[x]
 
 
-n, p, q = map(int, input().split())
-visited = defaultdict(int)
-visited[0] = 1
 print(dfs(n))
