2979f5f010efd7be55062a56e09f8f4e4241dd8d
예성 <66625672+yeseong31@users.noreply.github.com>
2023-11-01 22:27:07 +0900

    [level 3] Title: 인사고과, Time: 180.98 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.py
+++ 프로그래머스/3/152995. 인사고과/인사고과.py
@@ -1,19 +1,15 @@
-from collections import defaultdict
-
-
 def solution(scores):
     answer = scores[0]
     scores.sort(key=lambda x: (-x[0], x[1]))
-    
-    max_p = 0
-    rank = 1
+    _max, rank = 0, 1
     
     for w, p in scores:
         if answer[0] < w and answer[1] < p:
             return -1
-        if max_p <= p:
-            max_p = p
-            if sum(answer) < w + p:
-                rank += 1
+        if _max > p:
+            continue
+        if sum(answer) < w + p:
+            rank += 1
+        _max = p
             
     return rank
