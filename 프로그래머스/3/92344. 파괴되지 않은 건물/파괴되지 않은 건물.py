f1d4e79cfd8f23f9747e530350e4c61c73a5d9d0
예성 <66625672+yeseong31@users.noreply.github.com>
2023-11-06 23:08:00 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 605.09 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -0,0 +1,21 @@
+def solution(board, skill):
+    n, m = len(board), len(board[0])
+    checked = [[0] * (m + 1) for _ in range(n + 1)]
+    
+    for _type, *points, degree in skill:
+        degree *= -1 if _type == 1 else 1
+        x1, y1, x2, y2 = points
+        checked[x1][y1] += degree
+        checked[x2 + 1][y2 + 1] += degree
+        checked[x2 + 1][y1] -= degree
+        checked[x1][y2 + 1] -= degree
+    
+    for i in range(1, n + 1):
+        for j in range(m):
+            checked[i][j] += checked[i - 1][j]
+    
+    for i in range(n):
+        for j in range(1, m + 1):
+            checked[i][j] += checked[i][j - 1]
+    
+    return sum(True for i in range(n) for j in range(m) if board[i][j] + checked[i][j] > 0)
