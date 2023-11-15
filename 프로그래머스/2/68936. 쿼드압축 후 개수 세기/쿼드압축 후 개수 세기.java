d335b558233b34d2fa166bce3509a3dd7fe32c7f
예성 <66625672+yeseong31@users.noreply.github.com>
2023-11-15 11:11:25 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 4.84 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,65 +1,54 @@
 class Solution {
-
-    static class Counts {
-
-        private final int[][] arr;
-
-        private Counts(final int[][] arr) {
-            this.arr = arr;
-        }
-
-        public static Counts of(final int[][] arr) {
-            return new Counts(arr);
-        }
-
-        public Count dfs(final int x, final int y, final int size) {
-            int w = size / 2;
-
-            for (int i = x; i < x + size; i++) {
-                for (int j = y; j < y + size; j++) {
-                    if (arr[i][j] != arr[x][y]) {
-                        return dfs(x, y, w)
-                                .add(dfs(x + w, y, w))
-                                .add(dfs(x, y + w, w))
-                                .add(dfs(x + w, y + w, w));
-                    }
-                }
-            }
-
-            if (arr[x][y] == 0) {
-                return new Count(1, 0);
-            }
-            return new Count(0, 1);
-        }
-    }
-
+    
     static class Count {
-
+        
         private final int zero;
         private final int one;
-
-        public Count(final int zero, final int one) {
+        
+        private Count(int zero, int one) {
             this.zero = zero;
             this.one = one;
         }
-
+        
+        public static Count of(int zero, int one) {
+            return new Count(zero, one);
+        }
+        
         public int getZero() {
             return zero;
         }
-
+        
         public int getOne() {
             return one;
         }
-
+        
         public Count add(Count other) {
             return new Count(zero + other.getZero(), one + other.getOne());
         }
     }
-
-    public int[] solution(final int[][] arr) {
-        Count count =  Counts.of(arr)
-                .dfs(0, 0, arr.length);
-
+    
+    public int[] solution(int[][] arr) {
+        Count count = calculate(0, 0, arr.length, arr);
         return new int[] {count.getZero(), count.getOne()};
+    }
+    
+    public Count calculate(int sx, int sy, int size, int[][] arr) {
+        int h = size / 2;
+
+        for (int x = sx; x < sx + size; x++) {
+            for (int y = sy; y < sy + size; y++) {
+                if (arr[x][y] != arr[sx][sy]) {
+                    return calculate(sx, sy, h, arr)
+                            .add(calculate(sx, sy + h, h, arr))
+                            .add(calculate(sx + h, sy, h, arr))
+                            .add(calculate(sx + h, sy + h, h, arr));
+                }
+            }
+        }
+
+        if (arr[sx][sy] == 0) {
+            return Count.of(1, 0);
+        }
+        return Count.of(0, 1);
     }
 }
