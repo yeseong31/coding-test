0a25df42ff8e9ce52ff8441cf8459e6799e23dc1
예성 <66625672+yeseong31@users.noreply.github.com>
2023-11-22 21:14:49 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 61.42 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,16 +1,35 @@
 def solution(cap, n, deliveries, pickups):
-    answer = d = p = 0
-    point = n - 1
+    
+    def find_last_index(houses):
+        while houses and houses[-1] == 0:
+            houses.pop()
+        return len(houses) - 1
+    
+    def find_next_index(houses, index):
+        amount = 0
+        while index >= 0 and amount + houses[index] <= cap:
+            amount += houses[index]
+            houses[index] = 0
+            index -= 1
+        
+        houses[index] = houses[index] - (cap - amount)
+        return index
+    
+    def calculate_distance(index1, index2):
+        return (max(index1, index2) + 1) * 2
+    
+    
+    answer = 0
+    
+    delivery_index = find_last_index(deliveries)
+    pickup_index = find_last_index(pickups)
+    
+    answer += calculate_distance(delivery_index, pickup_index)
+    
+    while (delivery_index >= 0 or pickup_index >= 0):
+        delivery_index = find_next_index(deliveries, delivery_index)
+        pickup_index = find_next_index(pickups, pickup_index)
 
-    for i in range(n - 1, -1, -1):
-        d, p = (d + deliveries[i]), (p + pickups[i])
-
-        while d > cap or p > cap:
-            d, p = (d - cap), (p - cap)
-            answer += 2 * (point + 1)
-            point = i
-
-    if d > 0 or p > 0:
-        answer += 2 * (point + 1)
-
+        answer += calculate_distance(delivery_index, pickup_index)
+    
     return answer
