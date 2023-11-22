0d05aed1264b437fff8e2145190769de9af9002b
예성 <66625672+yeseong31@users.noreply.github.com>
2023-11-22 22:53:17 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 4.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+
+def solution(n, m, x, y, r, c, k):
+    dx, dy, dz = (1, 0, 0, -1), (0, -1, 1, 0), ('d', 'l', 'r', 'u')
+    q = deque([(x, y, '')])
+    
+    while q:
+        x, y, result = q.popleft()
+        
+        if x == r and y == c:
+            if k % 2 != len(result) % 2:
+                break
+            
+            if len(result) == k:
+                return result
+        
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx <= 0 or nx > n or ny <= 0 or ny > m or len(result) + abs(nx - r) + abs(ny - c) >= k:
+                continue
+            
+            q.append((nx, ny, result + dz[i]))
+            break
+                
+    return 'impossible'
