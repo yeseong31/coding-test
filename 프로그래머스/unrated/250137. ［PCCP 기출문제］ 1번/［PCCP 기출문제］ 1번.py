3f6342037af7f670653c9f5f951939687570b678
예성 <66625672+yeseong31@users.noreply.github.com>
2023-11-23 22:04:36 +0900

    [level unrated] Title: [PCCP 기출문제] 1번, Time: 0.24 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번/［PCCP 기출문제］ 1번.py
@@ -0,0 +1,30 @@
+def solution(bandage, health, attacks):
+    time = 0
+    index = 0
+    max_health = health
+    continuous = 0
+
+    while index < len(attacks):
+        time += 1
+
+        if attacks[index][0] == time:
+            health -= attacks[index][1]
+            index += 1
+
+            if health <= 0:
+                return -1
+
+            continuous = 0
+            continue
+
+        health += bandage[1]
+        continuous += 1
+
+        if continuous == bandage[0]:
+            health += bandage[2]
+            continuous = 0
+
+        if health > max_health:
+            health = max_health
+
+    return health
