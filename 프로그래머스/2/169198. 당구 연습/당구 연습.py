e75a97fa4a5c67394dae531810688b0a6226dfae
예성 <66625672+yeseong31@users.noreply.github.com>
2023-11-24 21:27:54 +0900

    [level 2] Title: 당구 연습, Time: 4.11 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.py
@@ -0,0 +1,17 @@
+def solution(m, n, startX, startY, balls):
+    def check_dist(x, y):
+        dx = (2 * (m - x), -2 * x, 0, 0)
+        dy = (0, 0, 2 * (n - y), -2 * y)
+        dist = int(1e6)
+        
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            ns = (abs(x - nx) ** 2) + (abs(y - ny) ** 2)
+            ne = (abs(startX - nx) ** 2) + (abs(startY - ny) ** 2)
+            
+            if not (startX == nx == x or startY == ny == y) or ns > ne:
+                dist = min(dist, ne)
+                
+        return dist
+            
+    return [check_dist(a, b) for a, b in balls]
