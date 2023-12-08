fcbe1338d66b37281b4fe2895058dbe33ccba122
예성 <66625672+yeseong31@users.noreply.github.com>
2023-12-08 21:26:23 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -0,0 +1,11 @@
+def solution(n, s):
+    if n > s:
+        return [-1]
+    
+    answer = [s // n] * n
+    
+    for i in range(s % n):
+        answer[n - 1] += 1
+        n -= 1
+
+    return answer
