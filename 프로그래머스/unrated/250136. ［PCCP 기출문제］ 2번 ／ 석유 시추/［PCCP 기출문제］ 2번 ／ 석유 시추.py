88af8fda7ed659c0a3ca9971d05189d639de8cec
예성 <66625672+yeseong31@users.noreply.github.com>
2023-12-31 19:36:04 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 61.22 ms, Memory: 13.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+def solution(land):
+    def bfs(x, y):
+        q = deque([(x, y)])
+        visited[x][y] = True
+        left, right = m, 0
+        result = 1
+
+        while q:
+            x, y = q.popleft()
+            left, right = min(y, left), max(y, right)
+
+            for i in range(4):
+                nx, ny = x + dx[i], y + dy[i]
+                
+                if nx < 0 or nx >= n or ny < 0 or ny >= m or land[nx][ny] == 0 or visited[nx][ny]:
+                    continue
+
+                result += 1
+                visited[nx][ny] = True
+                q.append((nx, ny))
+
+        for i in range(left, right + 1):
+            answer[i] += result
+
+    dx, dy = (1, 0, -1, 0), (0, 1, 0, -1)
+    n, m = len(land), len(land[0])
+    answer = [0] * m
+    visited = [[False] * m for _ in range(n)]
+
+    for j in range(m):
+        for i in range(n):
+            if land[i][j] == 1 and not visited[i][j]:
+                bfs(i, j)
+
+    return max(answer)
