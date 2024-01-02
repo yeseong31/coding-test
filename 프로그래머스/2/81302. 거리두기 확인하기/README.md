333001b8bfeb2bbe92cb57d96acc9e2565b253c2
예성 <66625672+yeseong31@users.noreply.github.com>
2024-01-02 17:10:25 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.07 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
 
 ### 성능 요약
 
-메모리: 79 MB, 시간: 0.06 ms
+메모리: 79 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 13:47:3
+2024년 1월 2일 17:10:24
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,72 +1,90 @@
 class Solution {
     
-    private static final int WAITING_ROOM_SIZE = 5;
-    private static final int NEIGHBOR_CELL_SIZE = 8;
-    
-    private static final int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
-    private static final int[] dy = {1, 1, 0, -1, -1, -1, 0, 1};
+    private static final int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    private static final int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    private static final int PLACE_SIZE = 5;
+    private static final int CHECK_AREA = 8;
     
     public int[] solution(String[][] places) {
-        
         int[] answer = new int[places.length];
         
-        for (int i = 0; i < WAITING_ROOM_SIZE; i++) {
-            
-            String[] place = places[i];
-            char[][] room = new char[place.length][];
-            
-            for (int j = 0; j < WAITING_ROOM_SIZE; j++) {
-                room[j] = place[j].toCharArray();
+        for (int i = 0; i < places.length; i++) {
+            if (check(places[i])) {
+                answer[i] = 1;
             }
-            
-            answer[i] = checkDistanceCompliant(room);
         }
         
         return answer;
     }
     
-    private int checkDistanceCompliant(char[][] room) {
+    private boolean check(final String[] place) {
+        char[][] board = new char[PLACE_SIZE][];
+        for (int i = 0; i < PLACE_SIZE; i++) {
+            board[i] = place[i].toCharArray();
+        }
         
-        for (int x = 0; x < WAITING_ROOM_SIZE; x++) {
-            for (int y = 0; y < WAITING_ROOM_SIZE; y++) {
-                if (room[x][y] != 'P') {
+        for (int i = 0; i < PLACE_SIZE; i++) {
+            for (int j = 0; j < PLACE_SIZE; j++) {
+                if (board[i][j] != 'P') {
                     continue;
                 }
-                
-                for (int k = 0; k < NEIGHBOR_CELL_SIZE; k++) {
-                    int nx = x + dx[k];
-                    int ny = y + dy[k];
-                    
-                    if (nx < 0 || nx >= WAITING_ROOM_SIZE || ny < 0 || ny >= WAITING_ROOM_SIZE) {
-                        continue;
-                    }
-                    
-                    if (k % 2 == 1) {
-                        if (room[nx][ny] == 'P' && (room[x][ny] == 'O' || room[nx][y] == 'O')) {
-                            return 0;
-                        }
-                        continue;
-                    }
-                    
-                    if (room[nx][ny] == 'P') {
-                        return 0;
-                    }
-                    
-                    if (room[nx][ny] == 'O') {
-                        nx += dx[k];
-                        ny += dy[k];
-                        
-                        if (nx < 0 || nx >= WAITING_ROOM_SIZE || ny < 0 || ny >= WAITING_ROOM_SIZE) {
-                            continue;
-                        }
-                        if (room[nx][ny] == 'P') {
-                            return 0;
-                        }
-                    }
+                if (!availableSeat(board, i, j)) {
+                    return false;
                 }
             }
         }
         
-        return 1;
+        return true;
+    }
+    
+    private boolean availableSeat(final char[][] board, final int x, final int y) {
+        for (int i = 0; i < CHECK_AREA; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if (nx < 0 || nx >= PLACE_SIZE || ny < 0 || ny >= PLACE_SIZE) {
+                continue;
+            }
+            
+            if (i % 2 == 0) {
+                if (board[nx][ny] == 'P') {
+                    return false;
+                }
+                continue;
+            }
+            
+            if (board[nx][ny] != 'P') {
+                continue;
+            }
+            
+            int lnx = x + dx[(i - 1) % CHECK_AREA];
+            int lny = y + dy[(i - 1) % CHECK_AREA];
+            int rnx = x + dx[(i + 1) % CHECK_AREA];
+            int rny = y + dy[(i + 1) % CHECK_AREA];
+            
+            if (board[lnx][lny] == 'O' || board[rnx][rny] == 'O') {
+                return false;
+            }
+            if (board[lnx][lny] == 'P' || board[rnx][rny] == 'P') {
+                return false;
+            }
+        }
+        
+        for (int i = 0; i < CHECK_AREA; i += 2) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            int dnx = x + dx[i] * 2;
+            int dny = y + dy[i] * 2;
+            
+            if (dnx < 0 || dnx >= PLACE_SIZE || dny < 0 || dny >= PLACE_SIZE || board[dnx][dny] != 'P') {
+                continue;
+            }
+            
+            if (board[nx][ny] != 'X') {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
