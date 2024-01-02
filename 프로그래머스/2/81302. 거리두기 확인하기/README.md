2e6b753ebc0db0fb2e6a111d0870cb9ed9d3b346
예성 <66625672+yeseong31@users.noreply.github.com>
2024-01-02 17:46:28 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=python3) 
 
 ### 성능 요약
 
-메모리: 79 MB, 시간: 0.07 ms
+메모리: 10.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 17:10:24
+2024년 1월 2일 17:46:26
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -0,0 +1,50 @@
+def is_available_seat(place: list, x: int, y: int, dx: tuple, dy: tuple) -> bool:
+    for i in range(8):
+        nx, ny = x + dx[i], y + dy[i]
+
+        if nx < 0 or nx >= 5 or ny < 0 or ny >= 5:
+            continue
+
+        if i % 2 == 0:
+            if place[nx][ny] == 'P':
+                return False
+            continue
+
+        if place[nx][ny] != 'P':
+            continue
+
+        lnx, lny = x + dx[i - 1], y + dy[i - 1]
+        rnx, rny = x + dx[(i + 1) % 8], y + dy[(i + 1) % 8]
+
+        if place[lnx][lny] in ('O', 'P'):
+            return False
+        if place[rnx][rny] in ('O', 'P'):
+            return False
+
+    for i in range(0, 8, 2):
+        nx, ny = x + dx[i], y + dy[i]
+        dnx, dny = x + dx[i] * 2, y + dy[i] * 2
+
+        if dnx < 0 or dnx >= 5 or dny < 0 or dny >= 5 or place[dnx][dny] != 'P':
+            continue
+
+        if place[nx][ny] != 'X':
+            return False
+
+    return True
+
+
+def check(place: list, dx: tuple, dy: tuple) -> int:
+    for i in range(5):
+        for j in range(5):
+            if place[i][j] != 'P':
+                continue
+            if not is_available_seat(place, i, j, dx, dy):
+                return False
+
+    return True
+
+
+def solution(places):
+    dx, dy = (-1, -1, 0, 1, 1, 1, 0, -1), (0, 1, 1, 1, 0, -1, -1, -1)
+    return [int(check(place, dx, dy)) for place in places]
