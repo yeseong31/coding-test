9968f677f3659716af9268b2de32adfa4bdfd271
예성 <66625672+yeseong31@users.noreply.github.com>
2024-01-02 20:47:43 +0900

    [level 1] Title: 시저 암호, Time: 1.43 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/README.md
+++ 프로그래머스/1/12926. 시저 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 2.83 ms
+메모리: 75.8 MB, 시간: 1.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 20:3:56
+2024년 1월 2일 20:47:41
 
 ### 문제 설명
 
--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,17 +1,21 @@
+import java.lang.StringBuilder;
+
+
 class Solution {
     public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
+        
         for (char c : s.toCharArray()) {
             sb.append(push(c, n));
         }
+        
         return sb.toString();
     }
     
-    private char push(char c, int n) {
-        if (!Character.isAlphabetic(c)) {
+    private char push(final char c, final int n) {
+        if (c == ' ') {
             return c;
         }
-        
         int offset = Character.isUpperCase(c) ? 'A' : 'a';
         int position = (c - offset + n) % ('z' - 'a' + 1);
         return (char) (offset + position);
