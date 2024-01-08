f9385d96050d0f60d84fbebd342826b72f399475
예성 <66625672+yeseong31@users.noreply.github.com>
2024-01-08 11:31:27 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 0.07 ms
+메모리: 71.9 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 10:58:7
+2024년 1월 1일 11:31:26
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,32 +1,13 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-
 class Solution {
     public int[] solution(int brown, int yellow) {
         int area = brown + yellow;
         
-        List<Integer> dividors = getDividors(area);
-        for (int height : dividors) {
-            int width = area / height;
-            if ((width - 2) * (height - 2) == yellow) {
-                return new int[] {width, height};
+        for (int n = 3; n <= (int) Math.sqrt(area); n++) {
+            if (area % n == 0 && (n - 2) * ((area / n) - 2) == yellow) {
+                return new int[] {area / n, n};
             }
         }
         
         return null;
-    }
-    
-    private List<Integer> getDividors(final int area) {
-        List<Integer> result = new ArrayList<>();
-        
-        for (int i = 1; i <= Math.sqrt(area); i++) {
-            if (area % i == 0) {
-                result.add(i);
-            }
-        }
-        
-        return Collections.unmodifiableList(result);
     }
 }
