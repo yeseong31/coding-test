d764bcdb224b509dc727da4dcff3442982817116
예성 <66625672+yeseong31@users.noreply.github.com>
2024-01-08 11:06:55 +0900

    [level 1] Title: 모의고사, Time: 4.53 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 68 MB, 시간: 5.16 ms
+메모리: 78.1 MB, 시간: 4.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 10:12:39
+2024년 1월 1일 11:6:53
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,38 +1,30 @@
 import java.util.stream.IntStream;
 
-
 class Solution {
     
-    private static final int[][] PERSONS = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2 ,4, 4, 5, 5}};
+    private static final int[][] PERSONS = {
+        {1, 2, 3, 4, 5},
+        {2, 1, 2, 3, 2, 4, 2, 5},
+        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
     
     public int[] solution(int[] answers) {
         int[] scores = new int[3];
-        int topScorerIndex = findTopScorerIndex(answers, scores);
-        
-        return IntStream.range(0, 3)
-                .filter(i -> scores[i] == scores[topScorerIndex])
-                .map(i -> i + 1)
-                .toArray();
-    }
-    
-    private int findTopScorerIndex(int[] answers, int[] scores) {
-        int maxScorerIndex = 0;
+        int maxScore = -1;
         
         for (int i = 0; i < answers.length; i++) {
             for (int j = 0; j < PERSONS.length; j++) {
-                if (answers[i] == getPicked(i, j)) {
-                    if (scores[maxScorerIndex] < ++scores[j]) {
-                        maxScorerIndex = j;
-                    }
+                if (PERSONS[j][i % PERSONS[j].length] == answers[i]) {
+                    scores[j]++;
+                    maxScore = Math.max(maxScore, scores[j]);
                 }
             }
         }
         
-        return maxScorerIndex;
-    }
-    
-    private int getPicked(int seq, int index) {
-        int personLength = PERSONS[index].length;
-        return PERSONS[index][seq % personLength];
+        int finalMaxScore = maxScore;
+        return IntStream.range(0, 3)
+                .filter(i -> scores[i] == finalMaxScore)
+                .map(i -> i + 1)
+                .toArray();
     }
 }
