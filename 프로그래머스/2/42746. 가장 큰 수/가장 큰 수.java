beaafb39d6528e6b299b7669b703b3045883faca
예성 <66625672+yeseong31@users.noreply.github.com>
2024-01-09 13:30:11 +0900

    [level 2] Title: 가장 큰 수, Time: 4.37 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,13 +1,16 @@
 import java.util.Arrays;
-
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int[] numbers) {
         return Arrays.stream(numbers)
                 .mapToObj(String::valueOf)
-                .sorted((x, y) -> (y + x).compareTo(x + y))
-                .reduce((x, y) -> x + y)
-                .filter(s -> !s.startsWith("0"))
-                .orElse("0");
+                .sorted((v1, v2) -> {
+                    int n1 = Integer.parseInt(v1 + v2);
+                    int n2 = Integer.parseInt(v2 + v1);
+                    return n2 - n1;
+                })
+                .collect(Collectors.joining(""))
+                .replaceAll("^0+", "0");
     }
 }
