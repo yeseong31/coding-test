fff487977b95c20395f67b2b2549d0a1b615a59e
예성 <66625672+yeseong31@users.noreply.github.com>
2024-01-14 20:57:52 +0900

    [level Lv.3] Title: 입국심사, Time: 0.12 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/43238. 입국심사/입국심사.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long solution(int n, int[] times) {
+        long left = 1;
+        long right = 1_000_000_000_000_000L;
+        
+        while (left < right) {
+            long mid = (left + right) / 2;
+            
+            if (canImmigrate(n, times, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+    
+    private boolean canImmigrate(int n, int[] times, long mid) {
+        long count = 0;
+        
+        for (int time : times) {
+            count += mid / time;
+        }
+        
+        return count >= n;
+    }
+}
