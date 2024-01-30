8eeb96057da6c76b397b1a297e987d35d87e18ea
예성 <66625672+yeseong31@users.noreply.github.com>
2024-01-30 14:39:42 +0900

    [level 2] Title: 스킬트리, Time: 7.15 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,13 +1,11 @@
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.List;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         return (int) Arrays.stream(skill_trees)
                 .filter(skill_tree -> {
-                    String regex = "[^" + Pattern.quote(skill) + "]";
+                    String regex = "[^" + skill + "]";
                     String target = skill_tree.replaceAll(regex, "");
                     return skill.startsWith(target);
                 })
