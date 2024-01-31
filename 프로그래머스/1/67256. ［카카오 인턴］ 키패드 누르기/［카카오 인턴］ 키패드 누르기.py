5d62db90c681a851663bf8971ccce6398f48f99f
예성 <66625672+yeseong31@users.noreply.github.com>
2024-01-31 10:55:37 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -0,0 +1,35 @@
+def solution(numbers, hand):
+    answer = []
+    lx, ly, rx, ry = 3, 0, 3, 2
+    dic = {
+        1: (0, 0), 2: (0, 1), 3: (0, 2),
+        4: (1, 0), 5: (1, 1), 6: (1, 2),
+        7: (2, 0), 8: (2, 1), 9: (2, 2), 0: (3, 1)}
+    
+    for number in numbers:
+        if number not in (2, 5, 8, 0):
+            if number in (1, 4, 7):           
+                answer.append('L')
+                lx, ly = dic[number]
+            else:
+                answer.append('R')
+                rx, ry = dic[number]
+            continue
+
+        ld = abs(lx - dic[number][0]) + abs(ly - dic[number][1])
+        rd = abs(rx - dic[number][0]) + abs(ry - dic[number][1])
+        if ld == rd:
+            if hand == 'right':
+                answer.append('R')
+                rx, ry = dic[number]
+            else:
+                answer.append('L')
+                lx, ly = dic[number]
+        elif ld < rd:
+            answer.append('L')
+            lx, ly = dic[number]
+        else:
+            answer.append('R')
+            rx, ry = dic[number]
+        
+    return ''.join(answer)
