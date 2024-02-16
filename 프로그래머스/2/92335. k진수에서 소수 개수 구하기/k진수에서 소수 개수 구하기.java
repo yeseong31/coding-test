612ebfa8362fbfd17467712748349afd78bead34
예성 <66625672+yeseong31@users.noreply.github.com>
2024-02-16 10:14:42 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 8.34 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,42 @@
+import java.lang.StringBuilder;
+import java.util.Arrays;
+
+class Solution {
+    
+    public int solution(int n, int k) {
+        String converted = convertToK(n, k);
+        String[] target = converted.split("0");
+        
+        return countPrimeNumber(target);
+    }
+    
+    private int countPrimeNumber(String[] target) {
+        return (int) Arrays.stream(target)
+                .filter(s -> !s.isEmpty())
+                .mapToLong(Long::parseLong)
+                .filter(number -> isPrimeNumber(number))
+                .count();
+    }
+    
+    private boolean isPrimeNumber(long number) {
+        for (int index = 2; index <= Math.sqrt(number); index++) {
+            if (number % index == 0) {
+                return false;
+            }
+        }
+        
+        return !(number <= 1);
+    }
+    
+    private String convertToK(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        int mod;
+        
+        while (n != 0) {
+            sb.append(n % k);
+            n /= k;
+        }    
+        
+        return sb.reverse().toString();
+    }
+}
