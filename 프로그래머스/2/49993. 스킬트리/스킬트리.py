75e9189cd12adfbb8768d6f375190edf20ad8184
예성 <66625672+yeseong31@users.noreply.github.com>
2024-03-01 14:49:07 +0900

    [level 2] Title: 스킬트리, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -2,10 +2,4 @@ from re import sub
 
 
 def solution(skill, skill_trees):
-    answer = 0
-    
-    for tree in skill_trees:
-        if skill.startswith(sub(f'[^{skill}]', '', tree)):
-            answer += 1
-    
-    return answer
+    return sum(skill.startswith(sub(f'[^{skill}]', '', tree)) for tree in skill_trees)
