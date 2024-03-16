4d7646f9e92c3a48cf36516d9e32c074aaf8032d
예성 <66625672+yeseong31@users.noreply.github.com>
2024-03-16 21:04:19 +0900

    [level 3] Title: 풍선 터트리기, Time: 196.76 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -0,0 +1,24 @@
+def solution(a):
+    if len(a) <= 3:
+        return len(a)
+    
+    l, r = 0, len(a) - 1
+    left_min, right_min = a[l], a[r]
+    answer = 2
+    
+    while l < r:
+        cur_left, cur_right = a[l], a[r]
+        
+        if left_min > cur_left:
+            left_min = cur_left
+            answer += 1
+        elif right_min > cur_right:
+            right_min = cur_right
+            answer += 1
+            
+        if a[l] > a[r]:
+            l += 1
+        else:
+            r -= 1
+    
+    return answer
