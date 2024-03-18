64e1bdbacb14ac43a8eb5709d743af39141ac82b
예성 <66625672+yeseong31@users.noreply.github.com>
2024-03-18 21:04:04 +0900

    [level 3] Title: 광고 삽입, Time: 69.63 ms, Memory: 14.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
@@ -0,0 +1,35 @@
+def get_seconds(t: str) -> int:
+    return int(t[:2]) * 3600 + int(t[3:5]) * 60 + int(t[6:])
+
+
+def seconds_to_time(s: int) -> str:
+    h, s = divmod(s, 3600)
+    m, s = divmod(s, 60)
+    return f'{str(h).zfill(2)}:{str(m).zfill(2)}:{str(s).zfill(2)}'
+
+
+def solution(play_time, adv_time, logs):
+    if play_time == adv_time:
+        return '00:00:00'
+    
+    answer, _max = 0, -1
+    play_time, adv_time = get_seconds(play_time), get_seconds(adv_time)
+    views = [0] * (play_time + 1)
+    
+    for log in logs:
+        start, end = log.split('-')
+        views[get_seconds(start)] += 1
+        views[get_seconds(end)] -= 1
+    for i in range(1, play_time):
+        views[i] += views[i - 1]
+    for i in range(1, play_time):
+        views[i] += views[i - 1]
+    
+    for i in range(adv_time - 1, play_time):
+        target = views[i] - views[i - adv_time]
+        
+        if _max < target:
+            _max = target
+            answer = i - adv_time + 1
+    
+    return seconds_to_time(answer)
