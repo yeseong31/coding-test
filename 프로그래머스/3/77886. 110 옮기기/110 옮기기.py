28c9386478e72bcd4c682033822502c6fe3f620a
예성 <66625672+yeseong31@users.noreply.github.com>
2024-03-23 20:53:30 +0900

    [level 3] Title: 110 옮기기, Time: 132.42 ms, Memory: 15.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.py
@@ -0,0 +1,32 @@
+def solution(s):
+    def count(x):
+        res = 0
+        stack = []
+        
+        for c in x:
+            if c != '0' or stack[-2:] != ['1', '1']:
+                stack.append(c)
+                continue
+                
+            del stack[-2:]
+            res += 1
+            
+        return ''.join(stack), res
+
+    
+    answer = []
+    
+    for target in s:
+        trans, cnt = count(target)
+        
+        check = False
+        for i in range(len(trans) - 1, -1, -1):
+            if trans[i] == '0':
+                answer.append(trans[:i+1] + '110' * cnt + trans[i+1:])
+                check = True
+                break
+        
+        if not check:
+            answer.append('110' * cnt + trans)
+    
+    return answer
