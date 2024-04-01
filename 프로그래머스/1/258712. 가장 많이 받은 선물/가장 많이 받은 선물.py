6c25d6ff33fee84f493d26a29267fbe89463ceab
예성 <66625672+yeseong31@users.noreply.github.com>
2024-04-01 13:44:15 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 1.80 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,27 +1,22 @@
-from collections import defaultdict
-
-
 def solution(friends, gifts):
-    answer = 0
     n = len(friends)
-    
-    indexes = {friend: index for index, friend in enumerate(friends)}
+    idx = {f: i for i, f in enumerate(friends)}
     graph = [[0] * n for _ in range(n)]
     
     for gift in gifts:
         a, b = gift.split(' ')
-        graph[indexes[a]][indexes[b]] += 1
+        graph[idx[a]][idx[b]] += 1
         
     scores = [0] * n
-    for index in range(n):
-        give_score = sum(graph[index])
-        receive_score = sum(graph[x][index] for x in range(n))
-        scores[index] = give_score - receive_score
+    for i in range(n):
+        give_score = sum(graph[i])
+        receive_score = sum(graph[x][i] for x in range(n))
+        scores[i] = give_score - receive_score
     
-    receive_count = [0] * n
+    counts = [0] * n
     for i in range(n):
         for j in range(n):
             if graph[i][j] > graph[j][i] or graph[i][j] == graph[j][i] and scores[i] > scores[j]:
-                receive_count[i] += 1
+                counts[i] += 1
     
-    return max(receive_count)
+    return max(counts)
