52b9be6ed6045646125606039a08fd46a8a87ae1
예성 <66625672+yeseong31@users.noreply.github.com>
2024-04-21 20:04:10 +0900

    [level 2] Title: 오픈채팅방, Time: 68.01 ms, Memory: 41.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+
+def solution(record):
+    users = defaultdict(str)
+    events = list()
+    txt = ('님이 들어왔습니다.', '님이 나갔습니다.')
+    
+    for r in record:
+        event, *userinfo = r.split()
+        if event == 'Enter':
+            events.append((userinfo[0], 0))
+            users[userinfo[0]] = userinfo[1]
+        elif event == 'Leave':
+            events.append((userinfo[0], 1))
+        else:
+            users[userinfo[0]] = userinfo[1]
+    
+    return [f'{users[u]}{txt[i]}' for u, i in events]
