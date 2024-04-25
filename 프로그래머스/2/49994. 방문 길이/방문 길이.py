54e7bfde83b23658a3a331dd81b04d89051b33a1
예성 <66625672+yeseong31@users.noreply.github.com>
2024-04-25 21:46:18 +0900

    [level 2] Title: 방문 길이, Time: 0.31 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -0,0 +1,19 @@
+def solution(dirs):
+    x, y = 0, 0
+    visited = set()
+
+    for c in dirs:
+        if c == 'U' and y < 5:
+            visited.add(((x, y), (x, y + 1)))
+            y += 1
+        elif c == 'D' and y > -5:
+            visited.add(((x, y - 1), (x, y)))
+            y -= 1
+        elif c == 'R' and x < 5:
+            visited.add(((x, y), (x + 1, y)))
+            x += 1
+        elif c == 'L' and x > -5:
+            visited.add(((x - 1, y), (x, y)))
+            x -= 1
+    
+    return len(visited)
