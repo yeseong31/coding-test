a3c047a58ee749919eb16d7fd258483451afee13
예성 <66625672+yeseong31@users.noreply.github.com>
2024-05-08 22:15:01 +0900

    [level 2] Title: 피로도, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -10,10 +10,8 @@ def solution(k, dungeons):
 
         for i in check:
             cond, cost = dungeons[i]
-            
             if current_k < cond:
                 break
-            
             current_k -= cost
             count += 1
 
