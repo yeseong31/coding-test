6dc27f09c1eebe0dbf687feaff89686e4d95e083
예성 <66625672+yeseong31@users.noreply.github.com>
2024-05-09 07:45:18 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -2,9 +2,9 @@ def solution(data, col, row_begin, row_end):
     answer = -1
     data.sort(key=lambda x: (x[col - 1], -x[0]))
     
-    for i in range(row_begin, row_end + 1):    
-        target = int(bin(sum(x % i for x in data[i - 1])), 2)
-        
+    for row in range(row_begin, row_end + 1):    
+        row_sum = sum(x % row for x in data[row - 1])
+        target = int(bin(row_sum), 2)
         if answer == -1:
             answer = target
         else:
