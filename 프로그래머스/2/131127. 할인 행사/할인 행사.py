aef3e64c3a53c0aa60308d69fcbcd2fb8c1235ea
예성 <66625672+yeseong31@users.noreply.github.com>
2024-07-26 21:43:43 +0900

    [level 2] Title: 할인 행사, Time: 179.36 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -2,14 +2,5 @@ from collections import Counter
 
 
 def solution(want, number, discount):
-    answer = 0
-    dic = {}
-    
-    for i in range(len(want)):
-        dic[want[i]] = number[i]
-
-    for i in range(len(discount)-9):
-        if dic == Counter(discount[i:i+10]): 
-            answer += 1
-
-    return answer
+    dic = {v: number[i] for i, v in enumerate(want)}
+    return sum(dic == Counter(discount[i:i + 10]) for i in range(len(discount) - 9))
