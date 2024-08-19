ceef7b9992417b964bd6fc09faa9cdd77e61726b
예성 <66625672+yeseong31@users.noreply.github.com>
2024-08-19 22:02:45 +0900

    [level 1] Title: 기사단원의 무기, Time: 1103.98 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,14 @@
+def solution(number, limit, power):
+    def count_gcd(n):
+        count = 0
+        for m in range(1, int(n ** 0.5) + 1):
+            if n % m == 0:
+                count += 1
+                if n // m != m:
+                    count += 1
+        return count
+    
+    answer = 0
+    for x in [count_gcd(i) for i in range(1, number + 1)]:
+        answer += power if x > limit else x
+    return answer
