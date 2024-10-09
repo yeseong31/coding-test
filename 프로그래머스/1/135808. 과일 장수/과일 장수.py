d42bfe94d86b8784d53478500e969879719c944c
예성 <66625672+yeseong31@users.noreply.github.com>
2024-10-09 22:03:48 +0900

    [level 1] Title: 과일 장수, Time: 142.26 ms, Memory: 21.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,3 +1,9 @@
 def solution(k, m, score):
+    answer = 0
     score.sort(reverse=True)
-    return sum(min(score[i:i + m]) for i in range(0, len(score), m) if i + m <= len(score)) * m
+    
+    for i in range(0, len(score), m):
+        if i + m <= len(score):
+            answer += min(score[i:i+m])
+            
+    return answer * m
