d89bd8accd2777d5f0f87e4e874ffa9125c2c785
예성 <66625672+yeseong31@users.noreply.github.com>
2024-10-11 22:04:59 +0900

    [level 2] Title: 방문 길이, Time: 0.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -6,13 +6,13 @@ def solution(dirs):
         if c == 'U' and y < 5:
             visited.add(((x, y), (x, y + 1)))
             y += 1
-        elif c == 'D' and y > -5:
+        if c == 'D' and y > -5:
             visited.add(((x, y - 1), (x, y)))
             y -= 1
-        elif c == 'R' and x < 5:
+        if c == 'R' and x < 5:
             visited.add(((x, y), (x + 1, y)))
             x += 1
-        elif c == 'L' and x > -5:
+        if c == 'L' and x > -5:
             visited.add(((x - 1, y), (x, y)))
             x -= 1
     
