7b64f8f8182a8ed52828f9c96be806cbae11e35b
예성 <66625672+yeseong31@users.noreply.github.com>
2024-10-20 12:38:31 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 453.59 ms, Memory: 93.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -0,0 +1,46 @@
+from collections import defaultdict, Counter
+from itertools import zip_longest
+
+
+def move_point(x, y, nx, ny):
+    result = []
+    if (x, y) == (nx, ny):
+        return result
+    
+    step = 1 if x <= nx else -1
+    for x in range(x + step, nx + step, step):
+        result.append((x, y))
+        
+    step = 1 if y <= ny else -1
+    for y in range(y + step, ny + step, step):
+        result.append((x, y))
+        
+    return result
+
+
+def get_footprints(points, via_routes):
+    result = []
+    
+    x, y = points[via_routes[0] - 1]
+    result.append((x, y))
+    
+    for index in range(1, len(via_routes)):
+        x, y = points[via_routes[index - 1] - 1]
+        nx, ny = points[via_routes[index] - 1]
+        result.extend(move_point(x, y, nx, ny))
+    
+    return result
+
+
+def solution(points, routes):
+    answer = 0
+    
+    footprints = [get_footprints(points, x) for x in routes]
+    footprints = [x for x in zip_longest(*footprints)]
+    
+    for robot_points in footprints:
+        for key, value in Counter(robot_points).items():
+            if key is not None and value > 1:
+                answer += 1
+        
+    return answer
