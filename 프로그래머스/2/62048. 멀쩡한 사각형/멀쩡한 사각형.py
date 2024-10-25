8990f5d65594352f63636d6f1b78a2c51aff7e20
예성 <66625672+yeseong31@users.noreply.github.com>
2024-10-25 22:53:49 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -1,6 +1,6 @@
-import math
+from math import gcd
 
 
 def solution(w,h):
-    n = math.gcd(w, h)
-    return w * h - ((w // n) + (h // n) - 1) * n
+    n = gcd(w, h)
+    return w * h - ((w + h - n) // n) * n
