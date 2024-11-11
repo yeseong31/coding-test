46537d73d2cc3eb0e51eb5c7d6e73e75d1dac519
예성 <66625672+yeseong31@users.noreply.github.com>
2024-11-11 22:36:49 +0900

    [level 1] Title: 덧칠하기, Time: 14.92 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -2,11 +2,10 @@ def solution(n, m, section):
     answer = 0
     
     while section:
+        answer += 1
         point = section.pop()
         
         while section and section[-1] > point - m:
             section.pop()
-            
-        answer += 1
         
     return answer
