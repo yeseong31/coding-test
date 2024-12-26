52567690d116f5297589a133a12ee341a18507fd
예성 <66625672+yeseong31@users.noreply.github.com>
2024-12-26 21:51:48 +0900

    [Gold III] Title: 팰린드롬 만들기, Time: 6840 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,13 +1,22 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 lst = list(map(int, input().split()))
 
-board = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+p, c = [], []
+for i in range(n):
+    pp, p, c = p, c, []  # double prev, prev, cur
+    for j in range(n - i):
+        if i == 0:
+            c.append(0)
+            continue
+        if lst[j] != lst[i + j]:
+            c.append(min(p[j], p[j + 1]) + 1)
+            continue
+        if i == 1:
+            c.append(0)
+            continue
+        c.append(pp[j + 1])
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if lst[i - 1] == lst[n - j]:
-            board[i][j] = board[i - 1][j - 1] + 1
-        else:
-            board[i][j] = max(board[i - 1][j], board[i][j - 1])
-
-print(n - board[n][n])
+print(c[0])
