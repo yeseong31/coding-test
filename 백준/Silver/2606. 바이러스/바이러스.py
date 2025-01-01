e5f11b078b6154ca143b92c990d29ef56e3d664e
예성 <66625672+yeseong31@users.noreply.github.com>
2025-01-01 22:23:37 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def dfs(v):
+    global cnt
+    visited.add(v)
+    for i in graph[v]:
+        if i not in visited:
+            cnt += 1
+            dfs(i)
+
+
+# 컴퓨터의 수
+n = int(input())
+# 간선의 수
+m = int(input())
+# 그래프
+graph = [[] * n for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+# 방문 기록
+visited = set()
+cnt = 0
+
+# DFS
+dfs(1)
+print(cnt)
