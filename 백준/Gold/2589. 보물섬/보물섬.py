991d858b680861f2c0189acab15c7e2b028500e9
예성 <66625672+yeseong31@users.noreply.github.com>
2025-01-03 23:00:48 +0900

    [Gold V] Title: 보물섬, Time: 1104 ms, Memory: 32440 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -14,23 +14,24 @@ def bfs(x, y):
     q = deque([(x, y)])
     visited = [[0] * m for _ in range(n)]
     visited[x][y] = 1
-    count = 0
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            if nx < 0 or nx >= n or ny < 0 or ny >= m or board[nx][ny] == 'W' or visited[nx][ny]:
                 continue
-            if board[nx][ny] == 'L' and visited[nx][ny] == 0:
-                visited[nx][ny] = visited[x][y] + 1
-                count = max(count, visited[nx][ny])
-                q.append((nx, ny))
-    return count - 1
+            visited[nx][ny] = visited[x][y] + 1
+            q.append((nx, ny))
+    return visited[x][y] - 1
 
 
 answer = 0
 for i in range(n):
     for j in range(m):
         if board[i][j] == 'L':
+            if 1 <= i < n - 1 and board[i - 1][j] == board[i + 1][j] == 'L':
+                continue
+            if 1 <= j < m - 1 and board[i][j - 1] == board[i][j + 1] == 'L':
+                continue
             answer = max(answer, bfs(i, j))
 print(answer)
