9c1055ad3b61919ef923e1ab45702f8d3ac5c3e2
예성 <66625672+yeseong31@users.noreply.github.com>
2025-01-15 21:29:14 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.52 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,16 @@
+from heapq import heappush, heappop
+
+
+def solution(k, score):
+    answer = []
+    q = []
+    
+    for s in score:
+        heappush(q, s)
+        
+        if len(q) > k:
+            heappop(q)
+        
+        answer.append(q[0])
+    
+    return answer
