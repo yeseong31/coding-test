59424f27208e573d5289890ea61525314b76277f
예성 <66625672+yeseong31@users.noreply.github.com>
2025-01-17 22:25:30 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.89 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -5,7 +5,7 @@ def solution(clothes):
     answer = 1
     dic = defaultdict(int)
     
-    for name, kind in clothes:
+    for _, kind in clothes:
         dic[kind] += 1
     for k in dic:
         answer *= dic[k] + 1
