166a514af254c6f2e6d40cbfceb32cd416a23a69
예성 <66625672+yeseong31@users.noreply.github.com>
2025-01-23 22:00:57 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 100 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def w(a, b, c):
+    global d
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+    if d[a][b][c]:
+        return d[a][b][c]
+    if a < b < c:
+        d[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+    else:
+        d[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
+    return d[a][b][c]
+
+
+d = [[[0] * 21 for _ in range(21)] for _ in range(21)]
+while True:
+    a, b, c = map(int, input().split())
+    if a == b == c == -1:
+        break
+    print(f'w({a}, {b}, {c}) = {w(a, b, c)}')
