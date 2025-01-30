7076a65f840dcc6287ce70f30456dc93ecc69c05
예성 <66625672+yeseong31@users.noreply.github.com>
2025-01-30 22:54:58 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 1.93 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -12,19 +12,17 @@ def solution(mats, park):
         for y in range(1, m + 1):
             if park[x - 1][y - 1] != '-1':
                 continue
-            elif x == 1 or y == 1:
+            if x == 1 or y == 1:
                 expanded_park[x][y] = 1
                 answer.add(1)
                 continue
             
             a, b, c = expanded_park[x - 1][y - 1], expanded_park[x][y - 1], expanded_park[x - 1][y]
             
-            if a != 0 and b != 0 and c != 0:
-                expanded_park[x][y] = min(a, b, c) + 1
-                answer.add(expanded_park[x][y])
-            else:
-                expanded_park[x][y] = 1
-                answer.add(1)
+            expanded_park[x][y] = min(a, b, c) + 1 if (a != 0 and b != 0 and c != 0) else 1
+            answer.add(expanded_park[x][y])
     
-    common_numbers = answer.intersection(mats)
-    return max(common_numbers) if common_numbers else -1
+    if common_numbers := answer.intersection(mats):
+        return max(common_numbers)
+    
+    return -1
