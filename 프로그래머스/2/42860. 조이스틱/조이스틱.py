a8b65dc5b53f24c47aae765420683012cb2283c2
예성 <66625672+yeseong31@users.noreply.github.com>
2025-02-20 21:55:49 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,10 +1,11 @@
 def solution(name):
-    def to_A(i):
-        return min(ord(name[i]) - ord('A'), ord('Z') - ord(name[i]) + 1)
-    
     n = len(name)
     target = ['A'] * n
-    count = sum(to_A(i) for i in range(n))
+    
+    count = 0
+    for i in range(n):
+        count += min(ord(name[i]) - ord('A'), ord('Z') - ord(name[i]) + 1)
+    
     min_step = n - 1
     
     for i, c in enumerate(name):
