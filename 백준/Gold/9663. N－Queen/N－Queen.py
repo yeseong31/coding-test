4ecb6a486d2d52afe7f83555be08501c26e83445
예성 <66625672+yeseong31@users.noreply.github.com>
2025-03-25 23:21:07 +0900

    [Gold IV] Title: N-Queen, Time: 6228 ms, Memory: 129424 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,17 @@
+def solution(n):
+    def queens(x):
+        ans = 0
+        if x == n + 1:
+            return 1
+        for i in range(1, n + 1):
+            a, b = x + i, n + x - i
+            if not (col[i] or d1[a] or d2[b]):
+                col[i] = d1[a] = d2[b] = True
+                ans += queens(x + 1)
+                col[i] = d1[a] = d2[b] = False
+        return ans
+
+    col, d1, d2 = [False] * (n + 1), [False] * (2 * n + 1), [False] * (2 * n + 1)
+    return queens(1)
+
+print(solution(int(input())))
