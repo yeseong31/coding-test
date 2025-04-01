02a4e340aea468146c17b1f26d59548dce8edcfa
예성 <66625672+yeseong31@users.noreply.github.com>
2025-04-01 22:05:49 +0900

    [level 1] Title: 콜라 문제, Time: 0.57 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,9 +1,9 @@
 def solution(a, b, n):
     answer = 0
     
-    while n >= a:
+    while a <= n:
         div, mod = divmod(n, a)
         answer += div * b
-        n = (div * b) + mod
+        n = div * b + mod
     
     return answer
