8b8d952e4732fc3122d9cdf343adc07986e57330
예성 <66625672+yeseong31@users.noreply.github.com>
2025-04-24 16:47:20 +0900

    [level 2] Title: 완전범죄, Time: 7.45 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+
+def solution(info, n, m):
+    answer = [n]
+    visited = set()
+
+    def dfs(i, a, b):
+        if (i, a, b) in visited:
+            return
+        
+        visited.add((i, a, b))
+        
+        if a >= n or b >= m or a >= answer[0]:
+            return
+        if i == len(info):
+            answer[0] = min(answer[0], a)
+            return
+
+        dfs(i + 1, a + info[i][0], b)
+        dfs(i + 1, a, b + info[i][1])
+
+    dfs(0, 0, 0)
+    return answer[0] if answer[0] != n else -1
