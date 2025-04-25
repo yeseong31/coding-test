60a1c216732c518af461d7e0d9d7917de84879d6
예성 <66625672+yeseong31@users.noreply.github.com>
2025-04-25 15:37:25 +0900

    [level 2] Title: 교점에 별 만들기, Time: 18.91 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/README.md
+++ 프로그래머스/2/87377. 교점에 별 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 교점에 별 만들기 - 87377 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 267.46 ms
+메모리: 77.6 MB, 시간: 18.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 25일 15:02:58
+2025년 04월 25일 15:37:22
 
 ### 문제 설명
 
--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -3,65 +3,104 @@ import java.util.Arrays;
 import java.util.List;
 
 class Point {
-
     private final long x;
     private final long y;
-
-    private Point(final long x, final long y) {
+    
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
-    }
-
-    public static Point of(final long x, final long y) {
-        return new Point(x, y);
-    }
-    
-    public long getX() {
-        return x;
-    }
-    
-    public long getY() {
-        return y;
-    }
-    
-    public long getMinimumX(long x) {
-        return Math.min(this.x, x);
-    }
-
-    public long getMinimumY(long y) {
-        return Math.min(this.y, y);
-    }
-
-    public long getMaximumX(long x) {
-        return Math.max(this.x, x);
-    }
-
-    public long getMaximumY(long y) {
-        return Math.max(this.y, y);
     }
 }
 
 class Solution {
+    private class Point {
+        private final long x;
+        private final long y;
+
+        private Point(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
         
+        public long getX() {
+            return x;
+        }
+        
+        public long getY() {
+            return y;
+        }
+    }
+    
+    private Point getIntersection(long a, long b, long e, long c, long d, long f) {
+        if ((a * d) == (b * c)) {
+            return null;
+        }
+        
+        double x = (double) ((b * f) - (e * d)) / ((a * d) - (b * c));
+        double y = (double) ((e * c) - (a * f)) / ((a * d) - (b * c));
+        
+        if (x % 1 != 0 || y % 1 != 0) {
+            return null;
+        }
+        
+        return new Point((long) x, (long) y);
+    }
+    
+    private Point getMinPoint(List<Point> points) {
+        long minX = Long.MAX_VALUE;
+        long minY = Long.MAX_VALUE;
+        
+        for (Point point : points) {
+            long px = point.getX();
+            long py = point.getY();
+            
+            if (px < minX) {
+                minX = px;
+            }
+            if (py < minY) {
+                minY = py;
+            }
+        }
+        
+        return new Point(minX, minY);
+    }
+    
+    private Point getMaxPoint(List<Point> points) {
+        long maxX = Long.MIN_VALUE;
+        long maxY = Long.MIN_VALUE;
+        
+        for (Point point : points) {
+            long px = point.getX();
+            long py = point.getY();
+            
+            if (maxX < px) {
+                maxX = px;
+            }
+            if (maxY < py) {
+                maxY = py;
+            }
+        }
+        
+        return new Point(maxX, maxY);
+    }
+    
     public String[] solution(int[][] line) {
-        
         List<Point> points = new ArrayList<>();
         
         for (int i = 0; i < line.length; i++) {
             for (int j = i + 1; j < line.length; j++) {
-                Point intersaction = findIntersaction(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
-                
-                if (intersaction != null) {
-                    points.add(intersaction);
+                Point point = getIntersection(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
+                if (point != null) {
+                    points.add(point);
                 }
             }
         }
         
-        Point minimumPoint = findMinimumPoint(points);
-        Point maximumPoint = findMaximumPoint(points);
+        Point minPoint = getMinPoint(points);
+        Point maxPoint = getMaxPoint(points);
         
-        int width = calculateWidth(minimumPoint, maximumPoint);
-        int height = calculateHeight(minimumPoint, maximumPoint);
+        int width = (int) (maxPoint.getX() - minPoint.getX() + 1);
+        int height = (int) (maxPoint.getY() - minPoint.getY() + 1);
         
         char[][] board = new char[height][width];
         for (char[] row : board) {
@@ -69,63 +108,15 @@ class Solution {
         }
         
         for (Point point : points) {
-            int x = (int) (point.getX() - minimumPoint.getX());
-            int y = (int) (maximumPoint.getY() - point.getY());
+            int x = (int) (point.getX() - minPoint.getX());
+            int y = (int) (maxPoint.getY() - point.getY());
             board[y][x] = '*';
         }
         
-        String[] answer = new String[height];
-        for (int i = 0; i < height; i++) {
+        String[] answer = new String[board.length];
+        for (int i = 0; i < answer.length; i++) {
             answer[i] = new String(board[i]);
         }
-        
         return answer;
-    }
-    
-    private int calculateWidth(final Point minimumPoint, final Point maximumPoint) {
-        return (int) (maximumPoint.getX() - minimumPoint.getX() + 1);
-    }
-    
-    private int calculateHeight(final Point minimumPoint, final Point maximumPoint) {
-        return (int) (maximumPoint.getY() - minimumPoint.getY() + 1);
-    }
-    
-    private Point findMinimumPoint(final List<Point> points) {
-        long x = Long.MAX_VALUE;
-        long y = Long.MAX_VALUE;
-        
-        for (Point point : points) {
-            x = point.getMinimumX(x);
-            y = point.getMinimumY(y);
-        }
-        
-        return Point.of(x, y);
-    }
-    
-    private Point findMaximumPoint(final List<Point> points) {
-        long x = Long.MIN_VALUE;
-        long y = Long.MIN_VALUE;
-        
-        for (Point point : points) {
-            x = point.getMaximumX(x);
-            y = point.getMaximumY(y);
-        }
-        
-        return Point.of(x, y);
-    }
-    
-    private Point findIntersaction(long a, long b, long e, long c, long d, long f) {
-        if (a * d == b * c) {
-            return null;
-        }
-        
-        double x = (double) (b * f - e * d) / (a * d - b * c);
-        double y = (double) (e * c - a * f) / (a * d - b * c);
-        
-        if (x % 1 != 0 || y % 1 != 0) {
-            return null;
-        }
-        
-        return Point.of((long) x, (long) y);
     }
 }
