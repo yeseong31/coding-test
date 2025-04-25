6c21c1d4a51f383dfc0094cae71618624e7eba1b
예성 <66625672+yeseong31@users.noreply.github.com>
2025-04-25 22:05:56 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.35 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=java) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 120.54 ms
+메모리: 83.7 MB, 시간: 7.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 25일 22:02:58
+2025년 04월 25일 22:05:54
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -2,22 +2,16 @@ class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int[arr1.length][arr2[0].length];
         
-        for (int x = 0; x < arr1.length; x++) {
-            for (int y = 0; y < arr2[0].length; y++) {
-                answer[x][y] = calculateMatrix(arr1, arr2, x, y);
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                int result = 0;
+                for (int k = 0; k < arr1[0].length; k++) {
+                    result += arr1[i][k] * arr2[k][j];
+                }
+                answer[i][j] = result;
             }
         }
         
         return answer;
-    }
-    
-    private int calculateMatrix(int[][] arr1, int[][] arr2, int x, int y) {
-        int result = 0;
-        
-        for (int k = 0; k < arr2.length; k++) {
-            result += arr1[x][k] * arr2[k][y];
-        }
-        
-        return result;
     }
 }
