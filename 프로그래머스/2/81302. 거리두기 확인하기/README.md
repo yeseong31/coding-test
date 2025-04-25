8ef37835b1113c1b1d38b6607eb52c871556f040
예성 <66625672+yeseong31@users.noreply.github.com>
2025-04-25 21:47:53 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.61 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=java) 
 
 ### 성능 요약
 
-메모리: 9.32 MB, 시간: 0.12 ms
+메모리: 90.2 MB, 시간: 0.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 25일 21:25:10
+2025년 04월 25일 21:47:51
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,90 +1,112 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 class Solution {
+    private static final int[] dx = {-1, 0, 1, 0, -1, 1, 1, -1, -2, 0, 2, 0};
+    private static final int[] dy = {0, 1, 0, -1, 1, 1, -1, -1, 0, 2, 0, -2};
     
-    private static final int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    private static final int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
-    private static final int PLACE_SIZE = 5;
-    private static final int CHECK_AREA = 8;
+    private class Point {
+        private final int x;
+        private final int y;
+        
+        private Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        public int getX() {
+            return x;
+        }
+        
+        public int getY() {
+            return y;
+        }
+    }
+    private boolean isDistanced(char[][] place, Point p) {
+        int x = p.getX();
+        int y = p.getY();
+
+        for (int i = 0; i < 12; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5 || place[nx][ny] != 'P') {
+                continue;
+            }
+
+            int distX = Math.abs(nx - x);
+            int distY = Math.abs(ny - y);
+
+            int mx;
+            int my;
+
+            if (distX == 2) {
+                if (nx < x) {
+                    mx = x - 1;
+                } else {
+                    mx = x + 1;
+                }
+                if (place[mx][ny] == 'O') {
+                    return false;
+                }
+            } else if (distY == 2) {
+                if (ny < y) {
+                    my = y - 1;
+                } else {
+                    my = y + 1;
+                }
+                if (place[nx][my] == 'O') {
+                    return false;
+                }
+            } else if (distX == 1 && distY == 1) {
+                if (!(place[nx][y] == 'X' && place[x][ny] == 'X')) {
+                    return false;
+                }
+            } else if (place[nx][ny] == 'P') {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private boolean isPossiblePlace(char[][] place) {
+        List<Point> persons = new ArrayList<>();
+        
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+                if (place[x][y] == 'P') {
+                    persons.add(new Point(x, y));
+                }
+            }
+        }
+        
+        for (Point person : persons) {
+            if (!isDistanced(place, person)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
     
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
         
         for (int i = 0; i < places.length; i++) {
-            if (check(places[i])) {
+            char[][] place = new char[5][];
+            for (int j = 0; j < 5; j++) {
+                place[j] = places[i][j].toCharArray();
+            }
+            
+            if (isPossiblePlace(place)) {
                 answer[i] = 1;
+            } else {
+                answer[i] = 0;
             }
         }
         
         return answer;
-    }
-    
-    private boolean check(final String[] place) {
-        char[][] board = new char[PLACE_SIZE][];
-        for (int i = 0; i < PLACE_SIZE; i++) {
-            board[i] = place[i].toCharArray();
-        }
-        
-        for (int i = 0; i < PLACE_SIZE; i++) {
-            for (int j = 0; j < PLACE_SIZE; j++) {
-                if (board[i][j] != 'P') {
-                    continue;
-                }
-                if (!availableSeat(board, i, j)) {
-                    return false;
-                }
-            }
-        }
-        
-        return true;
-    }
-    
-    private boolean availableSeat(final char[][] board, final int x, final int y) {
-        for (int i = 0; i < CHECK_AREA; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if (nx < 0 || nx >= PLACE_SIZE || ny < 0 || ny >= PLACE_SIZE) {
-                continue;
-            }
-            
-            if (i % 2 == 0) {
-                if (board[nx][ny] == 'P') {
-                    return false;
-                }
-                continue;
-            }
-            
-            if (board[nx][ny] != 'P') {
-                continue;
-            }
-            
-            int lnx = x + dx[(i - 1) % CHECK_AREA];
-            int lny = y + dy[(i - 1) % CHECK_AREA];
-            int rnx = x + dx[(i + 1) % CHECK_AREA];
-            int rny = y + dy[(i + 1) % CHECK_AREA];
-            
-            if (board[lnx][lny] == 'O' || board[rnx][rny] == 'O') {
-                return false;
-            }
-            if (board[lnx][lny] == 'P' || board[rnx][rny] == 'P') {
-                return false;
-            }
-        }
-        
-        for (int i = 0; i < CHECK_AREA; i += 2) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            int dnx = x + dx[i] * 2;
-            int dny = y + dy[i] * 2;
-            
-            if (dnx < 0 || dnx >= PLACE_SIZE || dny < 0 || dny >= PLACE_SIZE || board[dnx][dny] != 'P') {
-                continue;
-            }
-            
-            if (board[nx][ny] != 'X') {
-                return false;
-            }
-        }
-        
-        return true;
     }
 }
