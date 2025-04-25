8bc2b00316a52e937bd30b3ac8a1908af94fd147
예성 <66625672+yeseong31@users.noreply.github.com>
2025-04-25 22:28:53 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.09 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자연수 뒤집어 배열로 만들기 - 12932 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=java) 
 
 ### 성능 요약
 
-메모리: 9.43 MB, 시간: 0.04 ms
+메모리: 77.1 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 25일 22:15:31
+2025년 04월 25일 22:28:51
 
 ### 문제 설명
 
--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,17 +1,12 @@
 class Solution {
     public int[] solution(long n) {
-        
         String s = Long.toString(n);
-        char[] arr = new StringBuilder(s).reverse()
-                .toString()
-                .toCharArray();
+        String reversed = new StringBuilder(s).reverse().toString();
         
-        int[] answer = new int[arr.length];
-        
-        for (int i = 0; i < arr.length; i++) {
-            answer[i] = Character.getNumericValue(arr[i]);
+        int[] answer = new int[reversed.length()];
+        for (int i = 0; i < reversed.length(); i++) {
+            answer[i] = Character.getNumericValue(reversed.charAt(i));
         }
-            
         return answer;
     }
 }
