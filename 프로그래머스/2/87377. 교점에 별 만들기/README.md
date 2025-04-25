d9009844678164da012b041c28854a50ec2021a1
예성 <66625672+yeseong31@users.noreply.github.com>
2025-04-25 15:03:05 +0900

    [level 2] Title: 교점에 별 만들기, Time: 267.46 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/README.md
+++ 프로그래머스/2/87377. 교점에 별 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 교점에 별 만들기 - 87377 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 267.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 15:56:16
+2025년 04월 25일 15:02:58
 
 ### 문제 설명
 
--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.py
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.py
@@ -1,31 +1,36 @@
 from itertools import combinations
 
+INF = int(1e12)
+
 
 def solution(line):
     points = set()
-    min_x = min_y = int(1e15)
-    max_x = max_y = -int(1e15)
     
-    for lineA, lineB in combinations(line, 2):
-        (a, b, e), (c, d, f) = lineA, lineB
-
+    min_x, min_y = INF, INF
+    max_x, max_y = -INF, -INF
+    
+    for (a, b, e), (c, d, f) in combinations(line, 2):
         if a * d == b * c:
             continue
         
-        x = (b * f - e * d) / (a * d - b * c)
-        y = (e * c - a * f) / (a * d - b * c)
+        x = ((b * f) - (e * d)) / ((a * d) - (b * c))
+        y = ((e * c) - (a * f)) / ((a * d) - (b * c))
         
-        if x == int(x) and y == int(y):
-            x, y = int(x), int(y)
-            min_x, min_y = min(x, min_x), min(y, min_y)
-            max_x, max_y = max(x, max_x), max(y, max_y)
-            points.add((x, y))
-    
+        if x != int(x) or y != int(y):
+            continue
+            
+        x, y = int(x), int(y)
+        min_x, min_y = min(x, min_x), min(y, min_y)
+        max_x, max_y = max(x, max_x), max(y, max_y)
+        points.add((x, y))
+                       
     width = max_x - min_x + 1
     height = max_y - min_y + 1
-    board = [['.'] * width for _ in range(height)]
-    
+    answer = [['.'] * width for _ in range(height)]
+
     for x, y in points:
-        board[max_y - y][x - min_x] = '*'
-        
-    return [''.join(row) for row in board]
+        col = x - min_x
+        row = max_y - y
+        answer[row][col] = '*'
+    
+    return [''.join(row) for row in answer]
