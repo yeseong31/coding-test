f2404c89f775c28c601a95090975ff09780ec22b
예성 <66625672+yeseong31@users.noreply.github.com>
2025-04-28 23:08:53 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.39 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -2,18 +2,12 @@ import re
 
 
 def solution(new_id):
-    new_id = new_id.lower()
-    new_id = re.sub('[^a-z0-9\_\-\.]', '', new_id)
-    new_id = re.sub('[.]+', '.', new_id)
-    new_id = new_id.strip('.')
-    
-    if not new_id:
-        new_id = 'a'
-    
-    new_id = new_id[:15]
-    new_id = new_id.strip('.')
-    
-    while len(new_id) < 3:
-        new_id += new_id[-1]
-    
-    return new_id
+    s = new_id.lower()
+    s = re.sub('[^a-z0-9\_\-\.]', '', s)
+    s = re.sub('[.]+', '.', s)
+    s = s.strip('.')
+    s = s if s else 'a'
+    s = s[:15]
+    s = s.strip('.')
+    s = s if len(s) >= 3 else s + ''.join(s[-1] for _ in range(3 - len(s)))
+    return s
