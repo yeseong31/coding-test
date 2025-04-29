a33759fefa8675878365bd84392932eadc131c86
예성 <66625672+yeseong31@users.noreply.github.com>
2025-04-29 22:55:56 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.55 ms, Memory: 9.53 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,58 +1,55 @@
 import re
+
+from collections import deque
 from itertools import permutations
-
-
-def toPostFix(tokens, priority):
-    stack = []
-    result = []
-    
-    for token in tokens:
-        if token.isdigit():
-            result.append(token)
-            continue
-        
-        if not stack:
-            stack.append(token)
-            continue
-        
-        while stack and priority[token] <= priority[stack[-1]]:
-            result.append(stack.pop())
-        
-        stack.append(token)
-        
-    while stack:
-        result.append(stack.pop())
-    
-    return result
-
-
-def calculate(tokens):
-    stack = []
-    
-    for token in tokens:
-        if token.isdigit():
-            stack.append(int(token))
-            continue
-            
-        num2, num1 = stack.pop(), stack.pop()
-        if token == '+':
-            stack.append(num1 + num2)
-        elif token == '-':
-            stack.append(num1 - num2)
-        else:
-            stack.append(num1 * num2)
-    
-    return stack.pop()
+from math import inf
 
 
 def solution(expression):
-    answer = 0
-    tokens = re.split(r'([-+*])|\s+', expression)
-    operators = ('+', '-', '*')
+    numbers = list(re.sub('[^\d]', '|', expression).split('|'))
+    ops = list(re.sub('[\d]', '', expression))
     
-    for _case in map(list, permutations(operators)):
-        priority = {o: p for p, o in enumerate(list(_case))}
-        postfix = toPostFix(tokens, priority)
-        answer = max(answer, abs(calculate(postfix)))
+    exp = [numbers[0]]
+    for i, op in enumerate(ops):
+        exp.append(op)
+        exp.append(numbers[i + 1])
+    
+    answer = -inf
+    for priority_ops in permutations('+-*', 3):
+        result = calculate_result(exp, priority_ops)
+        answer = max(answer, abs(result))
     
     return answer
+
+
+def calculate_result(exp, priority_ops):
+    exp_q = deque(exp)
+    mid_q = deque()
+    
+    for priority_op in priority_ops:
+        while exp_q:
+            x = exp_q.popleft()
+            
+            if x.isdigit() or x != priority_op:
+                mid_q.append(x)
+                continue
+            
+            n1 = mid_q.pop()
+            n2 = exp_q.popleft()
+            mid_q.append(calculate(n1, n2, x))
+        
+        exp_q = mid_q.copy()
+        mid_q = deque()
+        
+    result = exp_q.popleft()
+    return int(result)
+        
+
+def calculate(number1, number2, op):
+    number1, number2 = int(number1), int(number2)
+    if op == '+':
+        return str(number1 + number2)
+    elif op == '-':
+        return str(number1 - number2)
+    else:
+        return str(number1 * number2)
