a93faca725af7510ac79a50a442659f5bad729bf
예성 <66625672+yeseong31@users.noreply.github.com>
2025-04-29 15:01:45 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 244.62 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -1,20 +1,20 @@
 def solution(arr):
-    def calculate(a, b, c, d):
-        zero = one = 0
-        for z, o in a, b, c, d:
-            zero += z
-            one += o
-        return zero, one
+    answer = [0, 0]
+    separate(arr, 0, 0, len(arr), answer)
+    return answer
+
+
+def separate(arr, x, y, n, answer):
+    value = arr[x][y]
+    k = n // 2
     
-    def dfs(x, y, size):
-        w = size // 2
-        for i in range(x, x + size):
-            for j in range(y, y + size):
-                if arr[i][j] != arr[x][y]:
-                    return calculate(dfs(x, y, w), dfs(x + w, y, w), dfs(x, y + w, w) , dfs(x + w, y + w, w))
-                
-        if arr[x][y] == 0:
-            return 1, 0
-        return 0, 1
-        
-    return dfs(0, 0, len(arr))
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if arr[i][j] != value:
+                separate(arr, x, y, k, answer)
+                separate(arr, x, y + k, k, answer)
+                separate(arr, x + k, y, k, answer)
+                separate(arr, x + k, y + k, k, answer)
+                return
+    
+    answer[value] += 1
