7d5acccea8635795aa7506a3b01a1a2c0b81702e
예성 <66625672+yeseong31@users.noreply.github.com>
2025-04-29 21:24:10 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
 
 ### 성능 요약
 
-메모리: 9.16 MB, 시간: 0.17 ms
+메모리: 91.2 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 29일 21:19:54
+2025년 04월 29일 21:24:08
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int area = brown + yellow;
-        
-        for (int n = 3; n <= (int) Math.sqrt(area); n++) {
-            if (area % n == 0 && (n - 2) * ((area / n) - 2) == yellow) {
-                return new int[] {area / n, n};
+        for (int height = 3; height < (int) Math.sqrt(area) + 1; height++) {
+            if (area % height != 0) {
+                continue;
+            }
+            int width = area / height;
+            if (area - (width - 2) * (height - 2) == brown) {
+                return new int[] {width, height};
             }
         }
-        
         return null;
     }
 }
