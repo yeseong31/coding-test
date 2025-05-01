c8da8b7ed8dae13fcaf655bb9d5b660280540e35
예성 <66625672+yeseong31@users.noreply.github.com>
2025-05-01 22:15:15 +0900

    [level 3] Title: 불량 사용자, Time: 73.96 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/README.md
+++ 프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064?language=java) 
 
 ### 성능 요약
 
-메모리: 9.13 MB, 시간: 6136.42 ms
+메모리: 128 MB, 시간: 73.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 01일 17:35:10
+2025년 05월 01일 22:15:14
 
 ### 문제 설명
 
--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -2,38 +2,36 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+
 class Solution {
+    private void count(int index, Set<String> banned, String[][] bans, Set<Set<String>> checkedBan) {
+        if (index == bans.length) {
+            checkedBan.add(new HashSet<>(banned));
+            return;
+        }
+
+        for (String id : bans[index]) {
+            if (banned.contains(id)) {
+                continue;
+            }
+            banned.add(id);
+            count(index + 1, banned, bans, checkedBan);
+            banned.remove(id);
+        }
+    }
+    
     public int solution(String[] user_id, String[] banned_id) {
         int answer = 0;
-        
-        String[][] bannedList = Arrays.stream(banned_id)
+
+        String[][] bans = Arrays.stream(banned_id)
                 .map(bid -> bid.replace("*", "."))
                 .map(bid -> Arrays.stream(user_id)
                         .filter(uid -> uid.matches(bid))
                         .toArray(String[]::new))
                 .toArray(String[][]::new);
-        
-        Set<Set<String>> result = new HashSet<>();
-        
-        dfs(0, new HashSet<>(), bannedList, result);
-        
-        return result.size();
-    }
-    
-    private void dfs(int currentIndex, Set<String> currentBanned, String[][] bannedList, Set<Set<String>> result) {
-        if (currentIndex == bannedList.length) {
-            result.add(new HashSet<>(currentBanned));
-            return;
-        }
-        
-        for (String bid : bannedList[currentIndex]) {
-            if (currentBanned.contains(bid)) {
-                continue;
-            }
-            
-            currentBanned.add(bid);
-            dfs(currentIndex + 1, currentBanned, bannedList, result);
-            currentBanned.remove(bid);
-        }
+
+        Set<Set<String>> checkedBan = new HashSet<>();
+        count(0, new HashSet<>(), bans, checkedBan);
+        return checkedBan.size();
     }
 }
