dab556f6191bbf1251999c838671ece0bdb6795b
예성 <66625672+yeseong31@users.noreply.github.com>
2025-05-09 22:46:16 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.95 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,11 +1,10 @@
 def solution(board, moves):
     stack = []
-    n = len(board)
-    height = [0] * n
+    height = [0] * len(board)
     cnt = 0
 
-    for r in range(n):
-        for c in range(n):
+    for r in range(len(board)):
+        for c in range(len(board)):
             if board[r][c] > 0:
                 height[c] += 1
 
@@ -13,8 +12,8 @@ def solution(board, moves):
         if height[m - 1] == 0:
             continue
         
-        catch = board[n - height[m - 1]][m - 1]
-        board[n - height[m - 1]][m - 1] = 0
+        catch = board[len(board) - height[m - 1]][m - 1]
+        board[len(board) - height[m - 1]][m - 1] = 0
         height[m - 1] -= 1
         
         if stack and stack[-1] == catch:
