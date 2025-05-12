cd88e6f7931df3a65166b39784b9bd28195addbc
예성 <66625672+yeseong31@users.noreply.github.com>
2025-05-12 17:12:40 +0900

    [level 3] Title: 등굣길, Time: 3.39 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 2.43 ms
+메모리: 9.41 MB, 시간: 3.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 0일 18:55:36
+2025년 05월 12일 17:12:37
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -2,9 +2,20 @@ def solution(m, n, puddles):
     board = [[0] * m for _ in range(n)]
     board[0][0] = 1
     
+    for x, y in puddles:
+        board[y - 1][x - 1] = -1
+    
     for i in range(n):
         for j in range(m):
-            if [j + 1, i + 1] not in puddles and (i, j) != (0, 0):
-                board[i][j] = (board[i - 1][j] + board[i][j - 1]) % 1000000007
+            if board[i][j] == -1:
+                board[i][j] = 0
+                continue
+                
+            if i != 0:
+                board[i][j] += board[i - 1][j]
+            if j != 0:
+                board[i][j] += board[i][j - 1]
+            
+            board[i][j] %= 1_000_000_007
     
-    return board[-1][-1]
+    return board[n - 1][m - 1]
