04a2b41f790382fada961f166b8d8f36f7b85170
예성 <66625672+yeseong31@users.noreply.github.com>
2025-05-15 17:47:03 +0900

    [level 5] Title: 방의 개수, Time: 666.32 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/README.md
+++ 프로그래머스/5/49190. 방의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 5] 방의 개수 - 49190 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49190?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49190?language=python3) 
 
 ### 성능 요약
 
-메모리: 534 MB, 시간: 804.33 ms
+메모리: 122 MB, 시간: 666.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 13:54:42
+2025년 05월 15일 17:47:01
 
 ### 문제 설명
 
--- 프로그래머스/5/49190. 방의 개수/방의 개수.py
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.py
@@ -1,34 +1,42 @@
+from collections import defaultdict
+
+
 class Vertex:
     def __init__(self, x, y):
         self.x = x;
         self.y = y;
-        self.id = (x, y)
-        self.connects = set()
+        self.id = self.get_id(x, y)
+        self.connected = set()
+        
+    @staticmethod
+    def get_id(x, y):
+        return f'{x}_{y}'
 
 
 def solution(arrows):
-    answer = 0
-    dx, dy = (-1, -1, 0, 1, 1, 1, 0, -1), (0, 1, 1, 1, 0, -1, -1, -1)
+    dx, dy = (0, 1, 1, 1, 0, -1, -1, -1), (1, 1, 0, -1, -1, -1, 0, 1)
     
-    v = Vertex(0, 0)
+    x, y, count = 0, 0, 0
+    v = Vertex(x, y)
     
-    vertices = {}
+    vertices = defaultdict()
     vertices[v.id] = v
     
     for arrow in arrows:
         for _ in range(2):
-            x, y = v.x + dx[arrow], v.y + dy[arrow]
+            nx, ny = x + dx[arrow], y + dy[arrow]
+            _id = Vertex.get_id(nx, ny)
             
-            if (x, y) not in vertices:
-                vertices[(x, y)] = Vertex(x, y)
-            elif (x, y) not in v.connects:
-                answer += 1
+            if _id not in vertices:
+                vertices[_id] = Vertex(nx, ny)
+            elif _id not in v.connected:
+                count += 1
             
-            next_v = vertices[(x, y)]
+            nv = vertices[_id]
+            v.connected.add(nv.id)
+            nv.connected.add(v.id)
             
-            v.connects.add(next_v.id)
-            next_v.connects.add(v.id)
-            
-            v = next_v
+            x, y = nx, ny
+            v = vertices[_id]
     
-    return answer
+    return count
