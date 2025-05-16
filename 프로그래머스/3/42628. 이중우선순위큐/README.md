dfa7dbb1c517e246ffdfdca766b9dd1ae340186b
예성 <66625672+yeseong31@users.noreply.github.com>
2025-05-16 22:21:45 +0900

    [level 3] Title: 이중우선순위큐, Time: 673.02 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=java) 
 
 ### 성능 요약
 
-메모리: 15.5 MB, 시간: 5700.48 ms
+메모리: 118 MB, 시간: 673.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 16일 21:58:16
+2025년 05월 16일 22:21:43
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,90 +1,50 @@
+import java.util.Iterator;
 import java.util.PriorityQueue;
 
-class DoublePriorityQueue {
-    
+class Solution {
     private final PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
     private final PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-    private int size;
     
-    public DoublePriorityQueue() {
-    }
-    
-    public void add(int value) {
-        maxHeap.add(value);
-        minHeap.add(value);
-        size++;
-    }
-    
-    public void removeMaxValue() {
-        if (size == 0) {
-            return;
-        }
-        
-        size--;
-        maxHeap.poll();
-        
-        if (size == 0) {
-            clearHeap();
-        }
-    }
-    
-    public void removeMinValue() {
-        if (size == 0) {
-            return;
-        }
-        
-        size--;
-        minHeap.poll();
-
-        if (size == 0) {
-            clearHeap();
-        }
-    }
-    
-    public int receiveMaxValue() {
-        if (size == 0) {
-            return 0;
-        }
-        return maxHeap.peek();
-    }
-    
-    public int receiveMinValue() {
-        if (size == 0) {
-            return 0;
-        }
-        return minHeap.peek();
-    }
-    
-    private void clearHeap() {
-        maxHeap.clear();
-        minHeap.clear();
-    }
-}
-
-class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = {};
-        DoublePriorityQueue doublePriorityQueue = new DoublePriorityQueue();
-        
         for (String operation : operations) {
             String[] tokens = operation.split(" ");
-            int value = Integer.parseInt(tokens[1]);
+            String op = tokens[0];
+            String numStr = tokens[1];
             
-            if (tokens[0].equals("I")) {
-                doublePriorityQueue.add(value);
-                continue;
-            }
-            
-            if (value < 0) {
-                doublePriorityQueue.removeMinValue();
-            } else {
-                doublePriorityQueue.removeMaxValue();
+            switch (op) {
+                case "I" -> {
+                    int number = Integer.parseInt(numStr);
+                    maxHeap.add(number);
+                    minHeap.add(number);
+                }
+                case "D" -> {
+                    if (maxHeap.size() == 0) {
+                        continue;
+                    }
+                    if (numStr.equals("-1")) {
+                        minHeap.poll();
+                        maxHeap.clear();
+                        
+                        Iterator<Integer> iter = minHeap.iterator();
+                        while (iter.hasNext()) {
+                            maxHeap.add(iter.next());
+                        }
+                    } else {
+                        maxHeap.poll();
+                        minHeap.clear();
+                        
+                        Iterator<Integer> iter = maxHeap.iterator();
+                        while (iter.hasNext()) {
+                            minHeap.add(iter.next());
+                        }
+                    }
+                }
             }
         }
         
-        return new int[] {
-            doublePriorityQueue.receiveMaxValue(),
-            doublePriorityQueue.receiveMinValue()
-        };
+        if (maxHeap.size() == 0 || minHeap.size() == 0) {
+            return new int[] {0, 0};
+        }
+        return new int[] {maxHeap.peek(), minHeap.peek()};
     }
 }
