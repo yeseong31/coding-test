d113d6822f2ec38db2f0ca57fa8806628278bb7b
예성 <66625672+yeseong31@users.noreply.github.com>
2025-05-17 13:53:36 +0900

    [level 2] Title: 서버 증설 횟수, Time: 1.66 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/README.md
+++ 프로그래머스/2/389479. 서버 증설 횟수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 서버 증설 횟수 - 389479 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479?language=java) 
 
 ### 성능 요약
 
-메모리: 9.18 MB, 시간: 0.40 ms
+메모리: 71.2 MB, 시간: 1.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 24일 17:24:43
+2025년 05월 17일 13:53:33
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -0,0 +1,29 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] players, int m, int k) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(24);
+        
+        for (int t = 0; t < players.length; t++) {
+            int p = players[t];
+            
+            while (!pq.isEmpty() && pq.peek() == t) {
+                pq.poll();
+            }
+            
+            if (p < pq.size() * m) {
+                continue;
+            }
+            
+            while (pq.size() * m <= p) {
+                pq.add(t + k);
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
