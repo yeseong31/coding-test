1af38649e690e51daba8ce097f57a32026cc1455
예성 <66625672+yeseong31@users.noreply.github.com>
2025-05-20 21:21:41 +0900

    [level 1] Title: 명예의 전당 (1), Time: 5.04 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/README.md
+++ 프로그래머스/1/138477. 명예의 전당 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 명예의 전당 (1) - 138477 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477?language=java) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 0.52 ms
+메모리: 73.8 MB, 시간: 5.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 15일 21:29:11
+2025년 05월 20일 21:21:38
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < score.length; i++) {
+            pq.offer(score[i]);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+            answer[i] = pq.peek();
+        }
+        
+        return answer;
+    }
+}
