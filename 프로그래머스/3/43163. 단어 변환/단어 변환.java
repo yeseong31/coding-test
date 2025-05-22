8d716d6d6da53f520b763d2f21cacd31e8f9bd5e
예성 <66625672+yeseong31@users.noreply.github.com>
2025-05-22 16:17:09 +0900

    [level 3] Title: 단어 변환, Time: 6.38 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,65 +1,74 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
-class State {
-    
-    private final int depth;
-    private final String word;
-    
-    public State(int depth, String word) {
-        this.depth = depth;
-        this.word = word;
-    }
-    
-    public int getDepth() {
-        return depth;
-    }
-    
-    public String getWord() {
-        return word;
-    }
-}
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 class Solution {
+    class Element {
+        private final Set<String> checked = new HashSet<>();
+        
+        public String word;
+        public int seq;
+        
+        public Element(String word, int seq) {
+            this.word = word;
+            this.seq = seq;
+        }
+        
+        public boolean isChecked(String word) {
+            return checked.contains(word);
+        }
+        
+        public void check(String word) {
+            checked.add(word);
+        }
+    }
     
     public int solution(String begin, String target, String[] words) {
-        if (!Arrays.asList(words).contains(target)) {
+        if (!List.of(words).contains(target)) {
             return 0;
         }
         
-        Queue<State> queue = new LinkedList<>();
-        queue.add(new State(0, begin));
+        int answer = words.length + 1;
+        
+        Deque<Element> queue = new ArrayDeque<>();
+        queue.add(new Element(begin, 0));
+        
+        int seq;
+        String word;
         
         while (!queue.isEmpty()) {
-            State state = queue.poll();
+            Element e = queue.pollFirst();
+            seq = e.seq;
+            word = e.word;
             
-            if (state.getWord().equals(target)) {
-                return state.getDepth();
+            if (seq > words.length) {
+                continue;
             }
-            if (state.getDepth() > words.length) {
-                return 0;
+            if (word.equals(target)) {
+                return seq;
             }
-            
-            for (String word : words) {
-                if (isOneCharDiff(word, state.getWord())) {
-                    queue.add(new State(state.getDepth() + 1, word));
+                
+            for (String next : words) {
+                if (e.isChecked(next)) {
+                    continue;
+                }
+                
+                int count = 0;
+                for (int i = 0; i < word.length(); i++) {
+                    if (word.charAt(i) == next.charAt(i)) {
+                        count++;
+                    }
+                }
+                
+                if (count == word.length() - 1) {
+                    e.check(next);
+                    queue.offerLast(new Element(next, seq + 1));
                 }
             }
         }
         
         return 0;
-    }
-    
-    private boolean isOneCharDiff(String wordA, String wordB) {
-        int count = 0;
-        
-        for (int index = 0; index < wordA.length(); index++) {
-            if (wordA.charAt(index) == wordB.charAt(index)) {
-                count++;
-            }
-        }
-        
-        return count == wordA.length() - 1;
     }
 }
