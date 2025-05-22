e3db26d92cae989210cc96eb8860957b9420c8fd
예성 <66625672+yeseong31@users.noreply.github.com>
2025-05-22 13:11:28 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.61 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.33 ms
+메모리: 9.31 MB, 시간: 0.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 10일 21:01:08
+2025년 05월 22일 13:11:23
 
 ### 문제 설명
 
--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,36 +1,58 @@
-def solution(numbers, hand):
-    answer = []
-    lx, ly, rx, ry = 3, 0, 3, 2
-    dic = {
-        1: (0, 0), 2: (0, 1), 3: (0, 2),
-        4: (1, 0), 5: (1, 1), 6: (1, 2),
-        7: (2, 0), 8: (2, 1), 9: (2, 2), 0: (3, 1)
-    }
-    
-    for number in numbers:
-        if number not in (2, 5, 8, 0):
-            if number in (1, 4, 7):           
-                answer.append('L')
-                lx, ly = dic[number]
-            else:
-                answer.append('R')
-                rx, ry = dic[number]
-            continue
+from math import sqrt
 
-        ld = abs(lx - dic[number][0]) + abs(ly - dic[number][1])
-        rd = abs(rx - dic[number][0]) + abs(ry - dic[number][1])
-        if ld == rd:
-            if hand == 'right':
-                answer.append('R')
-                rx, ry = dic[number]
-            else:
-                answer.append('L')
-                lx, ly = dic[number]
-        elif ld < rd:
-            answer.append('L')
-            lx, ly = dic[number]
-        else:
-            answer.append('R')
-            rx, ry = dic[number]
+
+class Point:
+    def __init__(self, x, y):
+        self.keypad = {
+            1: (0, 0), 2: (0, 1), 3: (0, 2),
+            4: (1, 0), 5: (1, 1), 6: (1, 2),
+            7: (2, 0), 8: (2, 1), 9: (2, 2),
+            0: (3, 1),
+        }
+        self.x = x
+        self.y = y
+    
+    def move_to(self, number):
+        nx, ny = self.keypad[number]
+        self.x = nx
+        self.y = ny
         
-    return ''.join(answer)
+    def get_distance(self, number):
+        nx, ny = self.keypad[number]
+        return abs(self.x - nx) + abs(self.y - ny)
+        
+
+def solution(numbers, hand):
+    result = []
+    left = Point(3, 0)
+    right = Point(3, 2)
+    
+    for n in numbers:
+        if n in (1, 4, 7):
+            result.append('L')
+            left.move_to(n)
+            continue
+            
+        if n in (3, 6, 9):
+            result.append('R')
+            right.move_to(n)
+            continue
+            
+        left_dist = left.get_distance(n)
+        right_dist = right.get_distance(n)
+
+        if left_dist == right_dist:
+            if hand == 'right':
+                result.append('R')
+                right.move_to(n)
+            else:
+                result.append('L')
+                left.move_to(n)
+        elif left_dist < right_dist:
+            result.append('L')
+            left.move_to(n)
+        else:
+            result.append('R')
+            right.move_to(n)
+    
+    return ''.join(result)
