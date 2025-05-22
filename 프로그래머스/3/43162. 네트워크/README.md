3a7e98868ba25b055ad558fc46de36ef6f3c5dfa
예성 <66625672+yeseong31@users.noreply.github.com>
2025-05-22 13:57:52 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.21 ms
+메모리: 9.3 MB, 시간: 0.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 01일 15:19:09
+2025년 05월 22일 13:57:48
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,23 @@
+def find_parent(parents, x):
+    if parents[x] != x:
+        parents[x] = find_parent(parents, parents[x])
+    return parents[x]
+
+def union_parent(parents, a, b):
+    a = find_parent(parents, a)
+    b = find_parent(parents, b)
+    if a < b:
+        parents[b] = a
+    else:
+        parents[a] = b
+
 def solution(n, computers):
-    def dfs(x):
-        visited[x] = True
-        
-        for i in range(n):
-            if computers[x][i] == 1 and not visited[i]:
-                dfs(i)
-    
-    
-    answer = 0
-    visited = [False] * n
+    parents = list(range(n))
     
     for i in range(n):
-        if not visited[i]:
-            dfs(i)
-            answer += 1
+        for j in range(i):
+            if i != j and computers[i][j] == 1:
+                union_parent(parents, i, j)
     
-    return answer
+    root_set = set(find_parent(parents, i) for i in range(n))
+    return len(root_set)
