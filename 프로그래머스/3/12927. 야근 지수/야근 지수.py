3edcd8c285c4ee209d4442ac9f20ef4738e4bb71
예성 <66625672+yeseong31@users.noreply.github.com>
2025-05-30 22:57:37 +0900

    [level 3] Title: 야근 지수, Time: 254.34 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,14 +1,14 @@
 def solution(n: int, works: list):
     works.sort(reverse=True)
-    length = len(works)
 
     while n > 0:
-        for i in range(length):
+        for i, _ in enumerate(works):
             works[i] -= 1
             n -= 1
             
             if n == 0:
                 return sum(x ** 2 for x in works if x > 0)
-            
-            if i < length - 1 and works[i] >= works[i + 1]:
+            if i < len(works) - 1 and works[i] >= works[i + 1]:
                 break
+                
+    return 0
