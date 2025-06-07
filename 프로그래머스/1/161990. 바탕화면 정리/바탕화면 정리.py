0787968bf3d55200460f4e6b205c4b6f10f95773
예성 <66625672+yeseong31@users.noreply.github.com>
2025-06-07 19:36:55 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.81 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -6,10 +6,12 @@ def solution(wallpaper):
     
     for i in range(n):
         for j in range(m):
-            if wallpaper[i][j] != '.':
-                lux = min(lux, i)
-                luy = min(luy, j)
-                rdx = i + 1
-                rdy = max(rdy, j + 1)
+            if wallpaper[i][j] == '.':
+                continue
+                
+            lux = min(lux, i)
+            luy = min(luy, j)
+            rdx = i + 1
+            rdy = max(rdy, j + 1)
             
     return lux, luy, rdx, rdy
