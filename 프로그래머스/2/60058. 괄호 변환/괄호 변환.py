7b7d16130feba9ff2c879b7e7d4b72b053c4d6c9
예성 <66625672+yeseong31@users.noreply.github.com>
2025-06-09 22:10:04 +0900

    [level 2] Title: 괄호 변환, Time: 0.82 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,41 @@
+def is_valid(s):
+    stack = []
+    
+    for c in s:
+        if c == '(':
+            stack.append(c)
+        elif stack:
+            stack.pop()
+        else:
+            return False
+    
+    return not stack
+
+
+def reverse_parenthesis(s, dic):
+    return ''.join(dic[c] for c in s)
+
+
+def solution(p):
+    dic = {
+        '(': ')',
+        ')': '('
+    }
+    
+    if p == '':
+        return p
+    
+    u, b = [], 0
+    for c in p:
+        b += 1 if c == '(' else -1
+        u.append(c)
+        
+        if b == 0:
+            break
+    
+    v = ''.join(p[len(u):])
+    
+    if is_valid(u):
+        return f"{''.join(u)}{solution(v)}"
+    else:
+        return f'({solution(v)}){reverse_parenthesis(u[1:-1], dic)}'
