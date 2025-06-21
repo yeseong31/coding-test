67dca796899cec5840027fe3c2690a1896d4bb78
devys <66625672+yeseong31@users.noreply.github.com>
2025-06-21 20:27:23 +0900

    [level 1] Title: 옹알이 (2), Time: 0.11 ms, Memory: 9.03 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,10 +1,13 @@
 def solution(babbling):
     check = ['aya', 'ye', 'woo', 'ma']
     answer = 0
+    
     for v in babbling:
         for c in check:
             if c * 2 not in v:
                 v = v.replace(c, ' ')
+        
         if not v.split():
             answer += 1
+        
     return answer
