ac941d7cfa526cad055284031059e0531adc0d7e
devys <66625672+yeseong31@users.noreply.github.com>
2025-07-01 15:16:55 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 24.81 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,15 +1,9 @@
-import java.util.Arrays;
-
 class Solution {
-    private int countZero(String s) {
-        return s.replaceAll("1", "").length();
-    }
-    
     public int[] solution(String s) {
         int[] answer = {0, 0};
         
         while (!s.equals("1")) {
-            int zero = countZero(s);
+            int zero = s.replaceAll("1", "").length();
             
             answer[0]++;
             answer[1] += zero;
