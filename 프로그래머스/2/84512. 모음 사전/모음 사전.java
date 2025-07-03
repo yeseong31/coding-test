d2ae8650a665f248568f03fc2c2f7f1ee646f137
devys <66625672+yeseong31@users.noreply.github.com>
2025-07-03 18:19:29 +0900

    [level 2] Title: 모음 사전, Time: 2.12 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,31 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 class Solution {
-    private static final char[] vowels = new char[] {'A', 'E', 'I', 'O', 'U'};
+    private static final char[] vowels = {'A', 'E', 'I', 'O', 'U'};
     
-    private void makeDictionary(StringBuilder sb, List<String> dic) {
-        if (sb.toString().length() == 5) {
-            return;
-        }
+    private static void dfs(StringBuilder sb, List<String> words) {
+        words.add(sb.toString());
+        if (sb.length() == 5) return;
         
         for (char vowel : vowels) {
             sb.append(vowel);
-            dic.add(sb.toString());
-            
-            makeDictionary(sb, dic);
-            
+            dfs(sb, words);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
     
     public int solution(String word) {
-        List<String> dic = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        
-        makeDictionary(sb, dic);
-        
-        return dic.indexOf(word) + 1;
+        List<String> words = new ArrayList<>();
+        dfs(new StringBuilder(), words);
+        return words.indexOf(word);
     }
 }
