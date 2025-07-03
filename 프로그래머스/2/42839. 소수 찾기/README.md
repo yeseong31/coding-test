d7dd87542ad5a7bcc61cca2b3821e8c88ef43dff
devys <66625672+yeseong31@users.noreply.github.com>
2025-07-03 21:52:23 +0900

    [level 2] Title: 소수 찾기, Time: 26.03 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 81 MB, 시간: 3.62 ms
+메모리: 95.8 MB, 시간: 26.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 01일 15:34:56
+2025년 07월 03일 21:52:21
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,48 +1,41 @@
 import java.util.HashSet;
 import java.util.Set;
 
-
 class Solution {
-    private boolean isPrimeNumber(int x) {
-        if (x <= 1) {
-            return false;
-        }
-        
-        for (int i = 2; i <= Math.sqrt(x); i++) {
-            if (x % i == 0) {
-                return false;
+    private final Set<Integer> primeSet = new HashSet<>();
+    
+    private void generatePermutations(String current, String numbers, boolean[] visited) {
+        if (!current.isEmpty()) {
+            int num = Integer.parseInt(current);
+            if (isPrime(num)) {
+                primeSet.add(num);
             }
         }
-        
+
+        for (int i = 0; i < numbers.length(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                generatePermutations(current + numbers.charAt(i), numbers, visited);
+                visited[i] = false;
+            }
+        }
+    }
+
+    private boolean isPrime(int number) {
+        if (number < 2) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            if (number % i == 0) return false;
+        }
+
         return true;
     }
     
-    private void getPrimeNums(int acc, int[] nums, boolean[] isUsed, Set<Integer> result) {
-        if (isPrimeNumber(acc)) {
-            result.add(acc);
-        }
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (isUsed[i]) {
-                continue;
-            }
-            
-            int newAcc = acc * 10 + nums[i];
-            
-            isUsed[i] = true;
-            getPrimeNums(newAcc, nums, isUsed, result);
-            isUsed[i] = false;
-        }
-    }
-    
     public int solution(String numbers) {
-        Set<Integer> result = new HashSet<>();
-        
-        int[] nums = numbers.chars()
-                .map(c -> Character.getNumericValue(c))
-                .toArray();
-        
-        getPrimeNums(0, nums, new boolean[nums.length], result);
-        return result.size();
+        boolean[] visited = new boolean[numbers.length()];
+        generatePermutations("", numbers, visited);
+        return primeSet.size();
     }
 }
