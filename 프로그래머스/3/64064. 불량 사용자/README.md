75e398893bd63abcd4a66c62dc66aa5bf535604e
devys <66625672+yeseong31@users.noreply.github.com>
2025-07-04 21:45:47 +0900

    [level 3] Title: 불량 사용자, Time: 88.04 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/README.md
+++ 프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
 
 ### 성능 요약
 
-메모리: 128 MB, 시간: 73.96 ms
+메모리: 133 MB, 시간: 88.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 01일 22:15:14
+2025년 07월 04일 21:45:44
 
 ### 문제 설명
 
--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,37 +1,50 @@
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
-
 class Solution {
-    private void count(int index, Set<String> banned, String[][] bans, Set<Set<String>> checkedBan) {
-        if (index == bans.length) {
-            checkedBan.add(new HashSet<>(banned));
+    private static final Map<String, Set<String>> bannedIdMap = new HashMap<>();
+    
+    private static void dfs(String[] banned_id, int seq, Set<String> current, Set<Set<String>> answer) {
+        if (seq == banned_id.length) {
+            if (current.size() == banned_id.length) {
+                answer.add(new HashSet<>(current));
+            }
             return;
         }
-
-        for (String id : bans[index]) {
-            if (banned.contains(id)) {
+        
+        for (String bid : bannedIdMap.get(banned_id[seq])) {
+            if (current.contains(bid)) {
                 continue;
             }
-            banned.add(id);
-            count(index + 1, banned, bans, checkedBan);
-            banned.remove(id);
+
+            current.add(bid);
+            dfs(banned_id, seq + 1, current, answer);
+            current.remove(bid);
         }
     }
     
     public int solution(String[] user_id, String[] banned_id) {
-        int answer = 0;
-
-        String[][] bans = Arrays.stream(banned_id)
-                .map(bid -> bid.replace("*", "."))
-                .map(bid -> Arrays.stream(user_id)
-                        .filter(uid -> uid.matches(bid))
-                        .toArray(String[]::new))
-                .toArray(String[][]::new);
-
-        Set<Set<String>> checkedBan = new HashSet<>();
-        count(0, new HashSet<>(), bans, checkedBan);
-        return checkedBan.size();
+        for (String bid : banned_id) {
+            String regex = bid.replace("*", ".");
+            if (bannedIdMap.containsKey(regex)) {
+                continue;
+            }
+            
+            Set<String> matchedIds = new HashSet<>();
+            for (String uid : user_id) {
+                if (uid.matches(regex)) {
+                    matchedIds.add(uid);
+                }
+            }
+            
+            bannedIdMap.put(bid, matchedIds);
+        }
+        
+        Set<Set<String>> answer = new HashSet<>();
+        dfs(banned_id, 0, new HashSet<>(), answer);
+        
+        return answer.size();
     }
 }
