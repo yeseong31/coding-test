13975954a763fce6192e676ab485c4c8208d2ab9
devys <66625672+yeseong31@users.noreply.github.com>
2025-07-05 22:27:53 +0900

    [level 2] Title: 순위 검색, Time: 1425.14 ms, Memory: 336 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -4,88 +4,78 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 class Solution {
+    private void getKeyCombinations(String[] tokens, int seq, String current, List<String> keyCombinations) {
+        if (seq == tokens.length - 1) {
+            if (current.length() > 0) {
+                keyCombinations.add(current);
+            }
+            return;
+        }
+        
+        getKeyCombinations(tokens, seq + 1, current + tokens[seq], keyCombinations);
+        getKeyCombinations(tokens, seq + 1, current + "-", keyCombinations);
+    }
+    
+    private int binarySearch(List<Integer> values, int score) {
+        int left = 0;
+        int right = values.size() - 1;
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            
+            if (values.get(mid) >= score) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        if (values.get(left) < score) {
+            return values.size();
+        }
+        return left;
+    }
     
     public int[] solution(String[] info, String[] query) {
         List<Integer> answer = new ArrayList<>();
         Map<String, List<Integer>> scores = new HashMap<>();
         
-        for (String s : info) {
-            String[] tokens = s.split(" ");
-            int score = Integer.parseInt(tokens[4]);
+        for (String i : info) {
+            String[] tokens = i.split(" ");
+            int score = Integer.parseInt(tokens[tokens.length - 1]);
             
-            List<String> conditions = new ArrayList<>();
-            record(0, new StringBuilder(), tokens, conditions);
+            List<String> keyCombinations = new ArrayList<>();
+            getKeyCombinations(tokens, 0, new String(), keyCombinations);
             
-            for (String key : conditions) {
-                scores.computeIfAbsent(key, k -> new ArrayList<>());
-                scores.get(key).add(score);
+            for (String key : keyCombinations) {
+                scores.computeIfAbsent(key, k -> new ArrayList<>()).add(score);
             }
         }
         
         for (List<Integer> value : scores.values()) {
             Collections.sort(value);
         }
-        
+
         for (String q : query) {
-            String[] tokens = q.replace(" and ", " ").split(" ");
-            
+            String[] tokens = q.replaceAll(" and ", " ").split(" ");
             String key = String.join("", Arrays.copyOf(tokens, tokens.length - 1));
+            
             if (!scores.containsKey(key)) {
                 answer.add(0);
                 continue;
             }
             
-            List<Integer> sortedList = scores.get(key);
+            List<Integer> values = scores.get(key);
             int score = Integer.parseInt(tokens[tokens.length - 1]);
             
-            int count = binarySearch(sortedList, score);
-            answer.add(sortedList.size() - count);
+            int result = binarySearch(values, score);
+            answer.add(values.size() - result);
         }
         
         return answer.stream()
-                .mapToInt(Integer::new)
+                .mapToInt(Integer::valueOf)
                 .toArray();
-    }
-    
-    private static int binarySearch(List<Integer> sortedList, int score) {
-        int start = 0;
-        int end = sortedList.size() - 1;
-        
-        while (start < end) {
-            int mid = (start + end) / 2;
-            int target = sortedList.get(mid);
-            
-            if (target < score) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-        
-        if (sortedList.get(start) < score) {
-            return sortedList.size();
-        }
-        return start;
-    }
-    
-    private static void record(int index, StringBuilder sb, String[] tokens, List<String> conditions) {
-        if (index == tokens.length - 1) {
-            if (sb.length() > 0) {
-                String key = sb.toString();
-                conditions.add(key);
-            }
-            return;
-        }
-        
-        sb.append(tokens[index]);
-        record(index + 1, sb, tokens, conditions);
-        sb.delete(sb.length() - tokens[index].length(), sb.length());
-        
-        sb.append("-");
-        record(index + 1, sb, tokens, conditions);
-        sb.deleteCharAt(sb.length() - 1);
     }
 }
