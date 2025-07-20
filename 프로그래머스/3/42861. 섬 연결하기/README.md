f3aa9260994e261f21d550a1a29e1148b9563c5a
devys <66625672+yeseong31@users.noreply.github.com>
2025-07-20 18:08:11 +0900

    [level 3] Title: 섬 연결하기, Time: 7.05 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 89.4 MB, 시간: 1.04 ms
+메모리: 88.2 MB, 시간: 7.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 17일 11:58:50
+2025년 07월 20일 18:08:07
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,31 +1,28 @@
-import java.util.PriorityQueue;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Node {
+    public final int a;
+    public final int b;
+    public final int c;
+    
+    public Node(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
 
 class Solution {
-    private class Node implements Comparable<Node> {
-        public int a;
-        public int b;
-        public int c;
-        
-        public Node(int a, int b, int c) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-        }
-        
-        @Override
-        public int compareTo(Node other) {
-            return this.c - other.c;
-        }
-    }
     
-    private int findParent(int[] parents, int x) {
+    private static int findParent(int[] parents, int x) {
         if (parents[x] != x) {
-            parents[x] = this.findParent(parents, parents[x]);
+            parents[x] = findParent(parents, parents[x]);
         }
         return parents[x];
     }
     
-    private void unionParent(int[] parents, int a, int b) {
+    private static void unionParent(int[] parents, int a, int b) {
         a = findParent(parents, a);
         b = findParent(parents, b);
         
@@ -37,31 +34,28 @@ class Solution {
     }
     
     public int solution(int n, int[][] costs) {
-        int answer = 0;
-        PriorityQueue<Node> pq = new PriorityQueue<>();
         
-        for (int[] cost : costs) {
-            int a = cost[0];
-            int b = cost[1];
-            int c = cost[2];
-            
-            pq.add(new Node(a, b, c));
-        }
+        Node[] nodes = Arrays.stream(costs)
+                .map(v -> new Node(v[0], v[1], v[2]))
+                .sorted(Comparator.comparingInt(v -> v.c))
+                .toArray(Node[]::new);
         
         int[] parents = new int[n];
         for (int i = 0; i < n; i++) {
             parents[i] = i;
         }
         
-        while (!pq.isEmpty()) {
-            Node node = pq.poll();
+        int answer = 0;
+        for (int seq = 0; seq < nodes.length; seq++) {
+            int a = nodes[seq].a;
+            int b = nodes[seq].b;
             
-            if (findParent(parents, node.a) != findParent(parents, node.b)) {
-                unionParent(parents, node.a, node.b);
-                answer += node.c;
+            if (findParent(parents, a) != findParent(parents, b)) {
+                unionParent(parents, a, b);
+                answer += nodes[seq].c;
             }
         }
-
+        
         return answer;
     }
 }
