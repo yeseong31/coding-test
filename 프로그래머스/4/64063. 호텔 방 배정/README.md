d5565f491e134767fb0f78e8e709439ba36ca328
devys <66625672+yeseong31@users.noreply.github.com>
2025-07-20 19:32:20 +0900

    [level 4] Title: 호텔 방 배정, Time: 355.81 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/README.md
+++ 프로그래머스/4/64063. 호텔 방 배정/README.md
@@ -1,10 +1,10 @@
 # [level 4] 호텔 방 배정 - 64063 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64063?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64063) 
 
 ### 성능 요약
 
-메모리: 154 MB, 시간: 421.89 ms
+메모리: 161 MB, 시간: 355.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 17일 12:43:59
+2025년 07월 20일 19:32:17
 
 ### 문제 설명
 
--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -2,25 +2,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    private long findEmptyRoom(Map<Long, Long> rooms, long room) {
-        if (!rooms.containsKey(room)) {
-            rooms.put(room, room + 1);
-            return room;
+    
+    private static long findEmptyRoom(Map<Long, Long> reserved, long current) {
+        if (!reserved.containsKey(current)) {
+            reserved.put(current, current + 1);
+            return current;
         }
-
-        long next = findEmptyRoom(rooms, rooms.get(room));
-        rooms.put(room, next);
+        
+        long next = findEmptyRoom(reserved, reserved.get(current));
+        reserved.put(current, next);
         return next;
     }
-
+    
     public long[] solution(long k, long[] room_number) {
         long[] answer = new long[room_number.length];
-        Map<Long, Long> rooms = new HashMap<>();
-
+        Map<Long, Long> reserved = new HashMap<>();
+        
         for (int i = 0; i < room_number.length; i++) {
-            answer[i] = findEmptyRoom(rooms, room_number[i]);
+            answer[i] = findEmptyRoom(reserved, room_number[i]);
         }
-
+        
         return answer;
     }
 }
