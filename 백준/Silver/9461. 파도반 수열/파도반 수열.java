c9f2788fd2bbfaf48240d4ff4e73ca1b9d560e0f
devys <66625672+yeseong31@users.noreply.github.com>
2025-07-27 15:56:16 +0900

    [Silver III] Title: 파도반 수열, Time: 116 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[101];
+        Arrays.fill(dp, -1);
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+
+        for (int i = 6; i <= 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
+
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
