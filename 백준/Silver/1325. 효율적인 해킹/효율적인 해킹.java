903ec8ba81a87b58abfe7b37b9c5e090ee0bbc2a
devys <66625672+yeseong31@users.noreply.github.com>
2025-08-10 14:32:43 +0900

    [Silver I] Title: 효율적인 해킹, Time: 9924 ms, Memory: 148616 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static List<List<Integer>> graph;
+    private static boolean[] visited;
+    private static int[] count;
+    private static int c;
+
+    private static void dfs(int curr) {
+        visited[curr] = true;
+        c++;
+
+        for (int next : graph.get(curr)) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(b).add(a);
+        }
+
+        int maxCount = 0;
+        count = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            visited = new boolean[n + 1];
+            c = 0;
+
+            dfs(i);
+
+            count[i] = c;
+            maxCount = Math.max(maxCount, c);
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            if (count[i] == maxCount) {
+                sb.append(i).append(" ");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
