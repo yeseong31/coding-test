5e8e4cbcb170914a64ba2afb1e407bda3f686020
devys <66625672+yeseong31@users.noreply.github.com>
2025-08-23 23:41:17 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1908 ms, Memory: 98260 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,37 @@
+import collections
+import sys
+
+input = sys.stdin.readline
+
+# 도시의 개수 n, 도로의 개수 m, 거리 정보 k, 출발 도시의 번호 x
+n, m, k, x = map(int, input().split())
+# a번에서 b번으로 이동하는 도로
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+# 거리 테이블
+distance = [-1] * (n + 1)
+# 시작 노드는 거리가 0
+distance[x] = 0
+
+# bfs
+q = collections.deque([x])
+while q:
+    node = q.popleft()
+    for v in graph[node]:
+        # 아직 방문하지 않았다면 최단 거리 갱신
+        if distance[v] == -1:
+            distance[v] = distance[node] + 1
+            q.append(v)
+
+# 전체적으로 거리 테이블 탐색
+flag = False
+for i, d in enumerate(distance):
+    if d == k:
+        flag = True
+        print(i)
+
+if not flag:
+    print(-1)
