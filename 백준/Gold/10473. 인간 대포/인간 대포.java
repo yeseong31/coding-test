bbb48254d0f7e9f240965b08dbbaac216b2d7401
devys <66625672+yeseong31@users.noreply.github.com>
2025-09-04 12:03:22 +0900

    [Gold II] Title: 인간 대포, Time: 124 ms, Memory: 14756 KB -BaekjoonHub

--- 백준/Gold/10473. 인간 대포/인간 대포.java
+++ 백준/Gold/10473. 인간 대포/인간 대포.java
@@ -2,191 +2,84 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    private static final double walkDist = 5;
-    private static final double cannonDist = 50;
-    private static final int cannonTime = 2;
-
-    private static int n;
+    static int n;
+    static double[][] points;
+    static double[] dist;
+    static PriorityQueue<double[]> pq = new PriorityQueue<>(
+            (o1, o2) -> Double.compare(o1[1], o2[1])
+    );
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
-        double x, y;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        x = Double.parseDouble(st.nextToken());
-        y = Double.parseDouble(st.nextToken());
-        Point src = new Point(x, y, 0);
+        double startX = Double.parseDouble(st.nextToken());
+        double startY = Double.parseDouble(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        x = Double.parseDouble(st.nextToken());
-        y = Double.parseDouble(st.nextToken());
-        Point dst = new Point(x, y, 0);
+        double endX = Double.parseDouble(st.nextToken());
+        double endY = Double.parseDouble(st.nextToken());
 
         n = Integer.parseInt(br.readLine());
-        Point[] cannons = new Point[n];
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            x = Double.parseDouble(st.nextToken());
-            y = Double.parseDouble(st.nextToken());
-            cannons[i] = new Point(x, y, 0);
-        }
-
-        double answer = solution(src, dst, cannons);
-        sb.append(String.format("%.6f", answer));
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
-
-    private static double solution(Point src, Point dst, Point[] cannons) {
-        int totalPoints = n + 2;
-        List<List<Edge>> graph = new ArrayList<>();
-
-        for (int i = 0; i < totalPoints; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < n; i++) {
-            double dist = calculateDist(src, cannons[i]);
-            double time = dist / walkDist;
-            graph.get(0).add(new Edge(i + 1, time));
-        }
-
-        double directDist = calculateDist(src, dst);
-        graph.get(0).add(new Edge(n + 1, directDist / walkDist));
-
-        for (int i = 0; i < n; i++) {
-            double dist = calculateDist(cannons[i], src);
-            double walkTime = dist / walkDist;
-            double cannonTime = calculateCannonTime(dist);
-            double minTime = Math.min(walkTime, cannonTime);
-            graph.get(i + 1).add(new Edge(0, minTime));
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j) {
-                    double dist = calculateDist(cannons[i], cannons[j]);
-                    double walkTime = dist / walkDist;
-                    double cannonTime = calculateCannonTime(dist);
-                    double minTime = Math.min(walkTime, cannonTime);
-
-                    graph.get(i + 1).add(new Edge(j + 1, minTime));
-                }
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            double dist = calculateDist(cannons[i], dst);
-            double walkTime = dist / walkDist;
-            double cannonTime = calculateCannonTime(dist);
-            double minTime = Math.min(walkTime, cannonTime);
-
-            graph.get(i + 1).add(new Edge(n + 1, minTime));
-        }
-
-        for (int i = 0; i < n; i++) {
-            double dist = calculateDist(dst, cannons[i]);
-            double time = dist / walkDist;
-            graph.get(n + 1).add(new Edge(i + 1, time));
-        }
-
-        double srcToDstDist = calculateDist(dst, src);
-        graph.get(n + 1).add(new Edge(0, srcToDstDist / walkDist));
-
-        return dijkstra(graph, 0, n + 1, totalPoints);
-    }
-
-    private static double calculateCannonTime(double dist) {
-        if (dist == cannonDist) {
-            return cannonTime;
-        }
-
-        return cannonTime + Math.abs(dist - cannonDist) / walkDist;
-    }
-
-    private static double dijkstra(List<List<Edge>> graph, int start, int end, int totalPoints) {
-        double[] dist = new double[totalPoints];
+        points = new double[n + 2][2];
+        dist = new double[n + 2];
         Arrays.fill(dist, Double.MAX_VALUE);
-        dist[start] = 0;
 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
+        points[0][0] = startX;
+        points[0][1] = startY;
+        points[n + 1][0] = endX;
+        points[n + 1][1] = endY;
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            points[i][0] = Double.parseDouble(st.nextToken());
+            points[i][1] = Double.parseDouble(st.nextToken());
+        }
+
+        dijkstra();
+        
+        System.out.printf("%.6f\n", dist[n + 1]);
+    }
+
+    static void dijkstra() {
+        dist[0] = 0;
+        pq.offer(new double[]{0, 0});
 
         while (!pq.isEmpty()) {
-            Node current = pq.poll();
-            int currentNode = current.node;
-            double currentDist = current.distance;
+            double[] cur = pq.poll();
+            int idx = (int)cur[0];
 
-            if (currentDist > dist[currentNode]) {
-                continue;
-            }
+            if (cur[1] > dist[idx]) continue;
 
-            for (Edge edge : graph.get(currentNode)) {
-                int nextNode = edge.to;
-                double newDist = currentDist + edge.weight;
+            for (int i = 0; i <= n + 1; i++) {
+                if (idx == i) continue;
 
-                if (newDist < dist[nextNode]) {
-                    dist[nextNode] = newDist;
-                    pq.offer(new Node(nextNode, newDist));
+                double time = calculateTime(idx, i);
+
+                if (dist[i] > dist[idx] + time) {
+                    dist[i] = dist[idx] + time;
+                    pq.offer(new double[]{i, dist[i]});
                 }
             }
         }
-
-        return dist[end];
     }
 
-    private static double calculateDist(Point p1, Point p2) {
-        double deltaX = p2.x - p1.x;
-        double deltaY = p2.y - p1.y;
-        return Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
-    }
+    static double calculateTime(int from, int to) {
+        double distance = getDistance(from, to);
 
-    private static class Point implements Comparable<Point> {
-        final double x;
-        final double y;
-        final double t;
-
-        Point(double x, double y, double t) {
-            this.x = x;
-            this.y = y;
-            this.t = t;
-        }
-
-        @Override
-        public int compareTo(Point o) {
-            return Double.compare(this.t, o.t);
+        if (from == 0 || from == n + 1) {
+            return distance / 5.0;
+        } else {
+            double walkTime = distance / 5.0;
+            double cannonTime = 2.0 + Math.abs(distance - 50.0) / 5.0;
+            return Math.min(walkTime, cannonTime);
         }
     }
 
-    private static class Edge {
-        int to;
-        double weight;
-
-        Edge(int to, double weight) {
-            this.to = to;
-            this.weight = weight;
-        }
-    }
-
-    private static class Node implements Comparable<Node> {
-        int node;
-        double distance;
-
-        Node(int node, double distance) {
-            this.node = node;
-            this.distance = distance;
-        }
-
-        @Override
-        public int compareTo(Node other) {
-            return Double.compare(this.distance, other.distance);
-        }
+    static double getDistance(int from, int to) {
+        double dx = points[to][0] - points[from][0];
+        double dy = points[to][1] - points[from][1];
+        return Math.sqrt(dx * dx + dy * dy);
     }
 }
