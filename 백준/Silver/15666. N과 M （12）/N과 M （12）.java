9e0b57a4944fb6a4e57d52932602833876b736e4
devys <66625672+yeseong31@users.noreply.github.com>
2025-09-05 10:23:21 +0900

    [Silver II] Title: N과 M (12), Time: 120 ms, Memory: 15588 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.java
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -1,17 +1,22 @@
 import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
     private static final List<Integer> numbers = new ArrayList<>();
     private static final boolean[] visited = new boolean[10_001];
+    private static final StringBuilder sb = new StringBuilder();
 
     private static int n;
     private static int m;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
@@ -27,24 +32,27 @@ public class Main {
         }
 
         Collections.sort(numbers);
-
         dfs(0, 0, new ArrayList<>());
 
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
         br.close();
     }
 
     private static void dfs(int start, int depth, List<Integer> result) {
         if (depth == m) {
-            System.out.println(result.stream()
-                    .map(String::valueOf)
-                    .collect(Collectors.joining(" ")));
+            for (int i = 0; i < m; i++) {
+                sb.append(result.get(i)).append(" ");
+            }
+            sb.append("\n");
             return;
         }
 
         for (int i = start; i < numbers.size(); i++) {
             result.add(numbers.get(i));
             dfs(i, depth + 1, result);
-            result.remove(result.size() - 1);
+            result.remove(depth);
         }
     }
 }
