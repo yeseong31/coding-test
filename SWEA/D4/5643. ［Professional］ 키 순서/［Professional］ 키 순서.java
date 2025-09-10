09675f55104386ff3f024abe53fbf70bce22f8fa
devys <66625672+yeseong31@users.noreply.github.com>
2025-09-10 11:55:48 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,647 ms, Memory: 100,040 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine().trim());
+
+        for (int testCase = 1; testCase <= T; testCase++) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int m = Integer.parseInt(br.readLine().trim());
+
+            boolean[][] checked = new boolean[n + 1][n + 1];
+
+            for (int i = 0; i < m; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                checked[a][b] = true;
+            }
+
+            for (int k = 1; k <= n; k++) {
+                for (int i = 1; i <= n; i++) {
+                    for (int j = 1; j <= n; j++) {
+                        if (checked[i][k] && checked[k][j]) {
+                            checked[i][j] = true;
+                        }
+                    }
+                }
+            }
+
+            int answer = 0;
+
+            for (int i = 1; i <= n; i++) {
+                int count = 0;
+
+                for (int j = 1; j <= n; j++) {
+                    if (i == j) continue;
+                    if (checked[i][j] || checked[j][i]) count++;
+                }
+
+                if (count == n - 1) answer++;
+            }
+
+            sb.append("#").append(testCase).append(" ").append(answer).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
