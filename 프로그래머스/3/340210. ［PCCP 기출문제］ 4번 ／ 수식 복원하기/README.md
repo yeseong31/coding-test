e042bb10caaadcea4d02147b5a34f7b5d2c1f6d1
devys <66625672+yeseong31@users.noreply.github.com>
2025-09-22 22:07:29 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 11.91 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/README.md
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] [PCCP 기출문제] 4번 / 수식 복원하기 - 340210 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340210?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340210) 
 
 ### 성능 요약
 
-메모리: 76.9 MB, 시간: 72.84 ms
+메모리: 77.9 MB, 시간: 11.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 21일 23:58:00
+2025년 09월 22일 22:07:26
 
 ### 문제 설명
 
--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -3,68 +3,57 @@ import java.util.*;
 class Solution {
 
     private static final List<Integer> toSolve = new ArrayList<>();
-    private static final Set<Integer> possibleSysNums = new HashSet<>();
+    private static final List<Integer> candidates = new ArrayList<>();
 
-    private static int[][] nums;
+    private static int[] nums1;
+    private static int[] nums2;
+    private static int[] results;
     private static char[] ops;
-    private static boolean[] isValid;
 
     public static String[] solution(String[] expressions) {
         int n = expressions.length;
 
-        nums = new int[n][3];
+        nums1 = new int[n];
+        nums2 = new int[n];
+        results = new int[n];
         ops = new char[n];
-        isValid = new boolean[n];
 
         int maxDigit = -1;
 
-        for (int idx = 0; idx < n; idx++) {
-            String[] tokens = expressions[idx].split(" ");
-            
-            int num1 = Integer.parseInt(tokens[0]);
-            int num2 = Integer.parseInt(tokens[2]);
-            int res = (!tokens[4].equals("X")) ? Integer.parseInt(tokens[4]) : -1;
-            char op = tokens[1].charAt(0);
+        for (int i = 0; i < n; i++) {
+            String[] tokens = expressions[i].split(" ");
 
-            nums[idx][0] = num1;
-            nums[idx][1] = num2;
-            nums[idx][2] = res;
-            ops[idx] = op;
+            nums1[i] = Integer.parseInt(tokens[0]);
+            nums2[i] = Integer.parseInt(tokens[2]);
+            results[i] = (!tokens[4].equals("X")) ? Integer.parseInt(tokens[4]) : -1;
+            ops[i] = tokens[1].charAt(0);
 
-            maxDigit = Math.max(maxDigit, getMaxNum(num1, num2, res));
+            maxDigit = Math.max(maxDigit, getMaxNum(nums1[i], nums2[i], results[i]));
 
-            if (res != -1) {
-                isValid[idx] = true;
-            } else {
-                toSolve.add(idx);
-            }
+            if (results[i] == -1) toSolve.add(i);
         }
 
-        for (int sysNum = maxDigit + 1; sysNum <= 9; sysNum++) {
+        for (int k = maxDigit + 1; k <= 9; k++) {
             boolean matchAll = true;
 
-            for (int idx = 0; idx < n; idx++) {
-                if (!isValid[idx]) continue;
-
-                int res = calculate(idx, sysNum);
-
-                if (res != nums[idx][2]) {
+            for (int i = 0; i < n; i++) {
+                if (!toSolve.contains(i) && calculate(i, k) != results[i]) {
                     matchAll = false;
                     break;
                 }
             }
 
-            if (matchAll) possibleSysNums.add(sysNum);
+            if (matchAll) candidates.add(k);
         }
 
-        for (int idx = 0; idx < n; idx++) {
-            if (isValid[idx]) continue;
+        for (int i = 0; i < n; i++) {
+            if (!toSolve.contains(i)) continue;
 
             int res = -1;
             boolean matchAll = true;
 
-            for (int sysNum : possibleSysNums) {
-                int tmp = calculate(idx, sysNum);
+            for (int k : candidates) {
+                int tmp = calculate(i, k);
 
                 if (res == -1) {
                     res = tmp;
@@ -74,60 +63,54 @@ class Solution {
                 }
             }
 
-            if (matchAll) nums[idx][2] = res;
+            if (matchAll) results[i] = res;
         }
 
         return toSolve.stream()
-                .map(Solution::getExp)
+                .map(i ->
+                        String.format("%d %c %d = %s",
+                                nums1[i], ops[i], nums2[i],
+                                (results[i] != -1) ? results[i] : "?"))
                 .toArray(String[]::new);
     }
 
-    private static String getExp(int idx) {
-        int num1 = nums[idx][0];
-        int num2 = nums[idx][1];
-        int res = nums[idx][2];
-        char op = ops[idx];
-        return num1 + " " + op + " " + num2 + " = " + (res != -1 ? res : "?");
-    }
-
-    private static int getMaxNum(int num1, int num2, int res) {
-        int max1 = Math.max(num1 / 10, num1 % 10);
-        int max2 = Math.max(num2 / 10, num2 % 10);
+    private static int getMaxNum(int a, int b, int res) {
+        int max1 = Math.max(a / 10, a % 10);
+        int max2 = Math.max(b / 10, b % 10);
         int max3 = (res != -1) ? Math.max((res % 100) / 10, res % 10) : -1;
 
         return Math.max(max1, Math.max(max2, max3));
     }
 
-    private static int calculate(int idx, int sysNum) {
-        int num1 = convertToDecimal(nums[idx][0], sysNum);
-        int num2 = convertToDecimal(nums[idx][1], sysNum);
+    private static int calculate(int i, int k) {
+        int a = convertToDecimal(nums1[i], k);
+        int b = convertToDecimal(nums2[i], k);
 
-        int res = (ops[idx] == '+') ? num1 + num2 : num1 - num2;
+        int res = (ops[i] == '+') ? a + b : a - b;
 
-        return convertFromDecimal(res, sysNum);
+        return convertFromDecimal(res, k);
     }
 
-    private static int convertToDecimal(int num, int sysNum) {
+    private static int convertToDecimal(int x, int k) {
         int res = 0;
         int pow = 1;
 
-        while (num > 0) {
-            int digit = num % 10;
-            res += digit * pow;
-            pow *= sysNum;
-            num /= 10;
+        while (x > 0) {
+            res += (x % 10) * pow;
+            pow *= k;
+            x /= 10;
         }
 
         return res;
     }
 
-    private static int convertFromDecimal(int num, int sysNum) {
+    private static int convertFromDecimal(int x, int k) {
         int res = 0;
         int pow = 1;
 
-        while (num > 0) {
-            res += (num % sysNum) * pow;
-            num /= sysNum;
+        while (x > 0) {
+            res += (x % k) * pow;
+            x /= k;
             pow *= 10;
         }
 
