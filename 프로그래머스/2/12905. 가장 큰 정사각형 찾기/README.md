79697b8830eeca84f1b68f10d92d257e81d44df0
devys <66625672+yeseong31@users.noreply.github.com>
2025-10-07 22:56:55 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 18.89 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/README.md
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 정사각형 찾기 - 12905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905?language=java) 
 
 ### 성능 요약
 
-메모리: 38 MB, 시간: 600.37 ms
+메모리: 90.2 MB, 시간: 18.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 27일 22:00:57
+2025년 10월 07일 22:56:52
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[][] board) {
+        int answer = 0;
+
+        int row = board.length;
+        int col = board[0].length;
+        int n = Math.max(row, col);
+
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (board[i - 1][j - 1] != 0) {
+                    dp[i][j] = Math.min(
+                        Math.min(dp[i][j - 1], dp[i - 1][j]),
+                        dp[i - 1][j - 1]
+                    ) + 1;
+                    answer = Math.max(answer, dp[i][j]);
+                }
+            }
+        }
+
+        return answer * answer;
+    }
+}
