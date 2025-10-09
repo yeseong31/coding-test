d65232c8531b7192fa1cbfa926f363386422e759
devys <66625672+yeseong31@users.noreply.github.com>
2025-10-09 23:12:01 +0900

    [level 1] Title: 유연근무제, Time: 93.72 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/README.md
+++ 프로그래머스/1/388351. 유연근무제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 유연근무제 - 388351 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 85.28 ms
+메모리: 156 MB, 시간: 93.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 08일 21:58:17
+2025년 10월 09일 23:11:58
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -0,0 +1,44 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+class Solution {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HHmm");
+
+    private String convertTime(int time) {
+        String timeStr = String.format("%04d", time);
+        LocalTime targetTime = LocalTime.parse(timeStr, FORMATTER);
+        return targetTime.format(FORMATTER);
+    }
+
+    private String getDeadlineTime(String timeStr) {
+        LocalTime deadline = LocalTime.parse(timeStr, FORMATTER).plusMinutes(10);
+        return deadline.format(FORMATTER);
+    }
+
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int answer = 0;
+        startday -= 1;
+
+        for (int i = 0; i < schedules.length; i++) {
+            String scheduleTime = convertTime(schedules[i]);
+            String deadline = getDeadlineTime(scheduleTime);
+
+            int count = 0;
+            int[] timelog = timelogs[i];
+
+            for (int j = 0; j < timelog.length; j++) {
+                int day = (startday + j) % 7;
+                
+                if (day >= 5) continue;
+
+                String logTime = convertTime(timelog[j]);
+                if (logTime.compareTo(deadline) <= 0) count++;
+            }
+
+            if (count == 5) answer++;
+        }
+
+        return answer;
+    }
+}
