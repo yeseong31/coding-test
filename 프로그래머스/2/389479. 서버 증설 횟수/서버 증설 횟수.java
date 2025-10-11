2ab78e5d6cdf425ea0923603aef3fe5c13e59fb4
devys <66625672+yeseong31@users.noreply.github.com>
2025-10-11 09:29:08 +0900

    [level 2] Title: 서버 증설 횟수, Time: 1.58 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,27 +1,24 @@
 import java.util.PriorityQueue;
 
 class Solution {
+
     public int solution(int[] players, int m, int k) {
         int answer = 0;
-        
+
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         pq.offer(24);
-        
+
         for (int t = 0; t < players.length; t++) {
-            int p = players[t];
-            
             while (!pq.isEmpty() && pq.peek() == t) {
                 pq.poll();
             }
-            
-            if (p < pq.size() * m) continue;
-            
-            while (pq.size() * m <= p) {
+
+            while (players[t] >= pq.size() * m) {
                 pq.offer(t + k);
                 answer++;
             }
         }
-        
+
         return answer;
     }
 }
