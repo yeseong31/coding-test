539de4f89612105b07e37494cece719701f9b85e
devys <66625672+yeseong31@users.noreply.github.com>
2025-10-14 23:23:56 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.61 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,39 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
+public class Solution {
     private static final int[] dx = {-1, 0, 1, 0, -1, 1, 1, -1, -2, 0, 2, 0};
     private static final int[] dy = {0, 1, 0, -1, 1, 1, -1, -1, 0, 2, 0, -2};
-    
-    private class Point {
-        private final int x;
-        private final int y;
-        
-        private Point(int x, int y) {
+
+    public static class Point {
+        public final int x;
+        public final int y;
+
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
-        
-        public int getX() {
-            return x;
-        }
-        
-        public int getY() {
-            return y;
-        }
     }
-    
+
     private boolean isDistanced(char[][] place, Point p) {
-        int x = p.getX();
-        int y = p.getY();
+        int x = p.x;
+        int y = p.y;
 
         for (int i = 0; i < 12; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5 || place[nx][ny] != 'P') {
-                continue;
-            }
+            if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5 || place[nx][ny] != 'P') continue;
 
             int distX = Math.abs(nx - x);
             int distY = Math.abs(ny - y);
@@ -47,47 +37,42 @@ class Solution {
                 if (place[nx][my] == 'O') return false;
             } else if (distX == 1 && distY == 1) {
                 if (!(place[nx][y] == 'X' && place[x][ny] == 'X')) return false;
-            } else if (place[nx][ny] == 'P') {
+            } else {
                 return false;
             }
         }
-        
+
         return true;
     }
-    
+
     private boolean isPossiblePlace(char[][] place) {
         List<Point> persons = new ArrayList<>();
-        
+
         for (int x = 0; x < 5; x++) {
             for (int y = 0; y < 5; y++) {
-                if (place[x][y] == 'P') {
-                    persons.add(new Point(x, y));
-                }
+                if (place[x][y] == 'P') persons.add(new Point(x, y));
             }
         }
-        
+
         for (Point person : persons) {
-            if (!isDistanced(place, person)) {
-                return false;
-            }
+            if (!isDistanced(place, person)) return false;
         }
-        
+
         return true;
     }
-    
+
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
-        
+
         for (int i = 0; i < places.length; i++) {
             char[][] place = new char[5][];
             for (int j = 0; j < 5; j++) {
                 place[j] = places[i][j].toCharArray();
             }
-            
-            if (isPossiblePlace(place)) answer[i] = 1;
-            else answer[i] = 0;
+
+            answer[i] = isPossiblePlace(place) ? 1 : 0;
         }
-        
+
         return answer;
     }
 }
