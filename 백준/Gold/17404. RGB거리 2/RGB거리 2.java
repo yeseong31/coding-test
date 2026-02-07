0c756420c98707d1ee1c1acd64eb5c28accf2c05
devys <66625672+yeseong31@users.noreply.github.com>
2026-02-07 22:13:32 +0900

    [Gold IV] Title: RGB거리 2, Time: 116 ms, Memory: 14792 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -22,44 +22,35 @@ public class Main {
         }
 
         int answer = INF;
-        answer = Math.min(answer, paintHouse(0, colors));
-        answer = Math.min(answer, paintHouse(1, colors));
-        answer = Math.min(answer, paintHouse(2, colors));
-        System.out.println(answer);
-    }
-
-    private static int paintHouse(int start, int[][] colors) {
-        int result = INF;
         int[] dp = new int[3];
 
-        Arrays.fill(dp, INF);
-        dp[start] = colors[0][start];
+        for (int start = 0; start < 3; start++) {
+            Arrays.fill(dp, INF);
+            dp[start] = colors[0][start];
 
-        for (int i = 1; i < colors.length; i++) {
-            int[] next = new int[3];
-            Arrays.fill(next, INF);
+            for (int i = 1; i < colors.length; i++) {
+                int[] next = new int[3];
+                Arrays.fill(next, INF);
 
-            for (int j = 0; j < 3; j++) {
-                if (dp[j] == INF) {
-                    continue;
+                for (int j = 0; j < 3; j++) {
+                    if (dp[j] != INF) {
+                        int l = (j + 2) % 3;
+                        int r = (j + 1) % 3;
+                        next[l] = Math.min(next[l], dp[j] + colors[i][l]);
+                        next[r] = Math.min(next[r], dp[j] + colors[i][r]);
+                    }
                 }
 
-                int l = (j + 2) % 3;
-                int r = (j + 1) % 3;
-
-                next[l] = Math.min(next[l], dp[j] + colors[i][l]);
-                next[r] = Math.min(next[r], dp[j] + colors[i][r]);
+                dp = next;
             }
 
-            dp = next;
-        }
-
-        for (int i = 0; i < 3; i++) {
-            if (i != start) {
-                result = Math.min(result, dp[i]);
+            for (int i = 0; i < 3; i++) {
+                if (i != start) {
+                    answer = Math.min(answer, dp[i]);
+                }
             }
         }
 
-        return result;
+        System.out.println(answer);
     }
 }
