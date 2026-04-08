d47587b54c80620d8e181ee7749f4d90c348dd1c
devys <66625672+yeseong31@users.noreply.github.com>
2026-04-08 21:15:40 +0900

    [level 2] Title: 요격 시스템, Time: 438.76 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/README.md
+++ 프로그래머스/2/181188. 요격 시스템/README.md
@@ -1,10 +1,10 @@
 # [level 2] 요격 시스템 - 181188 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188?language=python3) 
 
 ### 성능 요약
 
-메모리: 193 MB, 시간: 364.99 ms
+메모리: 90.3 MB, 시간: 438.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 11일 10:13:10
+2026년 04월 08일 21:15:36
 
 ### 문제 설명
 
--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,12 +1,17 @@
 def solution(targets):
     answer = 0
-    targets.sort()
-    
-    while targets:
-        start = targets.pop()[0]
+    n = len(targets)
+
+    targets.sort(key=lambda x: x[0])
+
+    idx = n - 1
+
+    while idx >= 0:
+        start = targets[idx][0]
+
+        while idx >= 0 and start < targets[idx][1]:
+            idx -= 1
+
         answer += 1
-        
-        while targets and start < targets[-1][1]:
-            targets.pop()
-    
+
     return answer
