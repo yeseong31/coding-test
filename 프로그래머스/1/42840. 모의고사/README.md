7c543767d9821c81acf4afef0ed8d59a642cd0ef
devys <66625672+yeseong31@users.noreply.github.com>
2026-04-22 21:16:58 +0900

    [level 1] Title: 모의고사, Time: 10.10 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 5.13 ms
+메모리: 93.2 MB, 시간: 10.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 29일 17:11:42
+2026년 04월 22일 21:16:56
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,41 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-    private static final int[][] persons = new int[][] {
+    
+    private static final int[][] persons = {
         {1, 2, 3, 4, 5},
         {2, 1, 2, 3, 2, 4, 2, 5},
-        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5},
+        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
     };
-    
+
     public int[] solution(int[] answers) {
-        int[] count = new int[] {0, 0, 0};
-        
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < answers.length; j++) {
-                if (persons[i][j % persons[i].length] == answers[j]) {
-                    count[i]++;
-                }
-            }
-        }
-        
-        int maxCount = -1;
-        for (int i = 0; i < count.length; i++) {
-            if (maxCount < count[i]) {
-                maxCount = count[i];
-            }
-        }
-        
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] == maxCount) {
-                result.add(i + 1);
-            }
-        }
-        
-        return result.stream()
-            .mapToInt(i -> i)
+
+        int[] count = IntStream.range(0, persons.length)
+            .map(i -> (int) IntStream.range(0, answers.length)
+                .filter(j -> persons[i][j % persons[i].length] == answers[j])
+                .count())
+            .toArray();
+
+        int max = Arrays.stream(count).max().getAsInt();
+
+        return IntStream.range(0, count.length)
+            .filter(i -> count[i] == max)
+            .map(i -> i + 1)
             .toArray();
     }
 }
