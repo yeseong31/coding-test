e7e4a5e3623438b01c8f2ec416982769e139b1db
devys <66625672+yeseong31@users.noreply.github.com>
2026-04-28 21:11:27 +0900

    [level 2] Title: 미로 탈출, Time: 6.27 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -2,56 +2,63 @@ import java.util.*;
 
 class Solution {
 
-    public final int[] dx = {-1, 0, 1, 0};
-    public final int[] dy = {0, 1, 0, -1};
-
-    public static class Point {
-        public int x, y, step;
-
-        public Point(int x, int y, int step) {
-            this.x = x;
-            this.y = y;
-            this.step = step;
-        }
-    }
+    private static final int[] dx = {-1, 0, 1, 0};
+    private static final int[] dy = {0, 1, 0, -1};
 
     public int solution(String[] maps) {
-        Map<Character, Point> points = new HashMap<>();
+        int n = maps.length;
+        int m = maps[0].length();
 
-        for (int i = 0; i < maps.length; i++) {
-            for (int j = 0; j < maps[0].length(); j++) {
+        int[] start = null;
+        int[] lever = null;
+        int[] end = null;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 char c = maps[i].charAt(j);
-                if (c == 'S' || c == 'E' || c == 'L') {
-                    points.put(c, new Point(i, j, 0));
+                if (c == 'S') {
+                    start = new int[]{i, j};
+                } else if (c == 'L') {
+                    lever = new int[]{i, j};
+                } else if (c == 'E') {
+                    end = new int[]{i, j};
                 }
             }
         }
 
-        int startToLever = bfs('S', 'L', maps, points);
-        int leverToEnd = bfs('L', 'E', maps, points);
+        int sToL = bfs(start, 'L', maps, n, m);
+        if (sToL == -1) {
+            return -1;
+        }
 
-        return (startToLever > 0 && leverToEnd > 0) ? startToLever + leverToEnd : -1;
+        int lToE = bfs(lever, 'E', maps, n, m);
+        if (lToE == -1) {
+            return -1;
+        }
+
+        return sToL + lToE;
     }
 
-    public int bfs(char start, char end, String[] maps, Map<Character, Point> points) {
-        Point p = points.get(start);
-        Queue<Point> q = new LinkedList<>();
-        q.add(p);
+    private int bfs(int[] start, char target, String[] maps, int n, int m) {
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[n][m];
 
-        boolean[][] visited = new boolean[maps.length][maps[0].length()];
-        visited[p.x][p.y] = true;
+        q.offer(new int[]{start[0], start[1], 0});
+        visited[start[0]][start[1]] = true;
 
         while (!q.isEmpty()) {
-            Point cur = q.poll();
-            if (maps[cur.x].charAt(cur.y) == end) {
-                return cur.step;
+            int[] cur = q.poll();
+            int x = cur[0], y = cur[1], dist = cur[2];
+
+            if (maps[x].charAt(y) == target) {
+                return dist;
             }
 
             for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-                if (nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length()) {
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
                     continue;
                 }
                 if (visited[nx][ny] || maps[nx].charAt(ny) == 'X') {
@@ -59,10 +66,10 @@ class Solution {
                 }
 
                 visited[nx][ny] = true;
-                q.add(new Point(nx, ny, cur.step + 1));
+                q.offer(new int[]{nx, ny, dist + 1});
             }
         }
-        
-        return 0;
+
+        return -1;
     }
 }
