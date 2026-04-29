dbf575c76fdff3d7cdaae481c96232062aee19c3
devys <66625672+yeseong31@users.noreply.github.com>
2026-04-29 20:49:51 +0900

    [level 2] Title: 호텔 대실, Time: 6.75 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,59 +1,43 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
-    class Time implements Comparable<Time>{
-        public int start;
-        public int end;
-        
-        public Time(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-        
-        @Override
-        public int compareTo(Time other) {
-            return this.start != other.start 
-                ? Integer.compare(this.start, other.start) 
-                : Integer.compare(this.end, other.end);
-        }
+
+    private int toMinute(String time) {
+        return (time.charAt(0) - '0') * 600
+             + (time.charAt(1) - '0') * 60
+             + (time.charAt(3) - '0') * 10
+             + (time.charAt(4) - '0');
     }
-    
-    private int convertTime(String time) {
-        int minute = Integer.parseInt(time.substring(0, 2));
-        int second = Integer.parseInt(time.substring(3, time.length()));
-        return minute * 60 + second;
-    }
-    
+
     public int solution(String[][] book_time) {
-        int answer = 1;
+        int n = book_time.length;
+        int[][] times = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            times[i][0] = toMinute(book_time[i][0]);
+            times[i][1] = toMinute(book_time[i][1]) + 10;
+        }
+
+        Arrays.sort(times, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        Time[] times = new Time[book_time.length];
-        
-        for (int i = 0; i < book_time.length; i++) {
-            int start = convertTime(book_time[i][0]);
-            int end = convertTime(book_time[i][1]);
-            times[i] = new Time(start, end);
-        }
-        
-        Arrays.sort(times);
-        
-        for (Time t : times) {
-            if (pq.isEmpty()) {
-                pq.offer(t.end);
-                continue;
-            }
-            
-            if (pq.peek() <= t.start) {
+
+        for (int i = 0; i < n; i++) {
+            int start = times[i][0];
+            int end = times[i][1];
+
+            if (!pq.isEmpty() && pq.peek() <= start) {
                 pq.poll();
-            } else {
-                answer++;
             }
-            
-            pq.offer(t.end + 10);
+
+            pq.offer(end);
         }
-        
-        return answer;
+
+        return pq.size();
     }
 }
