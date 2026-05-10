d4b54d89241145ab8a43f7c6916cb9a596c9bef1
devys <66625672+yeseong31@users.noreply.github.com>
2026-05-10 22:19:48 +0900

    [level 2] Title: 모음 사전, Time: 0.05 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,23 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    private static final char[] vowels = {'A', 'E', 'I', 'O', 'U'};
-    
-    private static void dfs(StringBuilder sb, List<String> words) {
-        words.add(sb.toString());
-        if (sb.length() == 5) return;
-        
-        for (char vowel : vowels) {
-            sb.append(vowel);
-            dfs(sb, words);
-            sb.deleteCharAt(sb.length() - 1);
-        }
-    }
     
     public int solution(String word) {
-        List<String> words = new ArrayList<>();
-        dfs(new StringBuilder(), words);
-        return words.indexOf(word);
+        String v = "AEIOU";
+        int[] weights = {781, 156, 31, 6, 1};
+        int answer = word.length();
+
+        for (int i = 0; i < word.length(); i++) {
+            answer += v.indexOf(word.charAt(i)) * weights[i];
+        }
+
+        return answer;
     }
 }
