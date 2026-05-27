7c42cbbd6a916f01d3a59249c102fee1a0fe6eb0
Yeseong <66625672+yeseong31@users.noreply.github.com>
2026-05-27 21:01:39 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
-메모리: 76 MB, 시간: 0.62 ms
+메모리: 61 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 01일 20:10:14
+2026년 05월 27일 21:01:36
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,21 +1,20 @@
 import java.util.*;
 
 class Solution {
-    
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        Deque<String> q1 = new ArrayDeque<>(Arrays.asList(cards1));
-        Deque<String> q2 = new ArrayDeque<>(Arrays.asList(cards2));
-        
+        int idx1 = 0;
+        int idx2 = 0;
+
         for (String word : goal) {
-            if (!q1.isEmpty() && q1.peekFirst().equals(word)) {
-                q1.pollFirst();
-            } else if (!q2.isEmpty() && q2.peekFirst().equals(word)) {
-                q2.pollFirst();
+            if (idx1 < cards1.length && cards1[idx1].equals(word)) {
+                idx1++;
+            } else if (idx2 < cards2.length && cards2[idx2].equals(word)) {
+                idx2++;
             } else {
                 return "No";
             }
         }
-        
+
         return "Yes";
     }
 }
