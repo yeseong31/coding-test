cff55a25bda104331cc1823d29c7e8462bd31480
Yeseong <66625672+yeseong31@users.noreply.github.com>
2026-07-04 21:40:45 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 12.44 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -1,10 +1,6 @@
 import java.util.*;
 
 class Solution {
-
-    private static final List<Integer> toSolve = new ArrayList<>();
-    private static final List<Integer> candidates = new ArrayList<>();
-
     private static int[] nums1;
     private static int[] nums2;
     private static int[] results;
@@ -12,108 +8,107 @@ class Solution {
 
     public static String[] solution(String[] expressions) {
         int n = expressions.length;
-
         nums1 = new int[n];
         nums2 = new int[n];
         results = new int[n];
         ops = new char[n];
 
+        List<Integer> toSolve = new ArrayList<>();
+        boolean[] isSolve = new boolean[n];
+        List<Integer> candidates = new ArrayList<>();
+
         int maxDigit = -1;
 
         for (int i = 0; i < n; i++) {
             String[] tokens = expressions[i].split(" ");
-
             nums1[i] = Integer.parseInt(tokens[0]);
             nums2[i] = Integer.parseInt(tokens[2]);
-            results[i] = (!tokens[4].equals("X")) ? Integer.parseInt(tokens[4]) : -1;
+            results[i] = tokens[4].equals("X") ? -1 : Integer.parseInt(tokens[4]);
             ops[i] = tokens[1].charAt(0);
-
-            maxDigit = Math.max(maxDigit, getMaxNum(nums1[i], nums2[i], results[i]));
-
-            if (results[i] == -1) toSolve.add(i);
+            maxDigit = Math.max(maxDigit, getMaxDigit(nums1[i], nums2[i], results[i]));
+            if (results[i] == -1) {
+                toSolve.add(i);
+                isSolve[i] = true;
+            }
         }
 
         for (int k = maxDigit + 1; k <= 9; k++) {
-            boolean matchAll = true;
-
+            boolean valid = true;
             for (int i = 0; i < n; i++) {
-                if (!toSolve.contains(i) && calculate(i, k) != results[i]) {
-                    matchAll = false;
+                if (isSolve[i]) continue;
+                if (calculate(i, k) != results[i]) {
+                    valid = false;
                     break;
                 }
             }
-
-            if (matchAll) candidates.add(k);
+            if (valid) candidates.add(k);
         }
 
-        for (int i = 0; i < n; i++) {
-            if (!toSolve.contains(i)) continue;
-
-            int res = -1;
-            boolean matchAll = true;
-
+        for (int i : toSolve) {
+            if (candidates.isEmpty()) continue;
+            int res = calculate(i, candidates.get(0));
+            boolean unique = true;
             for (int k : candidates) {
-                int tmp = calculate(i, k);
-
-                if (res == -1) {
-                    res = tmp;
-                } else if (res != tmp) {
-                    matchAll = false;
+                if (calculate(i, k) != res) {
+                    unique = false;
                     break;
                 }
             }
-
-            if (matchAll) results[i] = res;
+            if (unique) results[i] = res;
         }
 
         return toSolve.stream()
-                .map(i ->
-                        String.format("%d %c %d = %s",
-                                nums1[i], ops[i], nums2[i],
-                                (results[i] != -1) ? results[i] : "?"))
+                .map(i -> String.format("%d %c %d = %s",
+                        nums1[i], ops[i], nums2[i],
+                        results[i] != -1 ? results[i] : "?"))
                 .toArray(String[]::new);
     }
 
-    private static int getMaxNum(int a, int b, int res) {
-        int max1 = Math.max(a / 10, a % 10);
-        int max2 = Math.max(b / 10, b % 10);
-        int max3 = (res != -1) ? Math.max((res % 100) / 10, res % 10) : -1;
+    private static int getMaxDigit(int a, int b, int res) {
+        int max = 0;
+        max = Math.max(max, maxDigitOf(a));
+        max = Math.max(max, maxDigitOf(b));
+        if (res != -1) max = Math.max(max, maxDigitOf(res));
+        return max;
+    }
 
-        return Math.max(max1, Math.max(max2, max3));
+    private static int maxDigitOf(int x) {
+        int max = 0;
+        while (x > 0) {
+            max = Math.max(max, x % 10);
+            x /= 10;
+        }
+        return max;
     }
 
     private static int calculate(int i, int k) {
         int a = convertToDecimal(nums1[i], k);
         int b = convertToDecimal(nums2[i], k);
-
         int res = (ops[i] == '+') ? a + b : a - b;
-
         return convertFromDecimal(res, k);
     }
 
     private static int convertToDecimal(int x, int k) {
         int res = 0;
         int pow = 1;
-
         while (x > 0) {
             res += (x % 10) * pow;
             pow *= k;
             x /= 10;
         }
-
         return res;
     }
 
     private static int convertFromDecimal(int x, int k) {
+        if (x < 0) return -convertFromDecimal(-x, k);
+        if (x == 0) return 0;
         int res = 0;
         int pow = 1;
-
         while (x > 0) {
             res += (x % k) * pow;
             x /= k;
             pow *= 10;
         }
-
         return res;
     }
 }
