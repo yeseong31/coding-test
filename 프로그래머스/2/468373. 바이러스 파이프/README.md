a4e311434ddf89d971f1b32b571e5f5749a4e443
Yeseong <66625672+yeseong31@users.noreply.github.com>
2026-07-17 19:57:40 +0900

    [level 2] Title: 바이러스 파이프, Time: 4.03 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/468373. 바이러스 파이프/README.md
+++ 프로그래머스/2/468373. 바이러스 파이프/README.md
@@ -1,10 +1,10 @@
 # [level 2] 바이러스 파이프 - 468373 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/468373?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/468373?language=python3) 
 
 ### 성능 요약
 
-메모리: 65 MB, 시간: 5.33 ms
+메모리: 11.7 MB, 시간: 4.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 19일 21:27:50
+2026년 07월 17일 19:57:36
 
 ### 문제 설명
 
--- 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.py
+++ 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.py
@@ -1,46 +1,32 @@
 from collections import deque
 
 def solution(n, infection, edges, k):
-    answer = 1
-
     graph = [[] for _ in range(n + 1)]
-    for x, y, t in edges:
-        graph[x].append((y, t))
-        graph[y].append((x, t))
+    for a, b, t in edges:
+        graph[a].append((b, t))
+        graph[b].append((a, t))
 
-    def bfs(infected, type_):
-        next_state = infected[:]
-        queue = deque()
-
-        for i in range(1, n + 1):
-            if infected[i]:
-                queue.append(i)
-
-        while queue:
-            curr = queue.popleft()
-            for to, edge_type in graph[curr]:
-                if edge_type == type_ and not next_state[to]:
-                    next_state[to] = True
-                    queue.append(to)
-
-        return next_state
-
-    def dfs(step, infected):
-        nonlocal answer
-
-        count = sum(infected)
-        answer = max(answer, count)
-
-        if step == k:
-            return
-
-        for type_ in range(1, 4):
-            next_state = bfs(infected, type_)
-            dfs(step + 1, next_state)
-
+    max_infected = [1]
     infected = [False] * (n + 1)
     infected[infection] = True
 
-    dfs(0, infected)
+    def bfs(infected, edge_type):
+        next_state = infected[:]
+        queue = deque(i for i in range(1, n + 1) if infected[i])
+        while queue:
+            curr = queue.popleft()
+            for to, t in graph[curr]:
+                if t == edge_type and not next_state[to]:
+                    next_state[to] = True
+                    queue.append(to)
+        return next_state
 
-    return answer
+    def dfs(step, infected):
+        max_infected[0] = max(max_infected[0], sum(infected))
+        if step == k:
+            return
+        for edge_type in range(1, 4):
+            dfs(step + 1, bfs(infected, edge_type))
+
+    dfs(0, infected)
+    return max_infected[0]
