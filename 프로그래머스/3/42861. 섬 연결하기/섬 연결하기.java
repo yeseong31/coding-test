643426026426774082a8266d051bd81f279d7f27
Yeseong <66625672+yeseong31@users.noreply.github.com>
2026-08-10 22:35:57 +0900

    [level 3] Title: 섬 연결하기, Time: 0.79 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,61 +1,44 @@
 import java.util.Arrays;
-import java.util.Comparator;
-
-class Node {
-    public final int a;
-    public final int b;
-    public final int c;
-    
-    public Node(int a, int b, int c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-}
 
 class Solution {
-    
-    private static int find(int[] parents, int x) {
-        if (parents[x] != x) {
-            parents[x] = find(parents, parents[x]);
+
+    private int[] parent;
+    private int[] rank_;
+
+    private int find(int x) {
+        while (parent[x] != x) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
         }
-        return parents[x];
+        return x;
     }
-    
-    private static void union(int[] parents, int a, int b) {
-        a = find(parents, a);
-        b = find(parents, b);
-        
-        if (a < b) {
-            parents[b] = a;
-        } else {
-            parents[a] = b;
+
+    private boolean union(int a, int b) {
+        int ra = find(a), rb = find(b);
+        if (ra == rb) return false;
+
+        if (rank_[ra] < rank_[rb]) {
+            int tmp = ra; ra = rb; rb = tmp;
         }
+        parent[rb] = ra;
+        if (rank_[ra] == rank_[rb]) rank_[ra]++;
+        return true;
     }
-    
+
     public int solution(int n, int[][] costs) {
-        
-        Node[] nodes = Arrays.stream(costs)
-                .map(v -> new Node(v[0], v[1], v[2]))
-                .sorted(Comparator.comparingInt(v -> v.c))
-                .toArray(Node[]::new);
-        
-        int[] parents = new int[n];
-        for (int i = 0; i < n; i++) {
-            parents[i] = i;
-        }
-        
+        Arrays.sort(costs, (x, y) -> x[2] - y[2]);
+
+        parent = new int[n];
+        rank_ = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+
         int answer = 0;
-        for (int seq = 0; seq < nodes.length; seq++) {
-            int a = nodes[seq].a;
-            int b = nodes[seq].b;
-            
-            if (find(parents, a) != find(parents, b)) {
-                union(parents, a, b);
-                answer += nodes[seq].c;
+        for (int[] edge : costs) {
+            int a = edge[0], b = edge[1], cost = edge[2];
+            if (union(a, b)) {
+                answer += cost;
             }
         }
-        
         return answer;
     }
 }
