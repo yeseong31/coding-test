9f5716f0ed1aebc102112fd343e9b647cbba1291
Yeseong <66625672+yeseong31@users.noreply.github.com>
2026-08-27 21:01:23 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,20 +1,37 @@
-SELECT 
-    E.EMP_NO, 
-    E.EMP_NAME, 
-    CASE
-        WHEN AVG(G.SCORE) >= 96 THEN 'S'
-        WHEN AVG(G.SCORE) >= 90 THEN 'A'
-        WHEN AVG(G.SCORE) >= 80 THEN 'B'
-        ELSE 'C'
-    END AS GRADE, 
-    CASE
-        WHEN AVG(G.SCORE) >= 96 THEN E.SAL * 20 / 100
-        WHEN AVG(G.SCORE) >= 90 THEN E.SAL * 15 / 100
-        WHEN AVG(G.SCORE) >= 80 THEN E.SAL * 10 / 100
-        ELSE 0
-    END AS BONUS
-FROM HR_EMPLOYEES E
-    INNER JOIN HR_DEPARTMENT D ON E.DEPT_ID = D.DEPT_ID
-    INNER JOIN HR_GRADE G ON E.EMP_NO = G.EMP_NO
-GROUP BY E.EMP_NO, E.EMP_NAME, E.SAL
-ORDER BY E.EMP_NO;
+WITH EMP_EVALUATION AS (
+    SELECT
+        E.EMP_NO,
+        E.EMP_NAME,
+        E.SAL,
+        AVG(G.SCORE) AS AVG_SCORE
+    FROM HR_EMPLOYEES E
+    JOIN HR_GRADE G
+      ON G.EMP_NO = E.EMP_NO
+    GROUP BY E.EMP_NO, E.EMP_NAME, E.SAL
+),
+EMP_GRADE AS (
+    SELECT
+        EMP_NO,
+        EMP_NAME,
+        SAL,
+        CASE
+            WHEN AVG_SCORE >= 96 THEN 'S'
+            WHEN AVG_SCORE >= 90 THEN 'A'
+            WHEN AVG_SCORE >= 80 THEN 'B'
+            ELSE 'C'
+        END AS GRADE,
+        CASE
+            WHEN AVG_SCORE >= 96 THEN 0.20
+            WHEN AVG_SCORE >= 90 THEN 0.15
+            WHEN AVG_SCORE >= 80 THEN 0.10
+            ELSE 0
+        END AS BONUS_RATE
+    FROM EMP_EVALUATION
+)
+SELECT
+    EMP_NO,
+    EMP_NAME,
+    GRADE,
+    SAL * BONUS_RATE AS BONUS
+FROM EMP_GRADE
+ORDER BY EMP_NO;
