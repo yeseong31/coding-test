c6df723f5eb95bb5a4c009faaefdde6a3d000a40
Yeseong <66625672+yeseong31@users.noreply.github.com>
2026-09-02 21:18:33 +0900

    [level 2] Title: 짝지어 제거하기, Time: 13.36 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,13 +1,23 @@
-import java.util.*;
-
 class Solution {
 
     public int solution(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
-        for (char c : s.toCharArray()) {
-            if (!stack.isEmpty() && stack.peek() == c) stack.pop();
-            else stack.push(c);
+        if (s.length() % 2 == 1) {
+            return 0;
         }
-        return stack.isEmpty() ? 1 : 0;
+
+        char[] stack = new char[s.length()];
+        int top = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+
+            if (top > 0 && stack[top - 1] == current) {
+                top--;
+            } else {
+                stack[top++] = current;
+            }
+        }
+
+        return top == 0 ? 1 : 0;
     }
 }
