783610c7934ba3d1bce9b7ff5e20fa8853097f6b
Yeseong <66625672+yeseong31@users.noreply.github.com>
2026-09-03 22:01:12 +0900

    [level 3] Title: 이중우선순위큐, Time: 37.69 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,27 +1,38 @@
 import java.util.TreeMap;
 
 class Solution {
+    
     public int[] solution(String[] operations) {
         TreeMap<Integer, Integer> map = new TreeMap<>();
-        
-        for (String op : operations) {
-            String[] tokens = op.split(" ");
-            String cmd = tokens[0];
-            int val = Integer.parseInt(tokens[1]);
-            
-            if (cmd.equals("I")) {
-                map.put(val, map.getOrDefault(val, 0) + 1);
-            } else if (!map.isEmpty()) {
-                int key = (val == 1) ? map.lastKey() : map.firstKey();
-                if (map.put(key, map.get(key) - 1) == 1) {
+
+        for (String operation : operations) {
+            String[] tokens = operation.split(" ");
+            String command = tokens[0];
+            int value = Integer.parseInt(tokens[1]);
+
+            if (command.equals("I")) {
+                map.put(value, map.getOrDefault(value, 0) + 1);
+                continue;
+            }
+            if (!map.isEmpty()) {
+                int key = (value == 1)
+                        ? map.lastKey()
+                        : map.firstKey();
+
+                int count = map.get(key);
+
+                if (count == 1) {
                     map.remove(key);
+                } else {
+                    map.put(key, count - 1);
                 }
             }
         }
-        
+
         if (map.isEmpty()) {
-            return new int[] {0, 0};
+            return new int[]{0, 0};
         }
-        return new int[] {map.lastKey(), map.firstKey()};
+
+        return new int[]{map.lastKey(), map.firstKey()};
     }
 }
