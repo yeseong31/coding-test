2979a1028953d592b8e9444b437b3a527f383cdd
Yeseong <66625672+yeseong31@users.noreply.github.com>
2026-09-09 23:20:22 +0900

    [level 2] Title: 프로세스, Time: 1.08 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,33 +1,38 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 class Solution {
     
     public int solution(int[] priorities, int location) {
-        Queue<int[]> queue = new LinkedList<>();
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        
+        Queue<int[]> queue = new ArrayDeque<>();
+        PriorityQueue<Integer> maxHeap =
+                new PriorityQueue<>(Collections.reverseOrder());
+
         for (int i = 0; i < priorities.length; i++) {
             queue.offer(new int[]{i, priorities[i]});
             maxHeap.offer(priorities[i]);
         }
-        
+
         int order = 0;
-        
+
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
-            
+
             if (current[1] < maxHeap.peek()) {
                 queue.offer(current);
-            } else {
-                order++;
-                maxHeap.poll();
-                
-                if (current[0] == location) {
-                    return order;
-                }
+                continue;
+            }
+
+            order++;
+            maxHeap.poll();
+
+            if (current[0] == location) {
+                return order;
             }
         }
-        
-        return order;
+
+        return -1;
     }
 }
